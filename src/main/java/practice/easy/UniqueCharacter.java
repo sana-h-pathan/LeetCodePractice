0920436236d9d6899de750388fe1c4bfa8d9b3aa
sanapathan28 <63260577+sanapathan28@users.noreply.github.com>
2022-12-20 16:24:02 -0800

    387. First Unique Character in a String

---
+++ src/main/java/practice/easy/UniqueCharacter.java
@@ -0,0 +1,31 @@
+package practice.easy;
+
+import javax.swing.tree.TreeNode;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueCharacter {
+    public boolean uniqueOccurrences(int[] arr) {
+        //array to store number of occurrences
+        int[] c = new int[2001];
+        //count occurrences of each value
+        for (int n : arr) {
+            c[n+1000]++;
+        }
+        //check unique occurances (except for 0)
+        Set<Integer> set = new HashSet();
+        for (int count : c) {
+            if (count == 0)
+                continue;
+            if (!set.add(count))
+                return false;
+        }
+        return true;
+    }
+
+        public static void main(String[] args) {
+        UniqueCharacter uniqueCharacter =new UniqueCharacter();
+
+            System.out.println(uniqueCharacter.uniqueOccurrences(new int[]{1,2,2,1,1,3}));
+    }
+}
