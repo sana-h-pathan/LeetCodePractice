7ff51dd53ef68c750edafb720fa2cda60c9cbff8
sanapathan28 <63260577+sanapathan28@users.noreply.github.com>
2022-12-20 16:22:06 -0800

    509. Fibonacci Number

---
+++ src/main/java/practice/easy/FibonacciNumber.java
@@ -0,0 +1,18 @@
+package practice.easy;
+
+public class FibonacciNumber {
+    public int fib(int n) {
+        int[] fibNumber=new int[n+1];
+        if(fibNumber[n] != 0)
+            return fibNumber[n];
+        if(n<=1)
+            return n;
+        fibNumber[n] = fib(n-1) + fib(n-2);
+        return fibNumber[n];
+    }
+
+    public static void main(String[] args) {
+        FibonacciNumber fibonacciNumber=new FibonacciNumber();
+        System.out.println(fibonacciNumber.fib(9));
+    }
+}
