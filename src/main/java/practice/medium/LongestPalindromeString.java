3a616185e754c155a691231e3b44ffb6caa7242c
Sana Pathan <sanapathan28@gmail.com>
2023-01-17 17:21:04 -0800

    5 Longest Palindrome Substring

--- src/main/java/practice/medium/LongestPalindromeString.java
+++ src/main/java/practice/medium/LongestPalindromeString.java
@@ -7,31 +7,34 @@ public class LongestPalindromeString {
         System.out.println(lps.longestPalindrome("abab"));
     }
 
-    private String  longestPalindrome(String s) {
-        if(s.length()<1 || s==null)
+    int maxLen;
+    int start = 0, end = 0;
+
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() == 0)
             return "";
 
-        int start=0;
-        int end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandFromMiddle(s, i, i);
-            int len2 = expandFromMiddle(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
+        for(int i=0; i<s.length(); i++) {
+            checkForLongestPalindrome(s, i, i);
+            checkForLongestPalindrome(s, i, i+1);
         }
-        return s.substring(start, end + 1);
+
+        return s.substring(start, end+1);
     }
 
-    private int expandFromMiddle(String s, int left, int right) {
-        if(left>right || s==null)
-            return 0;
-        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+    private void checkForLongestPalindrome(String s, int left, int right) {
+        while(left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        return right-left-1;
+        left++;
+        right--;
+
+        if(maxLen < right-left+1) {
+            maxLen = right-left+1;
+            start = left;
+            end = right;
+        }
     }
 }
