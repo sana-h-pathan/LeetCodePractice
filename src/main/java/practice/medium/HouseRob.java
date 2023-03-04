98bac13d093516cacd5222a67d006a882691de74
Sana Pathan <sanapathan28@gmail.com>
2023-03-04 13:07:04 -0800

    198. House Robber

---
+++ src/main/java/practice/medium/HouseRob.java
@@ -0,0 +1,19 @@
+package practice.medium;
+
+public class HouseRob {
+    public int rob(int[] num) {
+        int rob = 0; //max monney can get if rob current house
+        int notrob = 0; //max money can get if not rob current house
+        for(int i=0; i<num.length; i++) {
+            int currob = notrob + num[i]; //if rob current value, previous house must not be robbed
+            notrob = Math.max(notrob, rob); //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
+            rob = currob;
+        }
+        return Math.max(rob, notrob);
+    }
+
+    public static void main(String[] args) {
+        HouseRob houseRob=new HouseRob();
+        houseRob.rob(new int[]{2,7,9,3,1});
+    }
+}
