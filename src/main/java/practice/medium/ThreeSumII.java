cf807f6d6999e2a2ac00cdf68c22da5458b96a26
Sana Pathan <sanapathan28@gmail.com>
2023-03-04 13:07:47 -0800

    15. 3Sum

---
+++ src/main/java/practice/medium/ThreeSumII.java
@@ -0,0 +1,36 @@
+package practice.medium;
+
+import java.util.HashSet;
+import java.util.*;
+
+public class ThreeSumII {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int target = 0;
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        output.addAll(s);
+        return output;
+    }
+
+    public static void main(String[] args) {
+        ThreeSumII threeSumII=new ThreeSumII();
+        threeSumII.threeSum(new int[]{-1,0,1,2,-1,-4});
+    }
+}
