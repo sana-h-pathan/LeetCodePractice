11a74d92e556aa79b3a2e3d4413105d038726321
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-03-18 17:56:39 -0700

    Time: 1 ms (100.00%) | Memory: 45.4 MB (31.91%) - LeetSync

--- 490-the-maze/the-maze.java
+++ 490-the-maze/the-maze.java
@@ -1,67 +1,86 @@
 class Solution {
-    int[][] dirs;
-    int m;
-    int n;
+    int[][] directions; // Directions array to store possible movement directions
+    int rows; // Number of rows in the maze
+    int cols; // Number of columns in the maze
+    
     public boolean hasPathBFS(int[][] maze, int[] start, int[] destination) {
-        this.dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        this.m=maze.length;
-        this.n=maze[0].length;
-        if(start[0]==destination[0] && start[1]==destination[1])
+        this.directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}}; // Right, Down, Left, Up
+        this.rows = maze.length;
+        this.cols = maze[0].length;
+        
+        // If start and destination are the same, return true
+        if(start[0] == destination[0] && start[1] == destination[1])
             return true;
-        Queue<int[]> bfsQueue=new LinkedList<>();
+        
+        Queue<int[]> bfsQueue = new LinkedList<>(); // Queue for BFS traversal
         bfsQueue.add(start);
-        maze[start[0]][start[1]]=2;
+        maze[start[0]][start[1]] = 2; // Mark start as visited
+        
+        // Perform BFS traversal
         while (!bfsQueue.isEmpty()){
-            int[] curr=bfsQueue.poll();
-            for(int[] dir: dirs){
-                int cr=curr[0];
-                int cc=curr[1];
-                while(cr>=0 && cc>=0 && cr<m && cc<n && maze[cr][cc]!=1){
-                    cr+=dir[0];
-                    cc+=dir[1];
+            int[] current = bfsQueue.poll();
+            for(int[] dir: directions){
+                int cr = current[0]; // Current row
+                int cc = current[1]; // Current column
+                // Move until hitting a wall or border
+                while(cr >= 0 && cc >= 0 && cr < rows && cc < cols && maze[cr][cc] != 1){
+                    cr += dir[0];
+                    cc += dir[1];
                 }
-                cr-=dir[0];
-                cc-=dir[1];
-                if(maze[cr][cc]!=2){
-                    if(cr==destination[0] && cc==destination[1])
+                // Step back to the valid position
+                cr -= dir[0];
+                cc -= dir[1];
+                
+                // If the cell is not visited, mark it visited and add to the queue
+                if(maze[cr][cc] != 2){
+                    if(cr == destination[0] && cc == destination[1])
                         return true;
-                    bfsQueue.add(new int[]{cr,cc});
-                    maze[cr][cc]=2;
+                    bfsQueue.add(new int[]{cr, cc});
+                    maze[cr][cc] = 2; // Mark as visited
                 }
             }
         }
-        return false;
+        return false; // If destination is not reached
     }
 
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        this.dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        this.m=maze.length;
-        this.n=maze[0].length;
-        if(start[0]==destination[0] && start[1]==destination[1])
-            return true;
-        maze[start[0]][start[1]]=2;
-        return dfs(maze, start[0], start[1], destination);
+        this.directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}}; // Right, Down, Left, Up
+        this.rows = maze.length;
+        this.cols = maze[0].length;
         
+        // If start and destination are the same, return true
+        if(start[0] == destination[0] && start[1] == destination[1])
+            return true;
+        
+        maze[start[0]][start[1]] = 2; // Mark start as visited
+        return dfs(maze, start[0], start[1], destination); // Perform DFS
     }
 
     private boolean dfs(int[][] maze, int i, int j, int[] destination){
-        if(i==destination[0] && j==destination[1])
+        // If current position is the destination, return true
+        if(i == destination[0] && j == destination[1])
             return true;
-        for(int[] dir: dirs){
-            int cr=i;
-            int cc=j;
-            while(cr>=0 && cc>=0 && cr<m && cc<n && maze[cr][cc]!=1){
-                cr+=dir[0];
-                cc+=dir[1];
+        
+        // Explore in all directions
+        for(int[] dir: directions){
+            int cr = i; // Current row
+            int cc = j; // Current column
+            // Move until hitting a wall or border
+            while(cr >= 0 && cc >= 0 && cr < rows && cc < cols && maze[cr][cc] != 1){
+                cr += dir[0];
+                cc += dir[1];
             }
-            cr-=dir[0];
-            cc-=dir[1];
-            if(maze[cr][cc]!=2){
-                maze[cr][cc]=2;
+            // Step back to the valid position
+            cr -= dir[0];
+            cc -= dir[1];
+            
+            // If the cell is not visited, mark it visited and recursively explore
+            if(maze[cr][cc] != 2){
+                maze[cr][cc] = 2; // Mark as visited
                 if(dfs(maze, cr, cc, destination))
                     return true;
             }
         }
-        return false;
+        return false; // If destination is not reachable
     }
 }
