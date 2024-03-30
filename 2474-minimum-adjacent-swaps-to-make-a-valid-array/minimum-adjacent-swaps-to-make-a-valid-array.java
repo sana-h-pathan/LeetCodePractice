fdd1b110855be37dac07926df62199565e6782b4
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-03-29 22:42:46 -0700

    Time: 1 ms (100.00%) | Memory: 56 MB (95.68%) - LeetSync

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.java
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.java
@@ -1,34 +1,24 @@
 class Solution {
     public int minimumSwaps(int[] nums) {
-        // Variables to store minimum and maximum values, their indices, and length of the array
-        int minNum = Integer.MAX_VALUE;
-        int maxNumber = Integer.MIN_VALUE;
-        int minIdx = 0;
-        int maxIdx = 0;
-        int n = nums.length;
-        
-        // Iterate through the array to find the minimum and maximum values and their indices
-        for(int i = 0; i < n; i++) {
-            // Update minimum value and its index if a smaller value is found
-            if(nums[i] < minNum) {
-                minNum = nums[i];
-                minIdx = i;
+        int minIdx=0;
+        int min=Integer.MAX_VALUE;
+        int maxIdx=0;
+        int max=Integer.MIN_VALUE;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]<min){
+                min=nums[i];
+                minIdx=i;
             }
-            // Update maximum value and its index if a larger value is found
-            if(nums[i] >= maxNumber) {
-                maxNumber = nums[i];
-                maxIdx = i;
+            if(nums[i]>=max){
+                max=nums[i];
+                maxIdx=i;
             }
         }
-        
-        // Check if the minimum index is greater than the maximum index
-        if(minIdx > maxIdx)
-            // Return the number of swaps required to bring the maximum element to its correct position 
-            // and the minimum element to its correct position
-            return (n - maxIdx - 1) + minIdx - 1;
-        
-        // Return the number of swaps required to bring the maximum element to its correct position 
-        // and the minimum element to its correct position
-        return (n - maxIdx - 1) + minIdx;
+        int minSwap=minIdx;
+        int maxSwap=n-(maxIdx+1);
+        if(minIdx>maxIdx)
+            return minSwap+maxSwap-1;
+        return minSwap+maxSwap;
     }
 }
