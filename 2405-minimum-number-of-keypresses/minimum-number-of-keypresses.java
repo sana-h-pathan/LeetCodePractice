5c036cf57bf2165a32c2644a9ca9a795881c66cf
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-03-29 22:59:22 -0700

    Time: 9 ms (50.39%) | Memory: 45.1 MB (47.62%) - LeetSync

--- 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.java
+++ 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.java
@@ -1,25 +1,23 @@
 class Solution {
     public int minimumKeypresses(String s) {
-        if(s.length()<=9)
+        int n=s.length();
+        if(n<=9)
             return s.length();
-        Integer count[]=new Integer[26];
-        Arrays.fill(count,0);
-        int presses=0;
-        for(int i=0;i<s.length();i++)
-            count[s.charAt(i)-'a']++; //freq of character in the given string
-        
-        Arrays.sort(count,(a,b)->b-a); //sorting freq in descending order
-        
-        for(int i=0;i<26;i++)
-        {
-            if(i<9) //character with max frequencies ar accomodated first character of each number
-                presses+=count[i];
-            else if(i<18) //charatcer with intermediate freq are accomodates second position (that is 2 presses are required)
-                presses+=2*count[i];
-            else
-                presses+=3*count[i]; //similarly least frequent characters will need 3 presses
-            
+        Integer[] freqCount=new Integer[26];
+        Arrays.fill(freqCount, 0);
+        int totalPress=0;
+        for(int i=0;i<n;i++){
+            freqCount[s.charAt(i)-'a']++;
         }
-        return presses;
+        Arrays.sort(freqCount,(a,b)->b-a);
+        for(int i=0;i<26;i++){
+            if(i<9)
+                totalPress+=freqCount[i];
+            else if(i<18)
+                totalPress+=2*freqCount[i];
+            else
+                totalPress+=3*freqCount[i];
+        }
+        return totalPress;
     }
 }
