287eae2a151fea1261784ce04ae494b3530e460b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-03-29 17:58:56 -0700

    Time: 4 ms (52.08%) | Memory: 41.6 MB (56.43%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,39 +1,39 @@
 class Solution {
     public String reorganizeString(String s) {
-        PriorityQueue<int[]> pq=new PriorityQueue<int[]>((a, b) -> Integer.compare(b[1], a[1]));
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int maxFreq=0;
+        HashMap<Integer, Integer> map=new HashMap<>();
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a, b) -> Integer.compare(b[1], a[1]));
+        int maxFrequency=0;
         int n=s.length();
         for(char ch: s.toCharArray()){
-            map.put(ch-'a', map.getOrDefault(ch-'a', 0)+1);
-            maxFreq=Integer.max(maxFreq, map.get(ch-'a'));
+            int temp=ch-'a';
+            map.put(temp, map.getOrDefault(temp, 0)+1);
+            maxFrequency=Math.max(maxFrequency,map.get(temp));
         }
-        for(int key: map.keySet()){
-            pq.offer(new int[]{key,map.get(key)});
-        }
-        if (maxFreq > (n+1)/2)
+        if(maxFrequency>(n+1)/2)
             return "";
+        for(int key: map.keySet()){
+            pq.offer(new int[]{key, map.get(key)});
+        }
         StringBuilder sb=new StringBuilder();
         while(pq.size()>=2){
             int[] first=pq.poll();
             int[] second=pq.poll();
             sb.append((char) (first[0] + 'a'));
-            first[1]--;
             sb.append((char) (second[0] + 'a'));
-            second[1]--;
-            if(first[1]!=0)
+            first[1]--;
+            if(first[1]>0)
                 pq.add(first);
-            if(second[1]!=0)
+            second[1]--;
+            if(second[1]>0)
                 pq.add(second);
-            
         }
         while(!pq.isEmpty()){
-            int[] curr=pq.poll();
+            int [] curr=pq.poll();
             if(curr[1]>1)
                 return "";
             sb.append((char) (curr[0] + 'a'));
         }
         return sb.toString();
-       
+        
     }
 }
