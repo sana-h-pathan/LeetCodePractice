85b7621bb328cfe6431b2798b6d1cb384564c71e
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-04-05 17:03:06 -0700

    Time: 31 ms (20.41%) | Memory: 45.5 MB (29.12%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,42 +1,27 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        
-        HashSet<Integer> validIndexSet = new HashSet<>();
-        int balance = 0;
-        int openSeen =0;
-        Stack<Integer> st = new Stack<>();
-         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<s.length();i++) {
             char ch = s.charAt(i);
-            
-            if(ch =='('){
-          
-                balance++;
-                openSeen++;
-            }else if(ch == ')'){
-                //if close come when there was no open, we will remove it
-                if(balance ==0)
-                    continue ;
-               balance--;
+            if(Character.isAlphabetic(ch))
+                continue;
+            if(ch == '(')
+                stack.push(i);
+            else {
+                if(!stack.isEmpty() && s.charAt(stack.peek()) == '(')
+                    stack.pop();
+                else stack.push(i);
             }
-            //so sb will remove all invalid close brackets in this iteration
-            sb.append(ch);
-            
         }
-        StringBuilder res = new StringBuilder();
-        int openToKeep = openSeen - balance;
-        // we should add only openToKeep number of opens to final res.
-        //use sb created in previous iteration
-         for(int i = 0; i < sb.length(); i++){
-              char ch = sb.charAt(i);
-             if(ch =='('){
-                 openToKeep--;
-                 if(openToKeep <0)
-                     continue;
-             }
-             res.append(ch);
-         }
         
-        return res.toString();
+        // if(stack.size() == 0) return "";
+        
+        StringBuilder result = new StringBuilder();
+        HashSet<Integer> set = new HashSet<>(stack);
+        for(int i=0;i<s.length();i++)
+            if(!set.contains(i))
+                result.append(s.charAt(i));
+        
+        return result.toString();
     }
 }
