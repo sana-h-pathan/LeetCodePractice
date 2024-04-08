46f06ffa228bfb01c7c049f194259c6bbcc7c879
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-04-08 13:36:35 -0700

    Time: 36 ms (66.42%) | Memory: 55.4 MB (41.80%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,54 +1,48 @@
 class Trie {
-
-    class TrieNode {
-        boolean isEnd;
+    class TrieNode{
         TrieNode[] children;
-        public TrieNode() {
-            children = new TrieNode[26];
-            isEnd = false;
+        boolean isEnd;
+        public TrieNode(){
+            children=new TrieNode[26];
+            isEnd=false;
         }
     }
     TrieNode root;
-
     public Trie() {
-        root = new TrieNode();
-
+        this.root=new TrieNode();
     }
-
+    
     public void insert(String word) {
-        TrieNode curr = root;
-        for(int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (curr.children[c - 'a'] == null) {
-                curr.children[c - 'a'] = new TrieNode();
-            }
-            curr = curr.children[c - 'a'];
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            char ch=word.charAt(i);
+            if(curr.children[ch-'a']==null)
+                curr.children[ch-'a']=new TrieNode();
+            curr=curr.children[ch-'a'];
         }
-        curr.isEnd = true;
+        curr.isEnd=true;
     }
-
+    
     public boolean search(String word) {
-        TrieNode curr = root;
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (curr.children[c - 'a'] == null) {
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            char ch=word.charAt(i);
+            if(curr.children[ch-'a']==null)
                 return false;
-            }
-            curr = curr.children[c - 'a'];
+            curr=curr.children[ch-'a'];
         }
-        return curr.isEnd;
+        return curr.isEnd;  
     }
-
+    
     public boolean startsWith(String prefix) {
-        TrieNode curr = root;
-        for (int i = 0; i < prefix.length(); i++) {
-            char c = prefix.charAt(i);
-            if (curr.children[c - 'a'] == null) {
+        TrieNode curr=root;
+        for(int i=0;i<prefix.length();i++){
+            char ch=prefix.charAt(i);
+            if(curr.children[ch-'a']==null)
                 return false;
-            }
-            curr = curr.children[c - 'a'];
+            curr=curr.children[ch-'a'];
         }
-        return true;
+        return true;  
     }
 }
 
