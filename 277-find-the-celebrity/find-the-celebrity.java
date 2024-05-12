66e37ec860c9098257f52e66309d8ae7da2d4038
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-05-12 14:06:32 -0700

    Time: 14 ms (100.00%) | Memory: 45 MB (81.05%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.java
+++ 277-find-the-celebrity/find-the-celebrity.java
@@ -10,7 +10,9 @@ public class Solution extends Relation {
         }
         for(int i=0;i<n;i++){
             if(probableCeleb!=i){
-                if(knows(probableCeleb,i) || !knows(i,probableCeleb))
+                if(i<probableCeleb && knows(probableCeleb,i))
+                    return -1;
+                if(!knows(i,probableCeleb))
                     return -1;
             }
         }
