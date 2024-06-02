eef1ef03776f7343ce79aeba6aa9825e5b0dd08f
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-06-02 11:48:27 -0700

    Time: 0 ms (100.00%) | Memory: 44.6 MB (10.66%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -16,9 +16,12 @@
 class Solution {
     TreeNode prev=null;
     boolean flag=true;
+    boolean limit=true;
     public boolean isValidBST(TreeNode root) {
-        inOrder(root);
-        return flag;
+        //return inOrder1(root);
+        //return flag;
+        helper(root, null, null);
+        return limit;
     }
     private void inOrder(TreeNode root){
         if(root==null)
@@ -30,5 +33,32 @@ class Solution {
         }
         prev=root;
         inOrder(root.right);
+    }
+
+    private boolean inOrder1(TreeNode root){
+        if(root==null)
+            return true;
+        boolean left=inOrder1(root.left);
+        if(prev!=null && root.val<=prev.val){
+            return false;
+        }
+        prev=root;
+        boolean right=inOrder1(root.right);
+        return left && right;
+    }
+
+    private void helper(TreeNode root, Integer low, Integer high){
+        if(root==null)
+            return;
+        if(low!=null && root.val<=low){
+            limit=false;
+            return;
+        }
+        if(high!=null && root.val>=high){
+            limit=false;
+            return;
+        }
+        helper(root.left, low, root.val);
+        helper(root.right, root.val, high);
     }
 }
