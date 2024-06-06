473f28744a2fa655942e7dc81e694df716ae30ac
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-06-06 13:36:29 -0700

    Time: 52 ms (21.84%) | Memory: 47.7 MB (18.29%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,39 +1,28 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        // Array to store the count of distinct values encountered
-        int[] distinctCount = new int[nums.length + 1];
+        return findSub(nums, k)-findSub(nums, k-1);
+    }
 
-        int totalCount = 0;
-        int left = 0;
-        int right = 0;
-        int currCount = 0;
-
-        while (right < nums.length) {
-            // Increment the count of the current element in the window
-            if (distinctCount[nums[right++]]++ == 0) {
-                // If encountering a new distinct element, decrement K
-                k--;
+    private int findSub(int[] nums, int goal){
+        if(goal<0)
+            return 0;
+        int l=0;
+        int r=0;
+        int sum=0;
+        int count=0;
+        HashMap<Integer,Integer> map =new HashMap<>();
+        while(r<nums.length){
+            map.put(nums[r],map.getOrDefault(nums[r], 0)+1);
+            while(map.size()>goal){
+                map.put(nums[l],map.getOrDefault(nums[l], 0)-1);
+                if(map.get(nums[l])==0)
+                    map.remove(nums[l]);
+                l++;
+                
             }
-
-            // If K becomes negative, adjust the window from the left
-            if (k < 0) {
-                // Move the left pointer until the count of distinct elements becomes valid again
-                --distinctCount[nums[left++]];
-                k++;
-                currCount = 0;
-            }
-
-            // If K becomes zero, calculate subarrays
-            if (k == 0) {
-                // While the count of left remains greater than 1, keep shrinking the window from the left
-                while (distinctCount[nums[left]] > 1) {
-                    --distinctCount[nums[left++]];
-                    currCount++;
-                }
-                // Add the count of subarrays with K distinct elements to the total count
-                totalCount += (currCount + 1);
-            }
+            count+=r-l+1;
+            r++;
         }
-        return totalCount;
+        return count;
     }
 }
