db66773eb4eac7c355f47d9572910265505f8016
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-06-28 15:50:45 -0700

    Time: 7 ms (87.70%) | Memory: 62.7 MB (65.77%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -1,22 +1,22 @@
 class Solution {
+
     public long maximumImportance(int n, int[][] roads) {
-        HashMap<Integer, Integer> map=new HashMap<>();
-        for(int[] road: roads){
-            map.put(road[0], map.getOrDefault(road[0], 0)+1);
-            map.put(road[1], map.getOrDefault(road[1], 0)+1);
+        long[] inDegree = new long[n];
+
+        for (int[] road : roads) {
+            inDegree[road[0]]++;
+            inDegree[road[1]]++;
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
-        for (int key : map.keySet()) {
-            pq.add(key);
+
+        Arrays.sort(inDegree);
+
+        long count = 1;
+        long result = 0;
+        for (long degree : inDegree) {
+            result += (count * degree);
+            count++;
         }
-        
-        // Assign importance values to cities, starting from the highest value
-        long answer = 0;
-        int count = n;
-        while (!pq.isEmpty()) {
-            answer += (long) map.get(pq.poll()) * count;
-            count--;
-        }
-        return answer;
+
+        return result;
     }
 }
