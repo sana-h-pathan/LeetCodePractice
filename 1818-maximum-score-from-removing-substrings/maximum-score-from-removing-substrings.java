08d29666c6ac4c19743479f0c208e377932a69d1
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-07-11 19:28:56 -0700

    Time: 112 ms (61.62%) | Memory: 45 MB (97.98%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int maximumGain(String s, int x, int y) {
+        StringBuilder text = new StringBuilder(s);
+        int totalPoints = 0;
+
+        if (x > y) {
+            // Remove "ab" first (higher points), then "ba"
+            totalPoints += removeSubstring(text, "ab", x);
+            totalPoints += removeSubstring(text, "ba", y);
+        } else {
+            // Remove "ba" first (higher or equal points), then "ab"
+            totalPoints += removeSubstring(text, "ba", y);
+            totalPoints += removeSubstring(text, "ab", x);
+        }
+
+        return totalPoints;
+    }
+
+    private int removeSubstring(StringBuilder s,String t,int pts) {
+        int totalPoints = 0;
+        int l=0;
+        int r=0;
+        while (r<s.length()) {
+            s.setCharAt(l++, s.charAt(r));
+            if(l>1&&s.substring(l-2,l).equals(t)){
+            //if (l>1 && s.charAt(l - 2) == t.charAt(0) && s.charAt(l - 1) == t.charAt(1)) {
+                l -= 2; 
+                totalPoints += pts;
+            }
+            r++;
+        }
+        s.setLength(l);
+        return totalPoints;
+    }
+}
