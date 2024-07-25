6a278cd0beb1277b7143c638c2ce507b0a6ebaef
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-07-24 17:17:04 -0700

    Time: 0 ms (100.00%) | Memory: 41.5 MB (8.82%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,25 +1,24 @@
-import java.util.Collections;
-
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1)
-            return nums[0];
-        if(n==2)
-            return Math.max(nums[0],nums[1]);
-        int first=helper(0,n-1,nums);
-        int second=helper(1,n,nums);
-        return Math.max(first,second);
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
+
+        // Consider two cases: not robbing the first house or not robbing the last house
+        return Math.max(robRange(nums, 0, n - 2), robRange(nums, 1, n - 1));
     }
 
-    public int helper(int start, int end, int[] nums){
-        int prev=nums[start];
-        int curr=Math.max(nums[start],nums[start+1]);     
-        for(int i=start+2;i<end;i++){
-            int temp=curr;
-            curr=Math.max(curr,prev+nums[i]);
-            prev=temp;
+    private int robRange(int[] nums, int start, int end) {
+        int n = end - start + 1;
+        int[] dp = new int[n];
+        dp[0] = nums[start];
+        //if (n > 1) 
+            dp[1] = Math.max(nums[start], nums[start + 1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[start + i]);
         }
-        return curr;
+
+        return dp[n - 1];
     }
 }
