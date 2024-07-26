ced8b62c90e95e753ab0754ab6750d0996fc5c77
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-07-25 17:25:31 -0700

    Time: 74 ms (25.83%) | Memory: 75.9 MB (20.61%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS1(int[] nums) {
         int n=nums.length;
         int[][] dp=new int[n][n+1];
         for (int row[] : dp) {
@@ -22,16 +22,21 @@ class Solution {
         return dp[idx][prevIdx + 1];
     }
     
-    public int lengthOfLIS1(int[] nums) {
+    public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[][] dp=new int[n][n+1];
-        for (int row[] : dp) {
-            Arrays.fill(row, -1);
+        int[][] dp=new int[n+1][n+1];
+        
+        for (int i=n-1;i>=0;i--) {
+            for(int j=i-1;j>=-1;j--) {
+                int notTake = dp[i+1][j+1];
+                int take=0;
+                if(j == -1 || nums[i] > nums[j]) {
+                    take =  1 + dp[i+1][i+1];
+                }
+                dp[i] [j+1] = Math.max(take, notTake);
+            }
         }
         
-        return dp[n][n+1];
-        
+        return dp[0][-1+1];        
     }
-    
-    
 }
