51d542f69bd0d5fdae7cb355c8a25d8c314fd60a
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-07-25 17:00:33 -0700

    Time: 417 ms (5.99%) | Memory: 79.3 MB (7.38%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,53 +1,37 @@
 class Solution {
-    public int lengthOfLISBinarySearch(int[] nums) {
-        if(nums==null || nums.length==0)
-            return 0;
+    public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int length=1;
-        int[] effectiveArray=new int[n];
-        effectiveArray[0]=nums[0];
-        for(int i=1;i<n;i++){
-            if(nums[i]>effectiveArray[length-1]){
-                effectiveArray[length]=nums[i];
-                length++;
-            }
-            else{
-                int bsIdx=binarySearch(effectiveArray,0,length-1,nums[i]);
-                effectiveArray[bsIdx]=nums[i];
-            }
+        int[][] dp=new int[n][n+1];
+        for (int row[] : dp) {
+            Arrays.fill(row, -1);
         }
-        return length;
+        return helper(nums, 0, -1,dp);
+    }
+    private int helper(int[] nums, int idx, int prevIdx, int[][] dp){
+        if(idx==nums.length)
+            return 0;
+        if (dp[idx][prevIdx + 1] != -1) {
+            return dp[idx][prevIdx + 1];
+        }
+        int notTake=helper(nums,idx+1, prevIdx,dp);
+        int take=0;
+        if(prevIdx==-1 || nums[idx]>nums[prevIdx])
+            take=1+helper(nums,idx+1, idx,dp);
+        dp[idx][prevIdx + 1] =Math.max(take,notTake);
+        
+        return dp[idx][prevIdx + 1];
     }
     
-    private int binarySearch(int[] effectiveArr, int low, int high, int target){
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(effectiveArr[mid]==target)
-                return mid;
-            else if(effectiveArr[mid]>target)
-                high=mid-1;
-            else
-                low=mid+1;
-        }
-        return low;
-    }
-
-    public int lengthOfLIS(int[] nums) {
-        if(nums==null || nums.length==0)
-            return 0;
+    public int lengthOfLIS1(int[] nums) {
         int n=nums.length;
-        int maxLIS=1;
-        int[] dp=new int[n];
-        Arrays.fill(dp, 1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                int compareNumber=nums[i];
-                if(compareNumber>nums[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                    maxLIS=Math.max(maxLIS, dp[i]);
-                }
-            }
+        int[][] dp=new int[n][n+1];
+        for (int row[] : dp) {
+            Arrays.fill(row, -1);
         }
-        return maxLIS;
-     }
+        
+        return dp[n][n+1];
+        
+    }
+    
+    
 }
