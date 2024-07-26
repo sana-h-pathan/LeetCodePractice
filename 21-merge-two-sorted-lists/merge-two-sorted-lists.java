331e5ce9ab196435cf04336fc19ba0932dc0540b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-07-26 16:31:41 -0700

    Time: 0 ms (100.00%) | Memory: 42.5 MB (36.91%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -8,47 +8,31 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-/*class Solution1 {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1==null)
-            return l2;
-        if(l2==null)
-            return l1;
-
-        if(l1.val<l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        }
-        else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
-    }
-}
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1==null)
-            return l2;
-        if(l2==null)
-            return l1;
-
-        if(l1.val<l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+       ListNode l3=new ListNode(0);
+       ListNode dummy=l3;
+       while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                l3.next=list1;
+                list1=list1.next;
+                l3=l3.next;
+            }else{
+                l3.next=list2;
+                list2=list2.next;
+                l3=l3.next;
+            }
         }
-        else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
+        while(list1!=null){
+            l3.next=list1;
+            list1=list1.next;
+            l3=l3.next;
         }
+        while(list2!=null){
+            l3.next=list2;
+            list2=list2.next;
+            l3=l3.next;
+        }
+        return dummy.next;
     }
 }
