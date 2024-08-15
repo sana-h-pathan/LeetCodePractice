356d6e8ad9cf4539dcb8e37d2fc37cc9f5d37267
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-08-14 19:26:03 -0700

    Time: 8 ms (13.62%) | Memory: 52.1 MB (14.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,62 +1,31 @@
 class Solution {
-    int[][] dirs;
-    int m;
-    int n;
-    public int numIslandsBFS(char[][] grid) {
-        dirs=new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
-        m=grid.length;
-        n=grid[0].length;
-        int result=0;
-        Queue<int []> bfsQue=new LinkedList<>();
+    public int numIslands(char[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean[][] visited=new boolean[m][n];
+        Queue<int[]> bfsQue=new LinkedList<>();
+        int island=0;
+        int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                    result++;
+                if(grid[i][j]=='1' && !visited[i][j]){
                     bfsQue.add(new int[]{i,j});
-                    grid[i][j]='0';
+                    island++;
+                    visited[i][j]=true;
                     while(!bfsQue.isEmpty()){
                         int[] curr=bfsQue.poll();
-                        for(int[] dir: dirs){
-                            int nr=dir[0]+curr[0];
-                            int nc=dir[1]+curr[1];
-                            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1'){
-                                grid[nr][nc]=0;
-                                bfsQue.add(new int[] {nr,nc});
+                        for(int[] dir:dirs){
+                            int nr=curr[0]+dir[0];
+                            int nc=curr[1]+dir[1];
+                            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1'&&!visited[nr][nc]){
+                                bfsQue.add(new int[]{nr,nc});
+                                visited[nr][nc]=true;
                             }
                         }
                     }
                 }
             }
         }
-        return result;
-    }
-
-    public int numIslands(char[][] grid) {
-        dirs=new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
-        m=grid.length;
-        n=grid[0].length;
-        int count=0;
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                    count++;
-                    dfs(grid,i,j);
-                }
-            }
-        }
-        return count;
-    }
-    private void dfs(char[][] grid, int i, int j){
-        //base case
-        if(i<0 || j<0 || i==m || j==n || grid[i][j]=='0')
-            return;
-        //logic
-        grid[i][j]='0';
-        for(int [] dir: dirs){
-            int nr=i+dir[0];
-            int nc=j+dir[1];
-            dfs(grid, nr, nc);
-        }
+        return island;
     }
 }
