4dc6e0ff068fb27182d7b7998ef2b1eb0539998a
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-08-14 17:07:55 -0700

    Time: 0 ms (100.00%) | Memory: 41 MB (49.50%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSameTree1(TreeNode p, TreeNode q) {
         if(p==null && q==null)
             return true;
         if((p==null && q!=null)||(p!=null && q==null))
@@ -22,5 +22,35 @@ class Solution {
         if(p.val!=q.val)
             return false;
         return (isSameTree(p.left, q.left) && (isSameTree(p.right, q.right)));
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+            return true;
+        if((p==null && q!=null)||(p!=null && q==null))
+            return false;
+        Queue<TreeNode> pQue=new LinkedList<>();
+        Queue<TreeNode> qQue=new LinkedList<>();
+        pQue.add(p);
+        qQue.add(q);
+        while(!pQue.isEmpty() && !qQue.isEmpty()){
+            TreeNode pCurr=pQue.poll();
+            TreeNode qCurr=qQue.poll();
+            if(pCurr.val!=qCurr.val)
+                return false;
+            if((pCurr.left!=null && qCurr.left==null) || (pCurr.left==null && qCurr.left!=null))
+                return false;
+            if(pCurr.left!=null && qCurr.left!=null){
+                pQue.add(pCurr.left);
+                qQue.add(qCurr.left);
+            }
+            
+            if((pCurr.right!=null && qCurr.right==null) || (pCurr.right==null && qCurr.right!=null))
+                return false;
+            if(pCurr.right!=null && qCurr.right!=null){
+                pQue.add(pCurr.right);
+                qQue.add(qCurr.right);
+            }
+        }
+        return true;
     }
 }
