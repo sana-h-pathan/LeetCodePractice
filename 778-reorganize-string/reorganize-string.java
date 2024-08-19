1149261f3f78ec0ca277d07cb2351981964ab5db
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-08-18 23:29:20 -0700

    Time: 6 ms (31.27%) | Memory: 41.4 MB (70.79%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,39 +1,41 @@
 class Solution {
     public String reorganizeString(String s) {
-        HashMap<Integer, Integer> map=new HashMap<>();
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a, b) -> Integer.compare(b[1], a[1]));
-        int maxFrequency=0;
-        int n=s.length();
+        HashMap<Character,Integer> map=new HashMap<>();
+        int maxValue=0;
         for(char ch: s.toCharArray()){
-            int temp=ch-'a';
-            map.put(temp, map.getOrDefault(temp, 0)+1);
-            maxFrequency=Math.max(maxFrequency,map.get(temp));
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+            maxValue=Math.max(maxValue, map.get(ch));
         }
-        if(maxFrequency>(n+1)/2)
+        int n=s.length();
+        if(maxValue>(n+1)/2)
             return "";
-        for(int key: map.keySet()){
-            pq.offer(new int[]{key, map.get(key)});
+        PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
+        for(char key: map.keySet()){
+            pq.add(key);
         }
         StringBuilder sb=new StringBuilder();
         while(pq.size()>=2){
-            int[] first=pq.poll();
-            int[] second=pq.poll();
-            sb.append((char) (first[0] + 'a'));
-            sb.append((char) (second[0] + 'a'));
-            first[1]--;
-            if(first[1]>0)
-                pq.add(first);
-            second[1]--;
-            if(second[1]>0)
-                pq.add(second);
+            char fChar=pq.poll();
+            char sChar=pq.poll();
+            sb.append(fChar).append(sChar);
+            map.put(fChar, map.getOrDefault(fChar, 0)-1);
+            if(map.get(fChar)<=0)
+                map.remove(fChar);
+            else
+                pq.add(fChar);
+
+            map.put(sChar, map.getOrDefault(sChar, 0)-1);
+            if(map.get(sChar)<=0)
+                map.remove(sChar);
+            else
+                pq.add(sChar);
         }
         while(!pq.isEmpty()){
-            int [] curr=pq.poll();
-            if(curr[1]>1)
+            char ch=pq.poll();
+            if(map.get(ch)>1)
                 return "";
-            sb.append((char) (curr[0] + 'a'));
+            sb.append(ch);
         }
         return sb.toString();
-        
     }
 }
