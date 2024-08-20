a7e2eaaf4a7b1c0ff7ce1bfb0934dc419d10b7b8
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-08-20 08:21:11 -0700

    Time: 8 ms (13.68%) | Memory: 52.1 MB (14.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,30 +2,31 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        boolean[][] visited=new boolean[m][n];
+        int totalIsland=0;
+        int[][] dirs=new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
         Queue<int[]> bfsQue=new LinkedList<>();
-        int island=0;
-        int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        boolean[][] visited=new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
                     bfsQue.add(new int[]{i,j});
-                    island++;
+                    totalIsland++;
                     visited[i][j]=true;
                     while(!bfsQue.isEmpty()){
                         int[] curr=bfsQue.poll();
-                        for(int[] dir:dirs){
+                        for(int[] dir: dirs){
                             int nr=curr[0]+dir[0];
                             int nc=curr[1]+dir[1];
-                            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1'&&!visited[nr][nc]){
+                            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1' && !visited[nr][nc]){
                                 bfsQue.add(new int[]{nr,nc});
                                 visited[nr][nc]=true;
                             }
+                            
                         }
                     }
                 }
             }
         }
-        return island;
+        return totalIsland;
     }
 }
