1eaf095ab59a078f898c7154b8ebaa23b553f3cb
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-08-21 08:19:33 -0700

    Time: 0 ms (100.00%) | Memory: 44.5 MB (89.81%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -29,29 +29,25 @@ class Solution {
             return null;
         ListNode mid=findMiddle(head);
         TreeNode root=new TreeNode(mid.val);
-
         if(head==mid)
             return root;
+
         root.left=sortedListToBST(head);
         root.right=sortedListToBST(mid.next);
-
         return root;
-        
     }
 
-    private ListNode findMiddle(ListNode curr){
-        ListNode slow=curr;
-        ListNode fast=curr;
+    private ListNode findMiddle(ListNode head){
         ListNode prev=null;
+        ListNode slow=head;
+        ListNode fast=head;
         while(fast!=null && fast.next!=null){
             prev=slow;
             slow=slow.next;
             fast=fast.next.next;
-            
         }
         if(prev!=null)
             prev.next=null;
-        
         return slow;
     }
 }
