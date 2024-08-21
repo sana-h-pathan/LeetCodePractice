23204f7bc3a4976d3bd16663ed845c0e13d91c54
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-08-21 09:24:31 -0700

    Time: 5 ms (48.23%) | Memory: 44.4 MB (45.34%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,7 +1,7 @@
 class Solution {
     public String intToRoman(int num) {
         String[][] symbols=new String[][]{
-            {"I","1"},{"IV","4"},{"V","5"},{"IX","9"},{"X","10"},{"IX","9"},{"X","10"},{"XL","40"},
+            {"I","1"},{"IV","4"},{"V","5"},{"IX","9"},{"X","10"},{"XL","40"},
             {"L","50"},{"XC","90"},{"C","100"},{"CD","400"},{"D","500"},{"CM","900"},{"M","1000"}
         };
         StringBuilder sb=new StringBuilder();
