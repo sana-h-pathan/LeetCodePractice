558f14922e82228bbcb0f33bb13c16e4f41ec174
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-08-20 17:13:19 -0700

    Time: 37 ms (13.82%) | Memory: 44.4 MB (26.15%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,32 +1,25 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l=0;
-        int r=0;
         int maxFreq=0;
         int maxLength=0;
-        HashMap<Character, Integer> map=new HashMap<>();
+        int l=0;
+        int r=0;
+        HashMap<Character,Integer> map=new HashMap<>();
         while(r<s.length()){
-            char c=s.charAt(r);
-            map.put(c, map.getOrDefault(c, 0)+1);
-            maxFreq=Integer.max(maxFreq, map.get(c));
-           // int currLen=r-l+1;
-            if(r-l+1-maxFreq>k){
-                char ch=s.charAt(l);
-                map.put(ch, map.getOrDefault(ch, 0)-1);
+            char rChar=s.charAt(r);
+            map.put(rChar, map.getOrDefault(rChar, 0)+1);
+            maxFreq=Math.max(maxFreq, map.get(rChar));
+            while(r-l+1-maxFreq>k){
+                char lChar=s.charAt(l);
+                map.put(lChar, map.getOrDefault(lChar, 0)-1);
                 maxFreq=0;
-                for(Character chr: map.keySet()){
-                    maxFreq=Math.max(map.get(chr),maxFreq);
+                for(Character ch: map.keySet()){
+                    maxFreq=Math.max(maxFreq, map.get(ch));
                 }
                 l++;
             }
-            /*if (r - l + 1 - maxFreq > k) {
-                char ch = s.charAt(l);
-                map.put(ch, map.get(ch) - 1);
-                l++;
-            }*/
-            if(r-l+1-maxFreq<=k){
-                maxLength=Integer.max(maxLength, r-l+1);
-            }
+            int currLength=(r-l+1);
+            maxLength=Math.max(currLength, maxLength);
             r++;
         }
         return maxLength;
