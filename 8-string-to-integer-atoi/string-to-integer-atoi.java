c60177e082301aad8b5aae62bb0068c1db87633e
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2024-08-21 07:31:16 -0700

    Time: 1 ms (100.00%) | Memory: 42.2 MB (65.81%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,29 +1,30 @@
 class Solution {
     public int myAtoi(String s) {
-        int sign = 1;
-        int result = 0;
-        int index = 0;
-        int  n = s.length();
-        while (index < n && s.charAt(index) == ' ') { 
-            index++; 
-        }
-        if (index < n && s.charAt(index) == '+') {
-            sign = 1;
-            index++;
-        } else
-         if (index < n && s.charAt(index) == '-') {
-            sign = -1;
-            index++;
-        }
+        int idx=0;
         int limit=Integer.MAX_VALUE/10;
-        while (index < n && Character.isDigit(s.charAt(index))) {
-            int digit = s.charAt(index) - '0';
-            if (result > limit || (result == limit && digit > 7)) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            result = result * 10 + digit;
-            index++;           
+        int sign=1;
+        int number=0;
+        int n=s.length();
+
+        while(idx<n && s.charAt(idx)==' '){
+            idx++;
         }
-        return sign * result;
+        if(idx<n && s.charAt(idx)=='+'){
+            sign=1;
+            idx++;
+        }else if(idx<n && s.charAt(idx)=='-'){
+            sign=-1;
+            idx++;
+        }
+        while(idx<n && Character.isDigit(s.charAt(idx))){
+            int digit=s.charAt(idx)-'0';
+            if(number>limit||(number==limit && digit>7)){
+                return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+            }
+            number=number*10+digit;
+            idx++;
+        }
+
+        return number*sign;
     }
 }
