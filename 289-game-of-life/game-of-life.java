c6db6ff6bda2a796349d80d589f7a721086ef6bd
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-08 15:19:05 -0800

    Time: 0 ms (100.00%) | Memory: 43.2 MB (6.26%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,34 +1,35 @@
 class Solution {
-    int[][] dirs;
     int m;
     int n;
+    int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
+
     public void gameOfLife(int[][] board) {
-        this.dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
-        m=board.length;
-        n=board[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int countAlive=countAliveCells(board,i,j);
+        this.m=board.length;
+        this.n=board[0].length;
+        for (int i =0;i<m;i++){
+            for (int j=0; j<n;j++){
+                int countAlive = countAlive(i,j,board);
                 if(board[i][j]==1){
                     if(countAlive<2 || countAlive>3)
                         board[i][j]=10;
-                }else if(board[i][j]==0){
-                    if(countAlive==3)
+                } else if(board[i][j] == 0 ){
+                    if(countAlive == 3)
                         board[i][j]=11;
                 }
             }
         }
+        
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==10)
-                    board[i][j]=0;
-                else if(board[i][j]==11)
+            for (int j=0; j<n;j++){
+                if(board[i][j]==11)
                     board[i][j]=1;
+                else if(board[i][j]==10)
+                    board[i][j]=0;
             }
         }
     }
 
-    private int countAliveCells(int[][] board, int r, int c){
+    public int countAlive(int r, int c, int[][] board){
         int countAlive=0;
         for(int[] dir: dirs){
             int nr=dir[0]+r;
