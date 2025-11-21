43c4ca0e23f8f37fb4e761d96b7065f270fdc647
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-21 07:12:23 -0800

    Time: 14 ms (70.35%) | Memory: 47.2 MB (17.29%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,35 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        int count=0;
-        for(char c: t.toCharArray())
-            map.put(c, map.getOrDefault(c, 0)+1);
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+        int slow=0;
+        int fast=0;
         int reqdCount=t.length();
         int currCount=0;
-        int length=Integer.MAX_VALUE;
-        int l=0;
-        int r=0;
         int sIdx=-1;
-        while(r<s.length()){
-            char ch=s.charAt(r);
-            if(map.getOrDefault(ch,0)>0)
+        int minLength=Integer.MAX_VALUE;
+        while(fast<s.length()){
+            char fChar=s.charAt(fast);
+            if(map.getOrDefault(fChar,0)>0){
                 currCount++;
-            map.put(ch, map.getOrDefault(ch, 0)-1);
-            while(currCount==reqdCount){
-                if(r-l+1<length){
-                    length=r-l+1;
-                    sIdx=l;
-                }
-                char lChar=s.charAt(l);
-                map.put(lChar, map.getOrDefault(lChar, 0)+1);
-                if(map.get(lChar)>0)
-                    currCount--;
-                l++;
             }
-            r++;
+            map.put(fChar, map.getOrDefault(fChar,0)-1);
+            while(currCount==reqdCount){
+                if(fast-slow+1<minLength){
+                    minLength=fast-slow+1;
+                    sIdx=slow;
+                }
+                char lChar=s.charAt(slow);
+                map.put(lChar, map.getOrDefault(lChar,0)+1);
+                if(map.getOrDefault(lChar,0)>0){
+                    currCount--;
+                }
+                slow++;
+            }
+            fast++;
         }
         if(sIdx==-1)
             return "";
-        return s.substring(sIdx ,sIdx+length);
+        return s.substring(sIdx,sIdx+minLength);
     }
 }
