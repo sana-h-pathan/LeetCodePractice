c07cf43a8a9994a043cb8278675009dcb603764b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-20 23:08:53 -0800

    Time: 10 ms (41.47%) | Memory: 43.5 MB (55.02%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,36 +2,35 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length()>s2.length())
             return false;
+        int matches=0;
         int[] s1Freq=new int[26];
         int[] s2Freq=new int[26];
         for(int i=0;i<s1.length();i++){
-            int char1=s1.charAt(i)-'a';
-            int char2=s2.charAt(i)-'a';
-            s1Freq[char1]++;
-            s2Freq[char2]++;
+            char s1Char=s1.charAt(i);
+            char s2Char=s2.charAt(i);
+            s1Freq[s1Char-'a']++;
+            s2Freq[s2Char-'a']++;
         }
-        int matches=0;
         for(int i=0;i<26;i++){
             if(s1Freq[i]==s2Freq[i])
                 matches++;
         }
-        
         int l=0;
         int r=s1.length();
         while(r<s2.length()){
             if(matches==26)
                 return true;
-            int rVal=s2.charAt(r)-'a';
-            s2Freq[rVal]++;
-            if(s2Freq[rVal]==s1Freq[rVal])
+            int rChar=s2.charAt(r)-'a';
+            s2Freq[rChar]++;
+            if(s2Freq[rChar]==s1Freq[rChar])
                 matches++;
-            else if(s2Freq[rVal]==s1Freq[rVal]+1)
+            else if(s2Freq[rChar]-1==s1Freq[rChar])
                 matches--;
-             int lVal=s2.charAt(l)-'a';
-            s2Freq[lVal]--;
-            if(s2Freq[lVal]==s1Freq[lVal])
+            int lChar=s2.charAt(l)-'a';
+            s2Freq[lChar]--;
+            if(s2Freq[lChar]==s1Freq[lChar])
                 matches++;
-             else if(s2Freq[lVal]==s1Freq[lVal]-1)
+            else if(s2Freq[lChar]+1==s1Freq[lChar])
                 matches--;
             l++;
             r++;
