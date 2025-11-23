e69d866512aa9370a05e7326870fc92f7d27bb29
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-22 16:47:03 -0800

    Time: 14 ms (70.15%) | Memory: 47.1 MB (19.97%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,38 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> map=new HashMap<>();
+        HashMap<Character,Integer> map=new HashMap<>();
         for(int i=0;i<t.length();i++){
             char ch=t.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0)+1);
+            map.put(ch, map.getOrDefault(ch,0)+1);
         }
-        int reqdCount=t.length();
-        int currCount=0;
+        int reqdMatch=t.length();
+        int currMatch=0;
         int l=0;
         int r=0;
         int sIdx=-1;
-        int maxLength=Integer.MAX_VALUE;
+        int minLength=Integer.MAX_VALUE;
         while(r<s.length()){
             char rChar=s.charAt(r);
             if(map.getOrDefault(rChar,0)>0){
-                currCount++; 
+                currMatch++;
             }
-            map.put(rChar, map.getOrDefault(rChar, 0)-1);
-            while(currCount==reqdCount){
-                if(r-l+1<maxLength){
+            map.put(rChar, map.getOrDefault(rChar,0)-1);
+            while(currMatch==reqdMatch){
+                if(r-l+1<minLength){
+                    minLength=r-l+1;
                     sIdx=l;
-                    maxLength=r-l+1;
                 }
                 char lChar=s.charAt(l);
-                map.put(lChar, map.getOrDefault(lChar, 0)+1);
-                if(map.getOrDefault(lChar,0)>0)
-                      currCount--; 
+                map.put(lChar,map.getOrDefault(lChar,0)+1);
+                if(map.getOrDefault(lChar,0)>0){
+                    currMatch--;
+                }
                 l++;
             }
             r++;
         }
         if(sIdx==-1)
             return "";
-        return s.substring(sIdx, sIdx+maxLength);
-
+        return s.substring(sIdx, sIdx+minLength);
     }
 }
