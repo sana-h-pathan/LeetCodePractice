fbf4858885df6f57c82bf250e98f1e6768687beb
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-23 17:09:08 -0800

    Time: 6 ms (38.81%) | Memory: 46.1 MB (39.17%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,35 +1,35 @@
-public class Solution {
+class Solution {
     public int countComponents(int n, int[][] edges) {
-        List<List<Integer>> adj = new ArrayList<>();
-        boolean[] visit = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+        List<List<Integer>> adjacencyList=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adjacencyList.add(new ArrayList<>());
         }
-        for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+        for(int[] edge: edges){
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
         }
-
-        int res = 0;
-        for (int node = 0; node < n; node++) {
-            if (!visit[node]) {
-                bfs(adj, visit, node);
-                res++;
+        boolean[] visited=new boolean[n];
+        int countComponenent=0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                 visited[i]=true;
+                countComponenent++;
+                bfs(adjacencyList, i,visited);
             }
         }
-        return res;
+        return countComponenent;
     }
 
-    private void bfs(List<List<Integer>> adj, boolean[] visit, int node) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(node);
-        visit[node] = true;
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            for (int nei : adj.get(cur)) {
-                if (!visit[nei]) {
-                    visit[nei] = true;
-                    q.offer(nei);
+    private void bfs(List<List<Integer>> adjacencyList, int node, boolean[] visited){
+        Queue<Integer> bfsQue=new LinkedList<>();
+        bfsQue.add(node);
+        while(!bfsQue.isEmpty()){
+            int curr=bfsQue.poll();
+            List<Integer> adjList=adjacencyList.get(curr);
+            for(Integer adj: adjList){
+                if(!visited[adj]){
+                    visited[adj]=true;
+                    bfsQue.add(adj);
                 }
             }
         }
