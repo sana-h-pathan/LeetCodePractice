87027bc72e6527c4a5c7264a3cf8900bb82ccab1
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-23 16:18:32 -0800

    Time: 10 ms (16.13%) | Memory: 46.8 MB (33.07%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,33 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] indegree=new int[numCourses];
-        HashMap<Integer,List<Integer>> courseMap=new HashMap();
-        for(int[] pre: prerequisites){
-            int dependent=pre[0];
-            int independent=pre[1];
-            if(!courseMap.containsKey(independent))
-                courseMap.put(independent, new ArrayList<>());
-            courseMap.get(independent).add(dependent);
-            indegree[dependent]++;
-        }
+        HashMap<Integer, List<Integer>> map=new HashMap<>();
         int completedCourse=0;
+        for(int[] pre: prerequisites){
+            int ind=pre[1];
+            int dep=pre[0];
+            if(!map.containsKey(ind)){
+                map.put(ind, new ArrayList<>());
+            }
+            indegree[dep]++;
+            map.get(ind).add(dep);
+        }
         Queue<Integer> bfsQue=new LinkedList<>();
-        for(int i=0;i<numCourses;i++){
+        for(int i=0;i<indegree.length;i++){
             if(indegree[i]==0){
                 bfsQue.add(i);
                 completedCourse++;
             }
         }
-        
         while(!bfsQue.isEmpty()){
-            int course=bfsQue.poll();
-            if(courseMap.containsKey(course)){
-                List<Integer> dependentCourse=courseMap.get(course);
-                for(int dc: dependentCourse){
-                    indegree[dc]--;
-                    if(indegree[dc]==0){
-                        bfsQue.add(dc);
+            int curr=bfsQue.poll();
+            if(map.containsKey(curr)){
+                List<Integer> depCourse=map.get(curr);
+                for(int course: depCourse){
+                    indegree[course]--;
+                    if(indegree[course]==0){
+                        bfsQue.add(course);
                         completedCourse++;
+                        if(completedCourse==numCourses)
+                            return true;
                     }
                 }
             }
