554cacb09a590b6cec0906bee294d7918c6c2eea
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-25 22:59:16 -0800

    Time: 15 ms (68.90%) | Memory: 47.1 MB (23.94%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,38 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character,Integer> map=new HashMap<>();
+        if(t.length()>s.length())
+            return "";
+        HashMap<Character, Integer> map=new HashMap<>();
         for(int i=0;i<t.length();i++){
             char ch=t.charAt(i);
             map.put(ch, map.getOrDefault(ch,0)+1);
         }
-        int reqdMatch=t.length();
         int currMatch=0;
+        int reqdMatch=t.length();
+        int maxLength=Integer.MAX_VALUE;
+        int sIdx=-1;
         int l=0;
         int r=0;
-        int sIdx=-1;
-        int minLength=Integer.MAX_VALUE;
         while(r<s.length()){
             char rChar=s.charAt(r);
-            if(map.getOrDefault(rChar,0)>0){
-                currMatch++;
-            }
-            map.put(rChar, map.getOrDefault(rChar,0)-1);
+            if(map.getOrDefault(rChar,0)>0)
+               currMatch++;
+            map.put(rChar,map.getOrDefault(rChar,0)-1);
             while(currMatch==reqdMatch){
-                if(r-l+1<minLength){
-                    minLength=r-l+1;
+                if(r-l+1<maxLength){
                     sIdx=l;
+                    maxLength=r-l+1;
                 }
                 char lChar=s.charAt(l);
                 map.put(lChar,map.getOrDefault(lChar,0)+1);
-                if(map.getOrDefault(lChar,0)>0){
+                if(map.getOrDefault(lChar,0)>0)
                     currMatch--;
-                }
                 l++;
             }
             r++;
         }
         if(sIdx==-1)
             return "";
-        return s.substring(sIdx, sIdx+minLength);
+        return s.substring(sIdx, sIdx+maxLength);
     }
 }
