4dc3d4c7c807c5565fa28e97c0f88289938e197f
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-27 12:23:47 -0800

    Time: 34 ms (33.21%) | Memory: 47.1 MB (15.06%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result=new ArrayList<>();
-        HashMap<Character, Integer> map=new HashMap<>();
         int l=0;
         int r=0;
-        int matches=0;
+        HashMap<Character, Integer> map=new HashMap<>();
         for(int i=0;i<p.length();i++){
             char pChar=p.charAt(i);
-            map.put(pChar, map.getOrDefault(pChar,0)+1);
+            map.put(pChar, map.getOrDefault(pChar, 0)+1);
         }
+        int matches=0;
         while(r<s.length()){
             char rChar=s.charAt(r);
             if(map.containsKey(rChar)){
-                map.put(rChar, map.getOrDefault(rChar,0)-1);
+                map.put(rChar, map.getOrDefault(rChar, 0)-1);
                 if(map.get(rChar)==0)
                     matches++;
             }
             if(r>=p.length()){
                 char lChar=s.charAt(l);
                 if(map.containsKey(lChar)){
-                    map.put(lChar, map.getOrDefault(lChar,0)+1);
+                    map.put(lChar,map.getOrDefault(lChar,0)+1);
                     if(map.get(lChar)==1)
                         matches--;
                 }
