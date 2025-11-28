df75908398d9d01c45d3a58ccb3456aeae23e3a7
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-27 20:02:16 -0800

    Time: 303 ms (7.84%) | Memory: 46.6 MB (7.47%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,34 +1,38 @@
 class Solution {
     int m;
     int n;
-    int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
     public boolean exist(char[][] board, String word) {
         this.m=board.length;
         this.n=board[0].length;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(backtrack(board, i, j, 0, word))
+                if(backtrack(board, word, i, j,0))
                     return true;
             }
         }
-        return false; 
+        return false;
     }
-    private boolean backtrack(char[][] board, int i, int j, int idx, String word){
+    private boolean backtrack(char[][] board, String word, int i, int j, int idx){
+        int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         //base case
         if(word.length()==idx)
             return true;
-        if(i<0 || j<0 || i==m || j==n ||board[i][j]=='#')
-            return false;
+        
+
         //logic
         if(board[i][j]==word.charAt(idx)){
+            if (idx == word.length() - 1) {
+                return true;
+            }
             board[i][j]='#';
             for(int[] dir: dirs){
-                int nr=i+dir[0];
-                int nc=j+dir[1];
-                if(backtrack(board, nr, nc, idx+1, word))
-                    return true;
+                int nr=dir[0]+i;
+                int nc=dir[1]+j;
+                if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]!='#'){
+                    if(backtrack(board, word, nr, nc, idx+1))
+                        return true;
+                }
             }
-            //backtrack
             board[i][j]=word.charAt(idx);
         }
         return false;
