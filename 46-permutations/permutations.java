d79ce90f1d222267b8df56b0d8ae6224ee463af6
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-27 18:12:02 -0800

    Time: 2 ms (38.04%) | Memory: 45.8 MB (6.18%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,20 +1,23 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result=new ArrayList<>();
-        backtrack(nums,result, new HashSet<>(),new ArrayList<>());
+        backtrack(nums, result, new ArrayList<>(), new HashSet<>());
         return result;
     }
-    private void backtrack(int[] nums, List<List<Integer>> result, HashSet<Integer> set,ArrayList<Integer> temp){
+    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> temp, HashSet<Integer> set){
+        //base case
         if(set.size()==nums.length){
             result.add(new ArrayList<>(temp));
             return;
         }
+
+        //for loop
         for(int i=0;i<nums.length;i++){
             if(set.contains(nums[i]))
                 continue;
             set.add(nums[i]);
             temp.add(nums[i]);
-            backtrack(nums, result, set, temp);
+            backtrack(nums, result, temp, set);
             set.remove(nums[i]);
             temp.remove(temp.size()-1);
         }
