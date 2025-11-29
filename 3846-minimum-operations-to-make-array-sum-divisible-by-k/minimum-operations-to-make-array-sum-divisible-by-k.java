baf904e2d3e789917ed2d53817660951311b0a00
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-28 16:57:43 -0800

    Time: 1 ms (92.46%) | Memory: 46.6 MB (38.62%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.java
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.java
@@ -1,10 +1,8 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
         int sum=0;
-        int max=0;
         for(int num:nums){
             sum+=num;
-            max=Integer.max(max, num);
         }
         int remainder=sum%k;
         return remainder;
