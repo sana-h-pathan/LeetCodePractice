55587bda07a0d8658f14220f1ab74e0a8cde4a6a
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-11-30 11:49:39 -0800

    Time: 94 ms (99.52%) | Memory: 123.7 MB (5.59%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -0,0 +1,44 @@
+class Bank {
+    long[] balance;
+
+    public Bank(long[] balance) {
+        this.balance=balance;
+    }
+    
+    public boolean transfer(int account1, int account2, long money) {
+        if(balance.length<account1-1 || balance.length<account2-1)
+            return false;
+        if(balance[account1-1]<money)
+            return false;
+        else{
+            balance[account1-1]-=money;
+            balance[account2-1]+=money;
+            return true;
+        }
+        
+    }
+    
+    public boolean deposit(int account, long money) {
+        if(balance.length<account-1)
+            return false;
+        balance[account-1]+=money;
+        return true;
+        
+    }
+    
+    public boolean withdraw(int account, long money) {
+        if(balance.length<account-1 || balance[account-1]<money)
+            return false;
+        balance[account-1]-=money;
+        return true;
+        
+    }
+}
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
