15c24683fde67393713c45b15ed7e4877a0a2085
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-02 11:47:30 -0800

    Time: 14 ms (69.13%) | Memory: 47.2 MB (26.96%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -2,32 +2,31 @@ class Solution {
     public String minWindow(String s, String t) {
         if(t.length()>s.length())
             return "";
-        HashMap<Character, Integer> map=new HashMap<>();
+        HashMap<Character,Integer> map=new HashMap<>();
         int l=0;
         int r=0;
-        int sIdx=-1;
-        int minLength=Integer.MAX_VALUE;
-        int currMatch=0;
         for(int i=0;i<t.length();i++){
             char tChar=t.charAt(i);
-            map.put(tChar, map.getOrDefault(tChar,0)+1);
+            map.put(tChar, map.getOrDefault(tChar, 0)+1);
         }
-        int reqdMatch=t.length();
+        int currMatches=0;
+        int reqdMatches=t.length();
+        int sIdx=-1;
+        int minLength=Integer.MAX_VALUE;
         while(r<s.length()){
             char rChar=s.charAt(r);
-            if(map.getOrDefault(rChar, 0)>0){
-                currMatch++;
-            }
+            if(map.getOrDefault(rChar,0)>0)
+                currMatches++;
             map.put(rChar, map.getOrDefault(rChar,0)-1);
-            while(currMatch==reqdMatch){
-                char lChar=s.charAt(l);
-                map.put(lChar, map.getOrDefault(lChar,0)+1);
-                if(map.getOrDefault(lChar,0)>0){
-                    currMatch--;
-                }
+            while(currMatches==reqdMatches){
                 if(minLength>r-l+1){
-                    minLength=r-l+1;
                     sIdx=l;
+                    minLength=r-l+1;
+                }
+                char lChar=s.charAt(l);
+                map.put(lChar, map.getOrDefault(lChar, 0)+1);
+                if(map.getOrDefault(lChar,0)>0){
+                    currMatches--;
                 }
                 l++;
             }
