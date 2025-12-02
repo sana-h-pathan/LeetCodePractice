1d6ae144bea08527879bb80499aa693406fe621a
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-02 13:56:19 -0800

    Time: 64 ms (81.79%) | Memory: 47.8 MB (54.10%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,30 +1,30 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         HashSet<String> set = new HashSet<>();
-        for(String word: wordList){
-            set.add(word);
-        }
-        if(!set.contains(endWord)){
+        for(String w: wordList)
+            set.add(w);
+        if(!set.contains(endWord))
             return 0;
-        }
+        int count=0;
         Queue<String> bfsQue=new LinkedList<>();
-        int steps=0;
         bfsQue.add(beginWord);
+        
         while(!bfsQue.isEmpty()){
             int size=bfsQue.size();
-            steps++;
+            count++;
             for(int i=0;i<size;i++){
-                String curr=bfsQue.poll();
+                String curr = bfsQue.poll();
                 for(int j=0;j<curr.length();j++){
                     char[] currChar=curr.toCharArray();
                     for(char ch='a';ch<='z';ch++){
                         currChar[j]=ch;
-                        String currWord=new String(currChar);
-                        if(set.contains(currWord)){
-                            if(currWord.equals(endWord))
-                                return steps+1;
-                            bfsQue.add(currWord);
-                            set.remove(currWord);
+                        String newCurr=new String(currChar);
+                        if(set.contains(newCurr)){
+                            if(newCurr.equals(endWord)){
+                                return count+1;
+                            }
+                            bfsQue.add(newCurr);
+                            set.remove(newCurr);
                         }
                     }
                 }
