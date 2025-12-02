c6dca5b7e7265af2b5906cd7644f7d6751800a02
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-02 08:36:00 -0800

    Time: 1 ms (84.80%) | Memory: 42.7 MB (46.07%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -3,12 +3,16 @@ class Solution {
         int n=s.length();
         int[] dp=new int[n+1];
         dp[n]=1;
-        for(int i=n-1;i>=0;i--)
-            if(s.charAt(i)!='0') {
+        for(int i=n-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(ch!='0'){
                 dp[i]=dp[i+1];
-                if(i<n-1 && (s.charAt(i) == '1' || (s.charAt(i)=='2' && s.charAt(i+1)<'7')))
+                if(i<n-1 && ((ch=='1') || (ch=='2'&& s.charAt(i+1)<'7'))){
+
                     dp[i]+=dp[i+2];
+                }
             }
+        }
         return dp[0];
     }
 }
