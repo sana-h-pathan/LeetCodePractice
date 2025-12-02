ce91241871270a8295322a733d9c1df4821461be
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-02 14:05:15 -0800

    Time: 162 ms (32.76%) | Memory: 43.1 MB (30.92%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,32 +1,31 @@
 class Solution {
     int m;
     int n;
+    int[][] dirs;
     public boolean exist(char[][] board, String word) {
         this.m=board.length;
         this.n=board[0].length;
+        this.dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(backtrack(board, word, i, j,0))
+                if(backtrack(board, i, j, 0, word))
                     return true;
             }
         }
         return false;
     }
-    private boolean backtrack(char[][] board, String word, int i, int j, int idx){
-        int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        //base case
+    private boolean backtrack(char[][] board, int i, int j, int idx, String word){
        
-        //logic
-        if(board[i][j]==word.charAt(idx)){
-            if (idx == word.length() - 1) {
+        
+        if(word.charAt(idx)==board[i][j]){
+            if(idx==word.length()-1)
                 return true;
-            }
             board[i][j]='#';
             for(int[] dir: dirs){
                 int nr=dir[0]+i;
                 int nc=dir[1]+j;
                 if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]!='#'){
-                    if(backtrack(board, word, nr, nc, idx+1))
+                    if(backtrack(board,nr,nc,idx+1, word))
                         return true;
                 }
             }
