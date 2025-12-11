0c42acee10d06c779a15ced33d47d205364bc250
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-11 13:06:04 -0800

    Time: 43 ms (89.65%) | Memory: 131.3 MB (39.55%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -2,31 +2,31 @@ class LRUCache {
     class Node{
         int key;
         int value;
-        Node prev;
         Node next;
+        Node prev;
         public Node(int key, int value){
             this.key=key;
             this.value=value;
         }
     }
+    int capacity;
+    HashMap<Integer, Node> map;
     Node head;
     Node tail;
-    HashMap<Integer, Node> map;
-    int capacity;
 
     public LRUCache(int capacity) {
         this.capacity=capacity;
         this.map=new HashMap<>();
-        this.head=new Node(-1,-1);
-        this.tail=new Node(-1,-1);
-        this.head.next=tail;
-        this.tail.prev=head;
+        this.head = new Node(-1,-1);
+        this.tail = new Node(-1,-1);
+        this.head.next = tail;
+        this.tail.prev = head;
     }
     
     public int get(int key) {
         if(!map.containsKey(key))
             return -1;
-        Node curr=map.get(key);
+        Node curr = map.get(key);
         removeNode(curr);
         addToHead(curr);
         return curr.value;
@@ -34,34 +34,34 @@ class LRUCache {
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-            Node curr=map.get(key);
-            removeNode(curr);
+            Node curr = map.get(key);
             curr.value=value;
+            removeNode(curr);
             addToHead(curr);
-        } else{
+        } else {
             if(map.size()==capacity){
-                Node remove=tail.prev;
+                Node remove = tail.prev;
                 removeNode(remove);
                 map.remove(remove.key);
             }
-            Node newNode=new Node(key,value);
+            Node newNode = new Node(key, value);
             addToHead(newNode);
             map.put(key, newNode);
         }
-        
     }
 
     private void removeNode(Node curr){
-        curr.next.prev=curr.prev;
-        curr.prev.next=curr.next;
+        //1-2-3-4
+        //1-3-4
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
     }
-    private void addToHead(Node curr){
-        //head->1->2->3->tail
-        //head->3->1->2->tail
-        Node temp=head.next;
+
+    public void addToHead(Node curr){
+        Node temp = head.next;
         head.next=curr;
-        curr.next=temp;
         curr.prev=head;
+        curr.next=temp;
         curr.next.prev=curr;
     }
 }
