40c7b18e6906205c51615ed85d4a64a7b6c2b115
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-15 18:24:37 -0800

    Time: 0 ms (100.00%) | Memory: 43.2 MB (21.25%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -15,30 +15,35 @@
  */
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
-        
-        Queue<TreeNode> bfsQue=new LinkedList<>();
+        Queue<TreeNode> bfsQue = new LinkedList<>();
         bfsQue.add(root);
         while(!bfsQue.isEmpty()){
-            int size=bfsQue.size();
             boolean xFound=false;
             boolean yFound=false;
+            int size = bfsQue.size();
             for(int i=0;i<size;i++){
-                TreeNode curr=bfsQue.poll();
-                if(curr.val==x)
+                TreeNode curr = bfsQue.poll();
+                if(curr.val==x){
                     xFound=true;
-                if(curr.val==y)
-                    yFound=true;
-                if(curr.left!=null && curr.right!=null){
-                    if(curr.left.val==x && curr.right.val==y)
-                    return false;
-                    if(curr.left.val==y && curr.right.val==x)
-                    return false;
                 }
-                if(curr.left!=null)
+                if(curr.val==y){
+                    yFound=true;
+                }
+                if(curr.left!=null && curr.right!=null){
+                    if(curr.left.val==x && curr.right.val==y){
+                        return false;
+                    }
+                    if(curr.left.val==y && curr.right.val==x){
+                        return false;
+                    }
+                }
+                if(curr.left!=null){
                     bfsQue.add(curr.left);
-                if(curr.right!=null)
+                }
+                if(curr.right!=null){
                     bfsQue.add(curr.right);
-            }    
+                }
+            }
             if(xFound && yFound)
                 return true;
             if(xFound || yFound)
