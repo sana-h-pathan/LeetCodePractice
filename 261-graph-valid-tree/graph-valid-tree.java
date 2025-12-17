1c4a0e831f43b06ded16a03d3392a78d4743d881
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-17 10:51:04 -0800

    Time: 6 ms (23.41%) | Memory: 46.4 MB (35.96%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int[] edge: edges){
+            int e1 = edge[0];
+            int e2 = edge[1];
+            adjList.get(e1).add(e2);
+            adjList.get(e2).add(e1);
+        }
+        boolean[] visited = new boolean[n];
+        Queue<int[]> bfsQue = new LinkedList<>();
+        bfsQue.add(new int[]{0, -1});
+        visited[0]=true;
+        while(!bfsQue.isEmpty()){
+            int[] curr = bfsQue.poll();
+            int v = curr[0];
+            int parent = curr[1];
+            for(int ne: adjList.get(v)){
+                if(ne==parent)
+                    continue;
+                if(visited[ne]){
+                    return false;
+                }
+                bfsQue.add(new int[]{ne, v});
+                visited[ne]=true;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(!visited[i])
+                return false;
+        }
+        return true;
+    }
+}
