6d51e5416a4fbd48be9e26ff40ebfddad119a28a
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-18 16:40:43 -0800

    Time: 7 ms (67.91%) | Memory: 44.1 MB (43.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,32 +2,34 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length()>s2.length())
             return false;
-        int[] s1Freq=new int[26];
-        int[] s2Freq=new int[26];
-        int matches=0;
+        int[] s1Freq = new int[26];
+        int[] s2Freq = new int[26];
         for(int i=0;i<s1.length();i++){
-            char s1Char=s1.charAt(i);
-            char s2Char=s2.charAt(i);
-            s1Freq[s1Char-'a']++;
-            s2Freq[s2Char-'a']++;
+            int s1Char = s1.charAt(i) - 'a';
+            int s2Char = s2.charAt(i) - 'a';
+            s1Freq[s1Char]++;
+            s2Freq[s2Char]++;
         }
+        int matches = 0;
         for(int i=0;i<26;i++){
-            if(s1Freq[i]==s2Freq[i])
+            if(s1Freq[i]==s2Freq[i]){
                 matches++;
+            }
         }
         int l=0;
         int r=s1.length();
         while(r<s2.length()){
             if(matches==26)
                 return true;
-            int rInt=s2.charAt(r)-'a';
+            int rInt = s2.charAt(r)-'a';
             s2Freq[rInt]++;
-            if(s1Freq[rInt]==s2Freq[rInt]){
+            if(s1Freq[rInt] == s2Freq[rInt]){
                 matches++;
-            } else if(s1Freq[rInt]==s2Freq[rInt]-1){
+            } 
+            else if(s1Freq[rInt]==s2Freq[rInt]-1){
                 matches--;
             }
-            int lInt=s2.charAt(l)-'a';
+            int lInt = s2.charAt(l) - 'a';
             s2Freq[lInt]--;
             if(s1Freq[lInt]==s2Freq[lInt]){
                 matches++;
