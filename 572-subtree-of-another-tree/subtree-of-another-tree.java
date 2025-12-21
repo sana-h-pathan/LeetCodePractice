071bb031997e26e3221cb8adb5214cca09f71ec5
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-20 19:55:11 -0800

    Time: 4 ms (18.34%) | Memory: 46.4 MB (36.01%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -1,27 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-        Queue<TreeNode> que = new LinkedList<TreeNode>();
-        que.add(s);
-        while(!que.isEmpty()) { // bfs
-            TreeNode node = que.poll();
-            if(sameTree(node, t)) 
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null && subRoot==null)
+            return true;
+        Queue<TreeNode> bfsQue = new LinkedList<>();
+        bfsQue.add(root);
+        while(!bfsQue.isEmpty()){
+            TreeNode curr = bfsQue.poll();
+            if(isSameTree(curr, subRoot))
                 return true;
-            if(node.left != null) 
-                que.add(node.left);
-            if(node.right != null) 
-                que.add(node.right);
+            if(curr.left!=null)
+                bfsQue.add(curr.left);
+            if(curr.right!=null)
+                bfsQue.add(curr.right);
         }
         return false;
     }
-    
-    private boolean sameTree(TreeNode s, TreeNode t) {
-        if(s == null && t == null) 
+    private boolean isSameTree(TreeNode p, TreeNode q){
+        if(p==null && q==null)
             return true;
-        if((s == null && t != null) || (s != null && t == null)) 
+        if((p!=null && q==null) || (p==null && q!=null))
             return false;
-        if(s.val != t.val) 
+        if(p.val!=q.val)
             return false;
-        
-        return sameTree(s.left, t.left) && sameTree(s.right, t.right);  
-    } 
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 }
