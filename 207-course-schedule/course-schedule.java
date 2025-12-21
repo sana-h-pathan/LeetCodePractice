b98559d5db2f43db7d5e388a945ab11898c27f22
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-21 12:23:31 -0800

    Time: 10 ms (17.57%) | Memory: 47 MB (51.66%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
         HashMap<Integer, List<Integer>> map = new HashMap<>();
-        int completedCourses=0;
-        Queue<Integer> bfsQue=new LinkedList<>();
+        Queue<Integer> bfsQue = new LinkedList<>();
         for(int[] pre: prerequisites){
             int ind = pre[1];
             int dep = pre[0];
@@ -13,26 +12,27 @@ class Solution {
             map.get(ind).add(dep);
             indegree[dep]++;
         }
+        int completedCourse = 0;
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
+                completedCourse++;
                 bfsQue.add(i);
-                completedCourses++;
             }
         }
-        while(!bfsQue.isEmpty()){
+        if(numCourses==completedCourse)
+            return true;
+        while (!bfsQue.isEmpty()) {
             int curr = bfsQue.poll();
             if(map.containsKey(curr)){
-                for(int depCourse: map.get(curr)){
-                    indegree[depCourse]--;
-                    if(indegree[depCourse]==0){
-                        completedCourses++;
-                        if(completedCourses==numCourses)
-                            return true;
-                        bfsQue.add(depCourse);
+                for(int dep: map.get(curr)){
+                    indegree[dep]--;
+                    if(indegree[dep]==0){
+                        completedCourse++;
+                        bfsQue.add(dep);
                     }
                 }
             }
         }
-        return completedCourses==numCourses;
+        return completedCourse==numCourses;
     }
 }
