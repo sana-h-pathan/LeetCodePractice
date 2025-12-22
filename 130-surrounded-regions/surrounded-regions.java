f41b8d95253b4d45c64d4195809b81e739dff2b6
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-21 22:10:16 -0800

    Time: 1 ms (99.79%) | Memory: 48 MB (15.70%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,49 +1,41 @@
 class Solution {
+    int[][] dirs;
     int m;
     int n;
-    int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
     public void solve(char[][] board) {
-        if(board.length==0 || board[0].length==0)
-            return;
         this.m = board.length;
         this.n = board[0].length;
+        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         if(m<2 || n<2)
             return;
-        for(int i=0;i<m;i++){
-            if(board[i][0]=='O')
-                dfs(board, i, 0);
-            if(board[i][n-1]=='O')
-                dfs(board, i, n-1);
+        for(int i=0;i<m;i++){ //run for all rows but 1st and last column 
+            helper(board,i,0);
+            helper(board, i, n-1);
         }
-
-        for(int j=0;j<n;j++){
-            if(board[0][j]=='O')
-                dfs(board, 0, j);
-            if(board[m-1][j]=='O')
-                dfs(board, m-1, j);
+        for(int j=0;j<n;j++){ //run for all columns but 1st and last row 
+            helper(board, 0, j);
+            helper(board, m-1, j);
         }
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]=='O')
+                if(board[i][j]=='O'){
                     board[i][j]='X';
-                else if(board[i][j]=='*')
+                } else if(board[i][j]=='0'){
                     board[i][j]='O';
+                }
             }
         }
     }
-
-    private void dfs(char[][] board, int i, int j){
-        if(i<0 || j<0 || i>m-1 || j>n-1)
+    private void helper(char[][] board, int r, int c){
+        if(r<0 || c<0 || r>m-1|| c>n-1)
             return;
-        if(board[i][j]!='O')
-            return;
-        if(board[i][j]=='O')
-            board[i][j] = '*';
-        for(int[] dir: dirs){
-            int nr = dir[0]+i;
-            int nc = dir[1]+j;
-            dfs(board, nr, nc);
+        if(board[r][c]=='O'){
+            board[r][c] = '0';
+            for(int[] dir: dirs){
+                int nr = dir[0]+r;
+                int nc = dir[1]+c;
+                helper(board, nr, nc);
+            }
         }
     }
 }
