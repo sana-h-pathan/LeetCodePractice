ab22931a06b61257ee367fd6239a9e66f44bbeff
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 22:29:49 -0800

    Time: 0 ms (100.00%) | Memory: 46.8 MB (16.54%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -14,34 +14,21 @@
  * }
  */
 class Solution {
-    HashMap<TreeNode, Integer> robResult = new HashMap<>();
-    HashMap<TreeNode, Integer> notRobResult = new HashMap<>();
     public int rob(TreeNode root) {
-        return helper(root, false);
+        int[] result = dfs(root); // res[0]=rob, res[1]=skip
+        return Math.max(result[0], result[1]);
     }
 
-    public int helper(TreeNode node, boolean parentRobbed) {
-        if (node == null) {
-            return 0;
-        }
-        if (parentRobbed) {
-            if (robResult.containsKey(node)) {
-                return robResult.get(node);
-            }
-            int result = helper(node.left, false) + helper(node.right, false);
-            robResult.put(node, result);
-            return result;
-        } else {
-            if (notRobResult.containsKey(node)) {
-                return notRobResult.get(node);
-            }
-            int rob = node.val + helper(node.left, true) + helper(node.right, true);
-            int notRob = helper(node.left, false) + helper(node.right, false);
-            int result = Math.max(rob, notRob);
-            notRobResult.put(node, result);
-            return result;
-        }
-    }
+    private int[] dfs(TreeNode node) {
+        if (node == null) 
+        return new int[]{0, 0};
 
-    
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+
+        int rob = node.val + left[1] + right[1];
+        int skip = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return new int[]{rob, skip};
+    }
 }
