cecc9a9b070af3c5d5b2cb73aca543f0937e92f2
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 19:01:39 -0800

    Time: 1 ms (97.23%) | Memory: 46.7 MB (25.17%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -14,25 +14,24 @@
  * }
  */
 class Solution {
-    HashSet<Integer> toDelete;
-    List<TreeNode> result;
+    
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        this.toDelete=new HashSet<>();
-        this.result=new ArrayList<>();
+        HashSet<Integer> toDelete =new HashSet<>();
+        List<TreeNode> result=new ArrayList<>();
         for(int num: to_delete)
             toDelete.add(num);
-        removeNodes(root);
+        removeNodes(root, toDelete, result);
         if(!toDelete.contains(root.val))
             result.add(root);
 
         return result;
     }
-    private TreeNode removeNodes(TreeNode root){
+    private TreeNode removeNodes(TreeNode root, HashSet<Integer> toDelete, List<TreeNode> result){
         if(root==null)
             return null;
         
-        root.left=removeNodes(root.left);
-        root.right=removeNodes(root.right);
+        root.left=removeNodes(root.left, toDelete, result);
+        root.right=removeNodes(root.right, toDelete, result);
         if(toDelete.contains(root.val)){
             if(root.left!=null)
                 result.add(root.left);
