09407b9b0f4e2c49007e54983c1161a913617ea4
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 20:59:31 -0800

    Time: 65 ms (81.10%) | Memory: 47.6 MB (76.63%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,30 +1,31 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         HashSet<String> set = new HashSet<>();
-        for(String w: wordList)
-            set.add(w);
-        if(!set.contains(endWord))
+        for(String word: wordList){
+            set.add(word);
+        }
+        if(!set.contains(endWord)){
             return 0;
+        }
+        Queue<String> bfsQueue = new LinkedList<>();
+        bfsQueue.add(beginWord);
         int count=0;
-        Queue<String> bfsQue=new LinkedList<>();
-        bfsQue.add(beginWord);
-        
-        while(!bfsQue.isEmpty()){
-            int size=bfsQue.size();
+        while(!bfsQueue.isEmpty()){
+            int size= bfsQueue.size();
             count++;
             for(int i=0;i<size;i++){
-                String curr = bfsQue.poll();
-                for(int j=0;j<curr.length();j++){
-                    char[] currChar=curr.toCharArray();
-                    for(char ch='a';ch<='z';ch++){
+                String currWord = bfsQueue.poll();
+                for(int j=0;j<currWord.length();j++){
+                    char[] currChar = currWord.toCharArray();
+                    for(char ch ='a';ch<='z';ch++){
                         currChar[j]=ch;
-                        String newCurr=new String(currChar);
-                        if(set.contains(newCurr)){
-                            if(newCurr.equals(endWord)){
+                        String newString = new String(currChar);
+                        if(set.contains(newString)){
+                            set.remove(newString);
+                            bfsQueue.add(newString);
+                            if(newString.equals(endWord)){
                                 return count+1;
                             }
-                            bfsQue.add(newCurr);
-                            set.remove(newCurr);
                         }
                     }
                 }
