6d496ed9f998591013323858fa866a5033562e00
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 23:21:12 -0800

    Time: 1 ms (45.59%) | Memory: 46.4 MB (71.49%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -15,20 +15,18 @@
  */
 class Solution {
     public int rob(TreeNode root) {
-        int[] result = dfs(root); // res[0]=rob, res[1]=skip
-        return Math.max(result[0], result[1]);
+        int[] result = helper(root);
+        return Math.max(result[0],result[1]);
     }
+    private int[] helper(TreeNode root){
+        if(root==null)
+            return new int[]{0,0};
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
 
-    private int[] dfs(TreeNode node) {
-        if (node == null) 
-        return new int[]{0, 0};
+        int rob = root.val+left[1]+right[1];
+        int notRob = Math.max(left[0], left[1])+Math.max(right[0], right[1]);
 
-        int[] left = dfs(node.left);
-        int[] right = dfs(node.right);
-
-        int rob = node.val + left[1] + right[1];
-        int skip = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-
-        return new int[]{rob, skip};
+        return new int[]{rob, notRob};
     }
 }
