e3098501a77b44c7a08c2aa1ce7eef7268e89c76
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 18:09:33 -0800

    Time: 105 ms (44.09%) | Memory: 111.9 MB (59.28%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -22,54 +22,54 @@ class Solution {
         return findMaxDistance(map, target);
     }
 
-    public void findParent(TreeNode root, int start, Map<TreeNode, TreeNode> parent) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
+    public void findParent(TreeNode root, int start, Map<TreeNode, TreeNode> map) {
+        Queue<TreeNode> bfsQue = new LinkedList<>();
+        bfsQue.add(root);
+        while (!bfsQue.isEmpty()) {
+            TreeNode node = bfsQue.poll();
             if (node.val == start) 
                 target = node;
             if (node.left != null) {
-                parent.put(node.left, node);
-                q.add(node.left);
+                map.put(node.left, node);
+                bfsQue.add(node.left);
             }
             if (node.right != null) {
-                parent.put(node.right, node);
-                q.add(node.right);
+                map.put(node.right, node);
+                bfsQue.add(node.right);
             }
         }
     }
 
     public int findMaxDistance(Map<TreeNode, TreeNode> map, TreeNode target) {
         Set<TreeNode> set = new HashSet<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        int maxi = -1;
-        q.add(target);
+        Queue<TreeNode> bfsQue = new LinkedList<>();
+        int count = -1;
+        bfsQue.add(target);
         set.add(target);
 
-        while (!q.isEmpty()) {
-            int qSize = q.size();
-            maxi++;
+        while (!bfsQue.isEmpty()) {
+            int qSize = bfsQue.size();
+            count++;
 
             for (int i = 0; i < qSize; i++) {
-                TreeNode node = q.poll();
+                TreeNode node = bfsQue.poll();
 
                 if (node.left != null && !set.contains(node.left)) {
                     set.add(node.left);
-                    q.add(node.left);
+                    bfsQue.add(node.left);
                 }
 
                 if (node.right != null && !set.contains(node.right)) {
                     set.add(node.right);
-                    q.add(node.right);
+                    bfsQue.add(node.right);
                 }
 
                 if (map.get(node) != null && !set.contains(map.get(node))) {
                     set.add(map.get(node));
-                    q.add(map.get(node));
+                    bfsQue.add(map.get(node));
                 }
             }
         }
-        return maxi;
+        return count;
     }
 }
