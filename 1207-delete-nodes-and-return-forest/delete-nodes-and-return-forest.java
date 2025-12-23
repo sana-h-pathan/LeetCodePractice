682892518b33620135dcc27fee277341c468c63f
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 19:07:21 -0800

    Time: 1 ms (97.23%) | Memory: 46.4 MB (71.65%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -14,32 +14,31 @@
  * }
  */
 class Solution {
-    
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        HashSet<Integer> toDelete =new HashSet<>();
-        List<TreeNode> result=new ArrayList<>();
+        HashSet<Integer> toDelete = new HashSet<>();
+        List<TreeNode> result = new ArrayList<>();
         for(int num: to_delete)
             toDelete.add(num);
-        removeNodes(root, toDelete, result);
-        if(!toDelete.contains(root.val))
+        removeNode(root, toDelete, result);
+        if(!toDelete.contains(root.val)){
             result.add(root);
-
+        }
         return result;
     }
-    private TreeNode removeNodes(TreeNode root, HashSet<Integer> toDelete, List<TreeNode> result){
+    private TreeNode removeNode(TreeNode root, HashSet<Integer> toDelete, List<TreeNode> result){
         if(root==null)
             return null;
-        
-        root.left=removeNodes(root.left, toDelete, result);
-        root.right=removeNodes(root.right, toDelete, result);
+        root.left = removeNode(root.left, toDelete, result);
+        root.right = removeNode(root.right, toDelete, result);
         if(toDelete.contains(root.val)){
-            if(root.left!=null)
+            if(root.left!=null){
                 result.add(root.left);
-            if(root.right!=null)
+            }
+            if(root.right!=null){
                 result.add(root.right);
+            }
             return null;
         }
         return root;
-
     }
 }
