c2a4a4e91bbe21f41f905e4ddcdd3d7a7e998516
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 23:31:29 -0800

    Time: 14 ms (90.65%) | Memory: 43.7 MB (57.61%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,30 +1,29 @@
 class Solution {
     int start=0;
-    int end=0;
+    int end =0;
     public String longestPalindrome(String s) {
-        if(s==null || s.length()==0)
+        if(s==null || s.length()==0){
             return "";
-
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            expandFromMiddle(i,i,s);
-            if(i<n-1 && s.charAt(i)==s.charAt(i+1))
-                expandFromMiddle(i,i+1,s);
         }
-        return s.substring(start,end+1);
+        int n=s.length();
+        for(int i=0;i<s.length();i++){
+            expandFromMiddle(s, i, i);
+            if(i<n-1 && s.charAt(i)==s.charAt(i+1)){
+                expandFromMiddle(s, i, i+1);
+            }
+        }
+        return s.substring(start, end+1);
     }
-
-    private void expandFromMiddle(int l, int r, String s){
-        while(l>=0 && r<=s.length()-1 && s.charAt(l)==s.charAt(r)){
+    private void expandFromMiddle(String s, int l, int r){
+        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
             l--;
             r++;
         }
         l++;
         r--;
-        if(end-start<=r-l){
+        if(end-start<r-l){
             start=l;
             end=r;
         }
     }
-    
 }
