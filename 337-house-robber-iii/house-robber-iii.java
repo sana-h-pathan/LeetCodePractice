90096988c4254c84ccaf9f524bfe81b7b3a127c5
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 20:52:42 -0800

    Time: 3 ms (13.60%) | Memory: 46.8 MB (23.94%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<TreeNode, Integer> robResult = new HashMap<>();
+    HashMap<TreeNode, Integer> notRobResult = new HashMap<>();
+    public int rob(TreeNode root) {
+        return helper(root, false);
+    }
+
+    public int helper(TreeNode node, boolean parentRobbed) {
+        if (node == null) {
+            return 0;
+        }
+        if (parentRobbed) {
+            if (robResult.containsKey(node)) {
+                return robResult.get(node);
+            }
+            int result = helper(node.left, false) + helper(node.right, false);
+            robResult.put(node, result);
+            return result;
+        } else {
+            if (notRobResult.containsKey(node)) {
+                return notRobResult.get(node);
+            }
+            int rob = node.val + helper(node.left, true) + helper(node.right, true);
+            int notRob = helper(node.left, false) + helper(node.right, false);
+            int result = Math.max(rob, notRob);
+            notRobResult.put(node, result);
+            return result;
+        }
+    }
+
+    
+}
