101ec3d8bf7259d64ac19feaeafa8505c9c9725d
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-22 22:12:39 -0800

    Time: 14 ms (67.18%) | Memory: 47.1 MB (48.14%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,39 +1,40 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length()>s.length()){
+        if(t.length()>s.length())
             return "";
-        }
         HashMap<Character, Integer> map = new HashMap<>();
         for(int i=0;i<t.length();i++){
             char ch = t.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
-        int reqdMatches = t.length();
-        int currMatches = 0;
+        int currentMatches = t.length();
+        int reqdMatches = 0;
+        int sIdx=-1;
         int l=0;
         int r=0;
         int minLength = Integer.MAX_VALUE;
-        int sIdx=-1;
         while(r<s.length()){
             char rChar = s.charAt(r);
-            if(map.getOrDefault(rChar,0)>0)
-                currMatches++;
-            map.put(rChar, map.getOrDefault(rChar,0)-1);
-            while(currMatches==reqdMatches){
+            if(map.getOrDefault(rChar,0)>0){
+                reqdMatches++;
+            }
+            map.put(rChar, map.getOrDefault(rChar, 0)-1);
+            while(reqdMatches==currentMatches){
                 if(r-l+1<minLength){
-                    minLength = r-l+1;
-                    sIdx = l;
+                    minLength = Math.min(r-l+1, minLength);
+                    sIdx=l;
                 }
                 char lChar = s.charAt(l);
-                map.put(lChar, map.getOrDefault(lChar,0)+1);
-                if(map.getOrDefault(lChar,0)>0)
-                    currMatches--;
-                l++;
+                map.put(lChar, map.getOrDefault(lChar, 0)+1);
+                if(map.getOrDefault(lChar, 0)>0){
+                    reqdMatches--;
+                }
+                l++; 
             }
             r++;
         }
         if(sIdx==-1)
             return "";
-        return s.substring(sIdx,sIdx+minLength);
+        return s.substring(sIdx, sIdx+minLength);
     }
 }
