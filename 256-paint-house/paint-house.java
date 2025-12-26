0c47d02bde4065a425ae023f13dda70b1c03a254
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-26 15:42:21 -0800

    Time: 1 ms (91.18%) | Memory: 44.6 MB (65.48%) - LeetSync

--- 256-paint-house/paint-house.java
+++ 256-paint-house/paint-house.java
@@ -1,33 +1,17 @@
 class Solution {
     public int minCost(int[][] costs) {
-        int n = costs.length;
-        int[][] dp = new int[n][4];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
+        if(costs==null || costs.length==0)
+            return 0;
+        int n=costs.length;
+        int[][] dp=new int[n][3];
+        dp[0][0]=costs[0][0];
+        dp[0][1]=costs[0][1];
+        dp[0][2]=costs[0][2];
+        for(int i=1;i<n;i++){
+            dp[i][0]=costs[i][0]+Math.min(dp[i-1][1],dp[i-1][2]);
+            dp[i][1]=costs[i][1]+Math.min(dp[i-1][0],dp[i-1][2]);
+            dp[i][2]=costs[i][2]+Math.min(dp[i-1][1],dp[i-1][0]);
         }
-        return helper(costs, n-1, 3, dp);
-    }
-
-    private int helper(int[][] costs, int idx, int task, int[][] dp){
-        if(idx==0){
-            int minVal = Integer.MAX_VALUE;
-            for(int i=0;i<=2;i++){
-                if(i!=task){
-                    minVal = Math.min(minVal, costs[0][i]);
-                }
-            }
-            return minVal;
-        }
-        if(dp[idx][task]!=-1)
-            return dp[idx][task];
-        int minVal = Integer.MAX_VALUE;
-        for(int i=0;i<=2;i++){
-            if(i!=task){
-                int currVal = costs[idx][i] + helper(costs, idx-1, i, dp);
-                minVal = Math.min(minVal, currVal);
-            }
-        }
-        dp[idx][task]=minVal;
-        return dp[idx][task];
+        return Math.min(dp[n-1][0],Math.min(dp[n-1][1],dp[n-1][2]));
     }
 }
