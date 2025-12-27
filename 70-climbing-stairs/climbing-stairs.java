ad851b0002e02ac14c29c674da33641b7a422da6
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-26 21:54:20 -0800

    Time: 0 ms (100.00%) | Memory: 42 MB (46.41%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,38 +1,21 @@
 class Solution {
-
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[0] = 1;
-        
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
-
-    public int climbStairs1(int n) {
-        if(n==0)
-            return 1;
-        if(n==1)
-            return 1;
-        int first = climbStairs1(n-1);
-        int second = climbStairs1(n-2);
-        return first+second;
-    }
-
-    public int climbStairs2(int n) {
-        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
         return helper(n, dp);
     }
+
     private int helper(int n, int[] dp){
         if(n==0)
             return 1;
         if(n==1)
             return 1;
-        if(dp[n]!=0)
+        if(dp[n]!=-1)
             return dp[n];
-        dp[n] = helper(n-1, dp) + helper(n-2, dp);
+
+        int step1Count = helper(n-1, dp);
+        int step2Count = helper(n-2, dp);
+        dp[n]= step1Count+step2Count;
         return dp[n];
     }
 }
