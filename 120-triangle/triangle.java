37afcca5b323bf15ebd11cdc786d0fd0f648179d
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-28 12:22:04 -0800

    Time: 6 ms (29.13%) | Memory: 45.3 MB (74.56%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,5 +1,21 @@
 class Solution {
+
     public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle.get(n-1).get(j);
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle.get(i).get(j)+ helper(triangle, i+1, j,dp);
+                int diagonal = triangle.get(i).get(j)+ helper(triangle, i+1, j+1, dp);
+                dp[i][j] = Math.min(down, diagonal);
+            }
+        }
+        return dp[0][0];
+    }
+    public int minimumTotal1(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[][] dp = new int[n][n];
         for(int i=0;i<n;i++){
@@ -18,10 +34,9 @@ class Solution {
         int diagonal = triangle.get(i).get(j)+ helper(triangle, i+1, j+1, dp);
         dp[i][j] = Math.min(down, diagonal);
         return dp[i][j];
-
     }
 
-    public int minimumTotal1(List<List<Integer>> triangle) {
+    public int minimumTotal2(List<List<Integer>> triangle) {
         return helper(triangle, 0, 0);
     }
     private int helper(List<List<Integer>> triangle, int i, int j){
