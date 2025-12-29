99068ca0c86477e09cbd442b98497c16197286b2
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-28 18:21:25 -0800

    Time: 3 ms (64.97%) | Memory: 46.4 MB (45.09%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -17,26 +17,32 @@ class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root==null && subRoot==null)
             return true;
-        Queue<TreeNode> bfsQue = new LinkedList<>();
+        if(root==null || subRoot==null)
+            return false;
+        Queue<TreeNode> bfsQue=new LinkedList<>();
         bfsQue.add(root);
         while(!bfsQue.isEmpty()){
             TreeNode curr = bfsQue.poll();
-            if(isSameTree(curr, subRoot))
+            if(isSameTree(curr, subRoot)){
                 return true;
-            if(curr.left!=null)
+            }
+            if(curr.left!=null){
                 bfsQue.add(curr.left);
-            if(curr.right!=null)
+            }
+            if(curr.right!=null){
                 bfsQue.add(curr.right);
+            }
         }
         return false;
     }
-    private boolean isSameTree(TreeNode p, TreeNode q){
+    public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null)
             return true;
-        if((p!=null && q==null) || (p==null && q!=null))
+        if((p==null && q!=null) || (p!=null && q==null))
             return false;
-        if(p.val!=q.val)
+        if(p.val!=q.val){
             return false;
+        }
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
