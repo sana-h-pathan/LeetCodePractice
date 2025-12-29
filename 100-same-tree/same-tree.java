0c03d20911dbe108beb80c01aee01c157047263a
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-28 18:16:49 -0800

    Time: 0 ms (100.00%) | Memory: 43.2 MB (10.38%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,43 +14,14 @@
  * }
  */
 class Solution {
-    public boolean isSameTree1(TreeNode p, TreeNode q) {
-        if(p==null && q==null)
-            return true;
-        if((p==null && q!=null)||(p!=null && q==null))
-            return false;
-        if(p.val!=q.val)
-            return false;
-        return (isSameTree(p.left, q.left) && (isSameTree(p.right, q.right)));
-    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null)
             return true;
-        if((p==null && q!=null)||(p!=null && q==null))
+        if((p==null && q!=null) || (p!=null && q==null))
             return false;
-        Queue<TreeNode> pQue=new LinkedList<>();
-        Queue<TreeNode> qQue=new LinkedList<>();
-        pQue.add(p);
-        qQue.add(q);
-        while(!pQue.isEmpty() && !qQue.isEmpty()){
-            TreeNode pCurr=pQue.poll();
-            TreeNode qCurr=qQue.poll();
-            if(pCurr.val!=qCurr.val)
-                return false;
-            if((pCurr.left!=null && qCurr.left==null) || (pCurr.left==null && qCurr.left!=null))
-                return false;
-            if(pCurr.left!=null && qCurr.left!=null){
-                pQue.add(pCurr.left);
-                qQue.add(qCurr.left);
-            }
-            
-            if((pCurr.right!=null && qCurr.right==null) || (pCurr.right==null && qCurr.right!=null))
-                return false;
-            if(pCurr.right!=null && qCurr.right!=null){
-                pQue.add(pCurr.right);
-                qQue.add(qCurr.right);
-            }
+        if(p.val!=q.val){
+            return false;
         }
-        return true;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
