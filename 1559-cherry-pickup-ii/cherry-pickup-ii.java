afbf458e6307057a2231ccef7b2f37274857723c
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-28 16:48:24 -0800

    Time: 25 ms (69.88%) | Memory: 46.3 MB (72.65%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -0,0 +1,71 @@
+class Solution {
+    int dir[] = {-1, 0, 1};
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] dp = new int[m][n][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return helper(grid, 0, 0, n-1, dp);
+    }
+    private int helper(int[][] grid, int i, int j1, int j2, int[][][] dp){
+        if(j1<0 || j1>grid[0].length-1|| j2<0 || j2>grid[0].length-1)
+            return (int)-1e9;
+        if(i==grid.length-1){
+            if(j1==j2){
+                return grid[i][j1];
+            } else{
+                return grid[i][j1]+grid[i][j2];
+            }
+        }
+        if(dp[i][j1][j2]!=-1){
+            return dp[i][j1][j2];
+        }
+        int maxValue = (int)-1e9;
+        for(int d1=0;d1<dir.length;d1++){
+            for(int d2=0;d2<dir.length;d2++){
+                maxValue = Math.max(maxValue, helper(grid, i+1, j1+dir[d1], j2+dir[d2], dp));
+            }
+        }
+        if(j1==j2){
+            maxValue+=grid[i][j1];
+        } else{
+            maxValue += grid[i][j1]+ grid[i][j2];
+        }
+        dp[i][j1][j2] = maxValue;
+        return dp[i][j1][j2];
+    }
+
+    public int cherryPickup2(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        return helper(grid, 0, 0, n-1);
+    }
+    private int helper(int[][] grid, int i, int j1, int j2){
+        if(j1<0 || j1>grid[0].length-1|| j2<0 || j2>grid[0].length-1)
+            return (int)-1e9;
+        if(i==grid.length-1){
+            if(j1==j2){
+                return grid[i][j1];
+            } else{
+                return grid[i][j1]+grid[i][j2];
+            }
+        }
+        int maxValue = (int)-1e9;
+        for(int d1=0;d1<dir.length;d1++){
+            for(int d2=0;d2<dir.length;d2++){
+                maxValue = Math.max(maxValue, helper(grid, i+1, j1+dir[d1], j2+dir[d2]));
+            }
+        }
+        if(j1==j2){
+            maxValue+=grid[i][j1];
+        } else{
+            maxValue += grid[i][j1]+ grid[i][j2];
+        }
+        return maxValue;
+    }
+}
