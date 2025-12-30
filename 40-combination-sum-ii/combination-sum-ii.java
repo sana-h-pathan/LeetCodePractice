d35f7d81e026464dcb07d4a68e6afe4fd606bb1a
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-30 13:49:09 -0800

    Time: 8 ms (15.51%) | Memory: 50.8 MB (5.20%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result=new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates);
-        backTrack(candidates,target,0,result,new ArrayList<>());
+        backtrack(candidates, target, 0, result, new ArrayList<>());
         return result;
     }
-    private void backTrack(int[] candidates, int target, int idx, List<List<Integer>> result,  List<Integer> temp){
-        if(target==0){
-            result.add(new ArrayList<>(temp));
+    private void backtrack(int[] candidates, int target, int idx, List<List<Integer>> result, List<Integer> temp){
+        if(target<0){
             return;
         }
-        if(target<0)
-            return;
-        int prev=-1;
+        if(target==0){
+            result.add(new ArrayList<>(temp));
+        }
+        Integer prev=null;
         for(int i=idx;i<candidates.length;i++){
-            if(candidates[i]==prev){
+            if(prev!=null && prev==candidates[i]){
                 continue;
             }
-            if(candidates[i]>target)
-                continue;
             temp.add(candidates[i]);
-            backTrack(candidates, target-candidates[i], i+1, result, temp);
+            backtrack(candidates, target-candidates[i], i+1, result, temp);
             temp.remove(temp.size()-1);
             prev=candidates[i];
         }
-
     }
 }
