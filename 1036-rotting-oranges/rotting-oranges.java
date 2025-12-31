3bf271c1e29e5e4cecca6c755f35a744574e191b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-30 17:21:31 -0800

    Time: 2 ms (90.00%) | Memory: 44.3 MB (38.56%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,42 +1,45 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         int freshOranges=0;
         Queue<int[]> bfsQue = new LinkedList<>();
-        int minutes=0;
+        int time=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     bfsQue.add(new int[]{i,j});
-                } else if(grid[i][j]==1)
+                } else if(grid[i][j]==1){
                     freshOranges++;
+                }
             }
         }
-        if(freshOranges==0)
+        if(freshOranges==0){
             return 0;
-
+        }
         while(!bfsQue.isEmpty()){
-            int size=bfsQue.size();
-            minutes++;
+            int size = bfsQue.size();
+            time++;
             for(int i=0;i<size;i++){
                 int[] curr = bfsQue.poll();
                 for(int[] dir: dirs){
-                    int nr=dir[0]+curr[0];
-                    int nc=dir[1]+curr[1];
+                    int nr = curr[0]+dir[0];
+                    int nc = curr[1]+dir[1];
                     if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
-                        grid[nr][nc]=2;
-                        bfsQue.add(new int[]{nr,nc});
                         freshOranges--;
-                        if(freshOranges==0)
-                            return minutes;
+                        bfsQue.add(new int[]{nr, nc});
+                        grid[nr][nc]=2;
+                        if(freshOranges==0){
+                            return time;
+                        }
                     }
                 }
             }
         }
-        if(freshOranges==0)
-            return minutes;
+        if(freshOranges==0){
+            return 0;
+        }
         return -1;
     }
 }
