9b781b7ed00bc9b61e24e824eba50b33ba9a53c8
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2025-12-31 20:17:19 -0800

    Time: 16 ms (37.38%) | Memory: 56.5 MB (52.71%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,25 +1,35 @@
 class Solution {
-    public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[amount+1][n];
-        for(int i=0;i<=amount;i++){
+    
+    public int change1(int amount, int[] coins) {
+        int m = coins.length;
+        int[][] dp = new int[m+1][amount+1];
+        for(int i=1;i<=m;i++){
             Arrays.fill(dp[i], -1);
         }
-        return helper(coins, amount, n-1, dp);
+        return 0;
+    }
+
+    public int change(int amount, int[] coins) {
+        int m = coins.length;
+        int[][] dp = new int[m+1][amount+1];
+        for(int i=0;i<=m;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return helper(coins, amount, m-1, dp);
     }
     private int helper(int[] coins, int amount, int idx, int[][] dp){
         if(amount==0)
             return 1;
         if(amount<0 || idx<0)
             return 0;
-        if(dp[amount][idx]!=-1){
-            return dp[amount][idx];
+        if(dp[idx][amount]!=-1){
+            return dp[idx][amount];
         }
         int notChoose = helper(coins, amount, idx - 1, dp);
         int choose = helper(coins, amount - coins[idx], idx, dp);
 
-        dp[amount][idx] = notChoose + choose;
-        return dp[amount][idx];
+        dp[idx][amount] = notChoose + choose;
+        return dp[idx][amount];
     }
 
     public int change2(int amount, int[] coins) {
