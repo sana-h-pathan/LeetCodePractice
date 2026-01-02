a1105cf446974b0d534c55005fdbc186fdac95ba
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-02 14:56:49 -0800

    Time: 3 ms (94.35%) | Memory: 45.3 MB (19.86%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
+        if(nums1.length>nums2.length){
             return intersect(nums2, nums1);
         }
-        HashMap<Integer, Integer> m = new HashMap<>();
-        for (int n : nums1) {
-            m.put(n, m.getOrDefault(n, 0) + 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num: nums1){
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
+
         int k = 0;
         for (int n : nums2) {
-            int cnt = m.getOrDefault(n, 0);
+            int cnt = map.getOrDefault(n, 0);
             if (cnt > 0) {
                 nums1[k++] = n;
-                m.put(n, cnt - 1);
+                map.put(n, cnt - 1);
             }
         }
         return Arrays.copyOfRange(nums1, 0, k);
