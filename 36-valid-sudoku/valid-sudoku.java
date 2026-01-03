162d4348b16f2be45bf50f261c6aaac0f9ac99af
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-02 23:23:56 -0800

    Time: 15 ms (6.97%) | Memory: 52.6 MB (7.63%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> set=new HashSet<>();
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                char ch=board[i][j];
+        HashSet<String> set = new HashSet<>();
+        for(int i =0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                char ch = board[i][j];
                 if(ch!='.'){
-                    if((set.contains(ch+"in row"+i))|| (set.contains(ch+"in column"+j)) || (set.contains(ch+"in row"+i/3+"-"+j/3))){
-                        return false;
+                    if(set.contains(ch +" in row " + i)|| set.contains(ch +" in column " + j) ||
+                        set.contains(ch +" in row/colum " + i/3+"/"+j/3)){
+                            return false;
                     }
-                    set.add(ch+"in row"+i);
-                    set.add(ch+"in column"+j);
-                    set.add(ch+"in row"+i/3+"-"+j/3);
+                    set.add(ch +" in row " + i);
+                    set.add(ch +" in column " + j);
+                    set.add(ch +" in row/colum " + i/3+"/"+j/3);
                 }
+                
             }
         }
         return true;
