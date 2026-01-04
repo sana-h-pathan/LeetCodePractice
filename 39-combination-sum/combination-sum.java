0d63c786021d24bb12154fd4fb3532753bc735f3
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-04 15:51:05 -0800

    Time: 2 ms (95.30%) | Memory: 45.9 MB (21.85%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -4,7 +4,7 @@ class Solution {
         helper(candidates, 0, target, result, new ArrayList<>());
         return result;
     }
-    private void helper(int[] candidates, int idx, int target, List<List<Integer>> result, List<Integer> temp){
+    private void helper1(int[] candidates, int idx, int target, List<List<Integer>> result, List<Integer> temp){
         if(target==0){
             result.add(new ArrayList<>(temp));
             return;
@@ -16,5 +16,20 @@ class Solution {
         temp.add(candidates[idx]);
         helper(candidates, idx, target-candidates[idx], result, temp);
         temp.remove(temp.size()-1);
+    }
+
+    private void helper(int[] candidates, int idx, int target, List<List<Integer>> result, List<Integer> temp){
+        if(target==0){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        if(target<0){
+            return;
+        }
+        for(int i=idx;i<candidates.length;i++){
+            temp.add(candidates[i]);
+            helper(candidates, i, target-candidates[i], result, temp);
+            temp.remove(temp.size()-1);
+        }
     }
 }
