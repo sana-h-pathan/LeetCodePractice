2402fe9b876a3bf456ca324b5fbb065e58b6be17
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-04 09:49:11 -0800

    Time: 31 ms (22.32%) | Memory: 58.5 MB (9.39%) - LeetSync

---
+++ 359-logger-rate-limiter/logger-rate-limiter.java
@@ -0,0 +1,25 @@
+class Logger {
+    HashMap<String, Integer> map;
+    public Logger() {
+        this.map = new HashMap<>();
+    }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!map.containsKey(message)){
+            map.put(message, timestamp+10);
+            return true;
+        }
+        int currTS = map.get(message);
+        if(currTS>timestamp){
+            return false;
+        }
+        map.put(message, timestamp+10);
+        return true;
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
