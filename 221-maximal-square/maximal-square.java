6142c74e2c6eed4b6b56c207b68c71d4bcb890c6
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-05 10:43:44 -0800

    Time: 13 ms (11.24%) | Memory: 70.9 MB (56.08%) - LeetSync

---
+++ 221-maximal-square/maximal-square.java
@@ -0,0 +1,81 @@
+class Solution {
+    private int m, n;
+    private int maxSide = 0;
+    int[][] dirs = new int[][]{{1,0},{0,1},{1,1}};
+
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) 
+            return 0;
+
+        this.m = matrix.length;
+        this.n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i], -1);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                solve(matrix, i, j, dp);
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+    private int solve(char[][] matrix, int i, int j, int[][] dp) {
+        // Out of bounds
+        if (i >= m || j >= n) 
+            return 0;
+        // If cell is '0', no square can start here
+        if (matrix[i][j] == '0') {
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int minVal=Integer.MAX_VALUE;
+        for(int[] dir: dirs){
+            int nr = dir[0]+i;
+            int nc = dir[1]+j;
+            minVal = Math.min(minVal, solve(matrix, nr, nc, dp));
+        }
+        maxSide = Math.max(maxSide, 1 + minVal);
+        dp[i][j]=1 + minVal;
+        return dp[i][j];
+    }
+
+
+    public int maximalSquare2(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) 
+            return 0;
+
+        this.m = matrix.length;
+        this.n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                solve(matrix, i, j);
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+    private int solve(char[][] matrix, int i, int j) {
+        // Out of bounds
+        if (i >= m || j >= n) 
+            return 0;
+        // If cell is '0', no square can start here
+        if (matrix[i][j] == '0') {
+            return 0;
+        }
+        int minVal=Integer.MAX_VALUE;
+        for(int[] dir: dirs){
+            int nr = dir[0]+i;
+            int nc = dir[1]+j;
+            minVal = Math.min(minVal, solve(matrix, nr, nc));
+        }
+        maxSide = Math.max(maxSide, 1 + minVal);
+        return 1 + minVal;
+    }
+
+}
