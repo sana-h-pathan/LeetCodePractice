996119f796d5a8b231377ba1b47a48d3f528a2af
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-05 15:57:58 -0800

    Time: 187 ms (75.79%) | Memory: 47.2 MB (55.00%) - LeetSync

--- 976-minimum-area-rectangle/minimum-area-rectangle.java
+++ 976-minimum-area-rectangle/minimum-area-rectangle.java
@@ -1,27 +1,31 @@
 class Solution {
     public int minAreaRect(int[][] points) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        for (int[] p : points) {
-            if (!map.containsKey(p[0])) {
-                map.put(p[0], new HashSet<>());
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+        for(int[] p : points){
+            int x = p[0];
+            int y = p[1];
+            if(!map.containsKey(x)){
+                map.put(x, new HashSet<>());
             }
-            map.get(p[0]).add(p[1]);
+            map.get(x).add(y);
         }
-        int area = Integer.MAX_VALUE;
+        int minArea = Integer.MAX_VALUE;
         for(int i=0;i<points.length;i++){
             for(int j=i+1;j<points.length;j++){
-                int x1=points[i][0];
-                int y1=points[i][1];
-                int x2=points[j][0];
-                int y2=points[j][1];
-                
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
                 if(x1!=x2 && y1!=y2){
                     if(map.get(x1).contains(y2) && map.get(x2).contains(y1)){
-                        area=Math.min(area,Math.abs(x1-x2)*Math.abs(y1-y2));
+                        int currArea = Math.abs(x2-x1) * Math.abs(y2-y1);
+                        minArea = Math.min(minArea, currArea);
                     }
                 }
             }
         }
-        return area == Integer.MAX_VALUE ? 0 : area;
+        return minArea == Integer.MAX_VALUE ? 0 : minArea;
+
     }
 }
