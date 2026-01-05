090750c7166df0212a2d11e7a224e46d41ca92d2
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-05 10:48:42 -0800

    Time: 8 ms (77.47%) | Memory: 70.9 MB (56.08%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -2,8 +2,34 @@ class Solution {
     private int m, n;
     private int maxSide = 0;
     int[][] dirs = new int[][]{{1,0},{0,1},{1,1}};
-
     public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int maxSide = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1; 
+                    } else {
+                        dp[i][j] = 1 + Math.min(dp[i-1][j],
+                                   Math.min(dp[i][j-1], dp[i-1][j-1]));
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+
+
+    public int maximalSquare1(char[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) 
             return 0;
 
