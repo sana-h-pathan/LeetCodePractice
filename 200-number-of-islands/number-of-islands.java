1889124dbd2c133f52ec601385200b070edf0a73
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-06 08:08:12 -0800

    Time: 6 ms (24.51%) | Memory: 51.6 MB (81.97%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,32 +1,38 @@
 class Solution {
+    int m;
+    int n;
+    int[][] dirs;
     public int numIslands(char[][] grid) {
-        int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        int m=grid.length;
-        int n=grid[0].length;
-        boolean[][] visited =new boolean[m][n];
-        Queue<int[]> bfsQue=new LinkedList<>();
-        int result=0;
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        boolean[][]visited = new boolean[m][n];
+        int islands=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
-                    bfsQue.add(new int[]{i,j});
-                    result++;
                     visited[i][j]=true;
-                    while(!bfsQue.isEmpty()){
-                        
-                        int[] curr=bfsQue.poll();
-                        for(int[] dir: dirs){
-                            int nr=curr[0]+dir[0];
-                            int nc=curr[1]+dir[1];
-                            if(nr>=0 && nc>=0 && nr<m && nc<n && !visited[nr][nc] && grid[nr][nc]=='1'){
-                                bfsQue.add(new int[]{nr, nc});
-                                visited[nr][nc]=true;
-                            }
-                        }
-                    }
+                    islands++;
+                    helper(grid, i, j, visited);
                 }
             }
         }
-        return result;
+        return islands;
+    }
+
+    private void helper(char[][] grid, int i, int j, boolean[][] visited){
+        Queue<int[]> bfsQue = new LinkedList<>();
+        bfsQue.add(new int[]{i,j});
+        while(!bfsQue.isEmpty()){
+            int[] curr = bfsQue.poll();
+            for(int[] dir: dirs){
+                int nr = curr[0]+dir[0];
+                int nc = curr[1]+dir[1];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1' && !visited[nr][nc]){
+                    bfsQue.add(new int[]{nr, nc});
+                    visited[nr][nc]=true;
+                }
+            }
+        }
     }
 }
