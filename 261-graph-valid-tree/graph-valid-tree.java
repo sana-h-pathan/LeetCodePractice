333f3cf0226423d3c248867af9cd249ee32bfad2
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-06 09:34:36 -0800

    Time: 6 ms (23.96%) | Memory: 46.6 MB (25.95%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -5,27 +5,27 @@ class Solution {
             adjList.add(new ArrayList<>());
         }
         for(int[] edge: edges){
-            int e1 = edge[0];
-            int e2 = edge[1];
-            adjList.get(e1).add(e2);
-            adjList.get(e2).add(e1);
+            int u = edge[0];
+            int v = edge[1];
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
         }
         boolean[] visited = new boolean[n];
         Queue<int[]> bfsQue = new LinkedList<>();
-        bfsQue.add(new int[]{0,-1});
+        bfsQue.add(new int[]{0, -1});
         visited[0]=true;
         while(!bfsQue.isEmpty()){
             int[] curr = bfsQue.poll();
             int node = curr[0];
             int parent = curr[1];
             for(int ne: adjList.get(node)){
-                if(ne==parent)
+                if(ne==parent){
                     continue;
-                if(visited[ne]){
-                    return false;
                 }
-                bfsQue.add(new int[]{ne,node});
-                visited[ne]=true;
+                if(visited[ne])
+                    return false;
+                visited[ne] = true;
+                bfsQue.add(new int[]{ne, node});
             }
         }
         for(int i=0;i<n;i++){
