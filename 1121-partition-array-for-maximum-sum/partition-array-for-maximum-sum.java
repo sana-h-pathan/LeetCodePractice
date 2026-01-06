a91a3407667faf0a010c04be284bd644ace23431
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-05 16:53:11 -0800

    Time: 6 ms (95.27%) | Memory: 44 MB (26.57%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,15 +1,50 @@
-
 class Solution {
+    int n;
+
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int n=arr.length;
-        int []dp=new int[n];
-        for(int i=0;i<n;i++){
-            int max=0;
-            for(int j=1;(j<=k) && (i-j+1>=0);j++){
-                max=Math.max(max,arr[i-j+1]);
-                dp[i]=Math.max(dp[i],((i>=j)?dp[i-j]:0)+max*j);
-            }
+        this.n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return helper(arr, k, 0, dp);
+        
+    }
+    private int helper(int[] nums, int k, int idx, int[] dp){
+        if(idx==nums.length){
+            return 0;
         }
-        return dp[n-1];
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int currLen = 0;
+        int maxVal = Integer.MIN_VALUE;
+        int maxTotal = Integer.MIN_VALUE;
+        for(int i=idx;i<Math.min(idx+k,n);i++){
+            currLen++;
+            maxVal = Math.max(nums[i], maxVal);
+            int currSum = maxVal*currLen + helper(nums, k, i+1,dp);
+            maxTotal = Math.max(maxTotal, currSum);
+        }
+        
+        return dp[idx] = maxTotal;
+    }
+    public int maxSumAfterPartitioning1(int[] arr, int k) {
+        this.n = arr.length;
+        return helper(arr, k, 0);
+        
+    }
+    private int helper(int[] nums, int k, int idx){
+        if(idx==nums.length){
+            return 0;
+        }
+        int currLen = 0;
+        int maxVal = Integer.MIN_VALUE;
+        int maxTotal = Integer.MIN_VALUE;
+        for(int i=idx;i<Math.min(idx+k,n);i++){
+            currLen++;
+            maxVal = Math.max(nums[i], maxVal);
+            int currSum = maxVal*currLen + helper(nums, k, i+1);
+            maxTotal = Math.max(maxTotal, currSum);
+        }
+        return maxTotal;
     }
 }
