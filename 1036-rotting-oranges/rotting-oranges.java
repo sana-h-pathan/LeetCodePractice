153e86cfb4f99721ac319c8c60c311e3fc6e4790
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-06 08:31:43 -0800

    Time: 2 ms (90.18%) | Memory: 44.5 MB (17.39%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,43 +2,43 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        int[][] dirs = new int[][]{{1,0},{0,1},{0,-1},{-1,0}};
+        Queue<int[]> bfsQueue = new LinkedList<>();
+        int minutes=0;
         int freshOranges=0;
-        Queue<int[]> bfsQue = new LinkedList<>();
-        int time=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    bfsQue.add(new int[]{i,j});
-                } else if(grid[i][j]==1){
+                if(grid[i][j]==1){
                     freshOranges++;
+                } else if(grid[i][j]==2){
+                    bfsQueue.add(new int[]{i,j});
                 }
             }
         }
         if(freshOranges==0){
-            return 0;
+            return minutes;
         }
-        while(!bfsQue.isEmpty()){
-            int size = bfsQue.size();
-            time++;
+        while(!bfsQueue.isEmpty()){
+            int size = bfsQueue.size();
+            minutes++;
             for(int i=0;i<size;i++){
-                int[] curr = bfsQue.poll();
-                for(int[] dir: dirs){
+                int[] curr = bfsQueue.poll();
+                for(int[] dir:dirs){
                     int nr = curr[0]+dir[0];
                     int nc = curr[1]+dir[1];
                     if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
-                        freshOranges--;
-                        bfsQue.add(new int[]{nr, nc});
                         grid[nr][nc]=2;
+                        freshOranges--;
                         if(freshOranges==0){
-                            return time;
+                            return minutes;
                         }
+                        bfsQueue.add(new int[]{nr, nc});
                     }
                 }
             }
         }
         if(freshOranges==0){
-            return 0;
+            return minutes;
         }
         return -1;
     }
