76fdc08c4ec86750cd232c3a274bb8d3c8318f3c
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-12 00:04:22 -0800

    Time: 80 ms (77.32%) | Memory: 89 MB (64.95%) - LeetSync

--- 1052-campus-bikes/campus-bikes.java
+++ 1052-campus-bikes/campus-bikes.java
@@ -1,44 +1,48 @@
 class Solution {
     public int[] assignBikes(int[][] workers, int[][] bikes) {
-        if(workers==null || workers.length==0 ||bikes==null || bikes.length==0 )
+        if(workers==null || bikes == null || workers.length == 0 || bikes.length==0){
             return new int[]{};
-        int b=bikes.length;
-        int w=workers.length;
+        }
+        HashMap<Integer, List<int[]>> map = new HashMap<>();
         int min=Integer.MAX_VALUE;
-        int max= Integer.MIN_VALUE;
-        HashMap<Integer, List<int[]>> map=new HashMap<>();
-        for(int i=0;i<b;i++){
-            for(int j=0;j<w;j++){
-                int dist=Math.abs(workers[j][0]-bikes[i][0])+Math.abs(workers[j][1]-bikes[i][1]);
-                min=Math.min(min, dist);
-                max=Math.max(max, dist);
-                if(!map.containsKey(dist))
+        int max=Integer.MIN_VALUE;
+        int m = bikes.length;
+        int n = workers.length; //n<=m
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int dist = calculateDistance(bikes[i],workers[j]);
+                min = Math.min(min, dist);
+                max = Math.max(max, dist);
+                if(!map.containsKey(dist)){
                     map.put(dist, new ArrayList<>());
+                }
                 map.get(dist).add(new int[]{i,j});
             }
         }
-
-        boolean[] availableWorker=new boolean[w];
-        boolean[] availableBikes=new boolean[b];
-        int count=0;
-        int[] result=new int[w];
+        boolean[] availableWorker = new boolean[n];
+        boolean[] availableBikes = new boolean[m];
+        int count =0;
+        int[] result = new int[n];
         for(int i=min;i<=max;i++){
-            List<int[]>  temp=map.get(i);
-            if(temp!=null){
-                for(int[] tmp: temp){
-                    int tmpB=tmp[0];
-                    int tmpW=tmp[1];
-                    if(!availableBikes[tmpB] && !availableWorker[tmpW]){
-                        availableBikes[tmpB]=true;
-                        availableWorker[tmpW]=true;
+            if(map.containsKey(i)){
+                for(int[] pair: map.get(i)){
+                    int bike = pair[0];
+                    int worker = pair[1];
+                    if(!availableWorker[worker] && !availableBikes[bike]){
+                        availableWorker[worker] = true;
+                        availableBikes[bike] = true;
                         count++;
-                        result[tmpW]=tmpB;
-                        if(count==workers.length)
+                        result[worker] = bike;
+                        if(count==availableWorker.length){
                             break;
+                        }
                     }
                 }
             }
         }
         return result;
+    }
+    private int calculateDistance(int[] bike, int[] worker){
+        return Math.abs(bike[0]-worker[0])+ Math.abs(bike[1]-worker[1]);
     }
 }
