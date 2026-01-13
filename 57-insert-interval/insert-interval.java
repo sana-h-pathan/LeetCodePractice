d2e78b8ec8609754c767e84a2b7f37dd985ca350
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-13 11:30:21 -0800

    Time: 1 ms (97.50%) | Memory: 47.2 MB (39.94%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -2,20 +2,17 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> result = new ArrayList<>();
         for(int[] interval: intervals){
-            // If the end of the new is before the start of the current interval
-            if(newInterval[1]<interval[0]){
-                result.add(newInterval);
-                newInterval=interval;
-            } 
-            // If the start of the new in after end of current interval
-            else if(newInterval[0]>interval[1]){
+            if(newInterval[0]>interval[1]){
                 result.add(interval);
+            } else if(interval[0]>newInterval[1]){
+                result.add(newInterval);
+                newInterval = interval;
             } else {
-                newInterval[0]=Math.min(interval[0], newInterval[0]);
-                newInterval[1]=Math.max(interval[1], newInterval[1]);
+                newInterval[0] = Math.min(newInterval[0],interval[0]);
+                newInterval[1] = Math.max(newInterval[1],interval[1]);
             }
         }
         result.add(newInterval);
-        return result.toArray(new int[0][]); 
+        return result.toArray(new int[0][]);
     }
 }
