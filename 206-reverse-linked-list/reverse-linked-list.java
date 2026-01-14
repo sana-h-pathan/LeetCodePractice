6dfe25da85a723b4e42a06b0f73925b16ccad71e
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-13 16:16:47 -0800

    Time: 0 ms (100.00%) | Memory: 44.5 MB (16.89%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -13,16 +13,16 @@ class Solution {
         if(head==null || head.next==null){
             return head;
         }
-        ListNode prev = null;
         ListNode curr = head;
+        ListNode prev = null;
         ListNode fast = head.next;
         while(fast!=null){
             curr.next = prev;
-            prev=curr;
-            curr=fast;
-            fast=fast.next;
+            prev = curr;
+            curr = fast;
+            fast = fast.next;
         }
-        curr.next = prev;
+        curr.next=prev;
         return curr;
     }
 }
