28a0072d38d679809180edadcacf98eebaa8b64c
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-15 12:05:55 -0800

    Time: 33 ms (32.54%) | Memory: 47.5 MB (29.49%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,32 +1,35 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result=new ArrayList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<p.length();i++){
+            char ch = p.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
         int l=0;
         int r=0;
-        HashMap<Character, Integer> map=new HashMap<>();
-        for(int i=0;i<p.length();i++){
-            char pChar=p.charAt(i);
-            map.put(pChar, map.getOrDefault(pChar, 0)+1);
-        }
         int matches=0;
         while(r<s.length()){
-            char rChar=s.charAt(r);
+            char rChar = s.charAt(r);
             if(map.containsKey(rChar)){
                 map.put(rChar, map.getOrDefault(rChar, 0)-1);
-                if(map.get(rChar)==0)
+                if(map.get(rChar)==0){
                     matches++;
+                }
             }
             if(r>=p.length()){
-                char lChar=s.charAt(l);
+                char lChar = s.charAt(l);
                 if(map.containsKey(lChar)){
-                    map.put(lChar,map.getOrDefault(lChar,0)+1);
-                    if(map.get(lChar)==1)
+                    map.put(lChar, map.getOrDefault(lChar, 0)+1);
+                    if(map.get(lChar)==1){
                         matches--;
+                    }
                 }
                 l++;
             }
-            if(map.size()==matches)
+            if(matches==map.size()){
                 result.add(l);
+            }
             r++;
         }
         return result;
