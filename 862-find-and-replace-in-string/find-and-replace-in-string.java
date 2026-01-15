3645fc83063a2cdee2fdbc9c474173a504f0d245
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-14 22:32:37 -0800

    Time: 2 ms (79.04%) | Memory: 43.5 MB (94.81%) - LeetSync

--- 862-find-and-replace-in-string/find-and-replace-in-string.java
+++ 862-find-and-replace-in-string/find-and-replace-in-string.java
@@ -1,24 +1,24 @@
 class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for (int i=0; i<indices.length; i++){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<indices.length;i++){
             int idx = indices[i];
             String src = sources[i];
-                if (s.startsWith(src, idx)){
-                    map.put(indices[i],i);
+            if(s.startsWith(src, idx)){
+                map.put(idx, i);
             }
-        }  
-        StringBuilder sb=new StringBuilder();
-        for (int i=0; i<s.length();){
-            if (!map.containsKey(i)){
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();){
+            if(!map.containsKey(i)){
                 sb.append(s.charAt(i));
                 i++;
-            } else { //replace chars
-                int index = map.get(i);
-                String source = sources[index];
-                String target = targets[index];
-                sb.append(target); // Replace with target
-                i += source.length(); // Skip over the source string
+            } else {
+                int idx = map.get(i);
+                String source = sources[idx];
+                String target = targets[idx];
+                sb.append(target);
+                i+=source.length();
             }
         }
         return sb.toString();
