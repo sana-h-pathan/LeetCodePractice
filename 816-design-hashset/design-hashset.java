f07b3d86ca8ba5ad4be5396650ba457aadc95772
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-16 11:57:41 -0800

    Time: 13 ms (77.89%) | Memory: 52.9 MB (45.48%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,49 +1,49 @@
 class MyHashSet {
     private int firstLevel;
     private int secondLevel;
-    private boolean[][] data;
-
+    private boolean[][] hashData;
     public MyHashSet() {
-        this.firstLevel=1000;
-        this.secondLevel=1000;
-        this.data=new boolean[firstLevel][];
+        this.firstLevel = 1000;
+        this.secondLevel = 1000;
+        this.hashData = new boolean[1000][];
     }
 
     private int firstHashing(int number){
         return number%firstLevel;
     }
-
     private int secondHashing(int number){
         return number/secondLevel;
     }
     
     public void add(int key) {
-        int firstHashingValue = firstHashing(key);
-        int secondHashingValue = secondHashing(key);
-        if(data[firstHashingValue]==null){
-            if(firstHashingValue==0){
-                data[firstHashingValue]=new boolean[secondLevel+1];
-            }else{
-                data[firstHashingValue]=new boolean[secondLevel];
+        int fKey = firstHashing(key);
+        int sKey = secondHashing(key);
+        if(hashData[fKey]==null){
+            if(fKey==0){
+                hashData[fKey] = new boolean[secondLevel+1];
+            } else {
+                hashData[fKey] = new boolean[secondLevel];
             }
         }
-        data[firstHashingValue][secondHashingValue]=true;
+        hashData[fKey][sKey]=true;
     }
     
     public void remove(int key) {
-        int firstHashingValue = firstHashing(key);
-        int secondHashingValue = secondHashing(key);
-        if(data[firstHashingValue]==null)
-            return;
-        data[firstHashingValue][secondHashingValue]=false;
+        int fKey = firstHashing(key);
+        int sKey = secondHashing(key);
+        if(hashData[fKey]!=null){
+            hashData[fKey][sKey]=false;
+        }
     }
     
     public boolean contains(int key) {
-        int firstHashingValue = firstHashing(key);
-        int secondHashingValue = secondHashing(key);
-        if(data[firstHashingValue]==null)
-            return false;
-        return data[firstHashingValue][secondHashingValue];
+        int fKey = firstHashing(key);
+        int sKey = secondHashing(key);
+        if(hashData[fKey]==null){
+           return false;
+        } else {
+            return hashData[fKey][sKey];
+        }
     }
 }
 
