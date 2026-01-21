9067e2d239e3f195aa7e90d1ff3ebe18bc6b825e
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-21 07:24:09 -0800

    Time: 3 ms (94.09%) | Memory: 45.2 MB (41.83%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -7,15 +7,18 @@ class Solution {
         for(int num: nums1){
             map.put(num, map.getOrDefault(num, 0)+1);
         }
-
-        int k = 0;
-        for (int n : nums2) {
-            int cnt = map.getOrDefault(n, 0);
-            if (cnt > 0) {
-                nums1[k++] = n;
-                map.put(n, cnt - 1);
+        List<Integer> result = new ArrayList<>();
+        for(int num: nums2){
+            if(map.containsKey(num)){
+                result.add(num);
+                map.put(num, map.getOrDefault(num, 0)-1);
+                map.remove(num, 0);
             }
         }
-        return Arrays.copyOfRange(nums1, 0, k);
+        int[] re = new int[result.size()];
+        for(int i=0; i<result.size(); i++){
+            re[i] = result.get(i);
+        }
+        return re;
     }
 }
