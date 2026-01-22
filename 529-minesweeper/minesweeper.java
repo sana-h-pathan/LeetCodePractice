07a6cec573f95bd57ddc64c99bf2f3aa4afc284f
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-22 15:11:04 -0800

    Time: 3 ms (30.85%) | Memory: 46.6 MB (94.05%) - LeetSync

--- 529-minesweeper/minesweeper.java
+++ 529-minesweeper/minesweeper.java
@@ -3,46 +3,43 @@ class Solution {
     int n;
     int[][] dirs;
     public char[][] updateBoard(char[][] board, int[] click) {
-        this.dirs=new int[][]{{1,0},{0,1},{1,1},{-1,-1},{-1,0},{0,-1},{1,-1},{-1,1}};
-        this.m=board.length;
-        this.n=board[0].length;
+        this.m = board.length;
+        this.n = board[0].length;
+        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1},{-1,-1},{1,1},{-1,1},{1,-1}};
         if(board[click[0]][click[1]]=='M'){
             board[click[0]][click[1]]='X';
             return board;
         }
-        Queue<int[]> bfsQue=new LinkedList<>();
-        bfsQue.add(click);
+        Queue<int[]> bfsQue = new LinkedList<>();
+        bfsQue.add(new int[]{click[0],click[1]});
         board[click[0]][click[1]]='B';
         while(!bfsQue.isEmpty()){
-            int[] curr=bfsQue.poll();
-            int i=curr[0];
-            int j=curr[1];
-            int minesAround=countMines(board, curr[0], curr[1]);
-            if(minesAround==0){
+            int[] curr = bfsQue.poll();
+            int mines = countMines(board, curr[0],curr[1]);
+            if(mines==0){
                 for(int[] dir: dirs){
-                    int nr=i+dir[0];
-                    int nc=j+dir[1];
+                    int nr = curr[0]+dir[0];
+                    int nc = curr[1]+dir[1];
                     if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='E'){
-                        bfsQue.add(new int[]{nr,nc});
                         board[nr][nc]='B';
+                        bfsQue.add(new int[]{nr, nc});
                     }
                 }
-            }else if(minesAround>0){
-                board[i][j]=(char)(minesAround+'0');
+            } else {
+                board[curr[0]][curr[1]]=(char)(mines+'0');
             }
         }
         return board;
     }
-
     private int countMines(char[][] board, int r, int c){
-        int mines=0;
+        int count=0;
         for(int[] dir: dirs){
-            int nr=r+dir[0];
-            int nc=c+dir[1];
+            int nr = dir[0]+r;
+            int nc = dir[1]+c;
             if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='M'){
-                mines++;
+                count++;
             }
         }
-        return mines;
+        return count;
     }
 }
