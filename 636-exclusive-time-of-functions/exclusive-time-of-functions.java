4cb9973c303f5e473aa1c10e7a4e6dc7b10b0e41
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-23 17:05:14 -0800

    Time: 13 ms (44.21%) | Memory: 47.3 MB (25.07%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.java
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         Stack<Integer> stk = new Stack<>();
-        int prev = 0;
-        int curr = 0;
         int[] result = new int[n];
-        for(String log: logs){
+        int curr = 0;
+        int prev = 0;
+        for(String log : logs){
             String[] parts = log.split(":");
             curr = Integer.parseInt(parts[2]);
             int idx = Integer.parseInt(parts[0]);
             if(parts[1].equals("start")){
                 if(!stk.isEmpty()){
-                    result[stk.peek()]+= curr-prev;
+                    result[stk.peek()]+=curr-prev;
                     prev = curr;
-                } 
+                }
                 stk.push(idx);
-            } 
-            else {
-                result[stk.pop()]+= curr+1-prev;
-                prev = curr+1;
+            } else {
+                result[stk.pop()]+=curr+1-prev;
+                prev= curr+1;
             }
         }
         return result;
