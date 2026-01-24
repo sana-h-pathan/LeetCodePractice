f55112f880b3b0a502ce2900397a59220b432088
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-24 12:50:45 -0800

    Time: 9 ms (94.70%) | Memory: 66.3 MB (19.89%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -3,46 +3,48 @@ class Solution {
         TrieNode[] children;
         boolean isEnd;
         public TrieNode(){
-            this.children=new TrieNode[26];
-            this.isEnd=false;
+            this.children = new TrieNode[26];
         }
     }
     TrieNode root;
-
     private void insert(String word){
-        TrieNode curr=root;
+        TrieNode curr = root;
         for(int i=0;i<word.length();i++){
-            char ch=word.charAt(i);
-            if(curr.children[ch-'a']==null)
-                curr.children[ch-'a']=new TrieNode();
-            curr=curr.children[ch-'a'];
+            char ch = word.charAt(i);
+            if(curr.children[ch-'a']==null){
+                curr.children[ch-'a'] = new TrieNode();
+            }
+            curr = curr.children[ch-'a'];
         }
-        curr.isEnd=true;
-
+        curr.isEnd = true;
     }
+
+
     public String replaceWords(List<String> dictionary, String sentence) {
-        this.root=new TrieNode();
+        this.root = new TrieNode();
         for(String word: dictionary){
             insert(word);
         }
-        StringBuilder sb=new StringBuilder();
-        String[] words=sentence.split(" ");
+        String[] words = sentence.split(" ");
+        StringBuilder replacementSentence = new StringBuilder();
         for(String word: words){
-            TrieNode curr=root;
-            StringBuilder replacementWord=new StringBuilder();
+            StringBuilder replacementWord = new StringBuilder();
+            TrieNode curr = root;
             for(int i=0;i<word.length();i++){
-                char ch=word.charAt(i);
-                if(curr.isEnd ||curr.children[ch - 'a']==null)
+                char ch = word.charAt(i);
+                if(curr.children[ch-'a']==null || curr.isEnd){
                     break;
-                curr=curr.children[ch-'a'];
+                }
                 replacementWord.append(ch);
+                curr = curr.children[ch-'a'];
             }
-            if(!curr.isEnd)
-                sb.append(word);
-            else
-                sb.append(replacementWord.toString());
-            sb.append(" ");
+            if(!curr.isEnd){
+                replacementSentence.append(word);
+            } else {
+                replacementSentence.append(replacementWord.toString());
+            }
+            replacementSentence.append(" ");
         }
-        return sb.toString().trim();
+        return replacementSentence.toString().trim();
     }
 }
