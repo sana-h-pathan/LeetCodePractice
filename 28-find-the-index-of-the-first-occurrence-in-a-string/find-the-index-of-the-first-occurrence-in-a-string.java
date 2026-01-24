fa8de557332c2c987fd53ecd81d79ae72e04fde4
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-23 19:44:49 -0800

    Time: 2 ms (14.35%) | Memory: 42.9 MB (72.68%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -5,32 +5,30 @@ class Solution {
         if(n>m){
             return -1;
         }
-        long nHash=0L;
-        long hHash=0L;
-        long k = 26L;
+        long k=26L;
         long pwr = 1L;
+        long nHash=0;
+        long hHash=0;
         for(int i=0;i<n;i++){
-            pwr*=k;
+            pwr = pwr*k;
         }
         for(int i=0;i<n;i++){
-            int ch = (needle.charAt(i)-'a')+1;
-            nHash = nHash*k+ch;
+            int ch = needle.charAt(i) - 'a'+1;
+            nHash=nHash*k+ch;
         }
         int l=0;
         int r=0;
-        while(r<m){
-            int in = (haystack.charAt(r)-'a')+1;
+        while(r<haystack.length()){
+            int in = haystack.charAt(r) - 'a'+1;
             hHash = hHash*k+in;
             if(r>=n){
-                int out = (haystack.charAt(l)-'a')+1;
-                hHash = hHash - (out*pwr);
-                
+                int out = haystack.charAt(l)-'a'+1;
+                hHash = hHash - (pwr*out);
                 l++;
             }
             if(nHash==hHash){
                 return l;
             }
-            
             r++;
         }
         return -1;
