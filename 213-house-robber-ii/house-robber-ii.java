cfa51a49e0ffaa9d03317415cd5257e3ce9765f7
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-25 13:47:58 -0800

    Time: 0 ms (100.00%) | Memory: 42.7 MB (84.62%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -7,18 +7,18 @@ class Solution {
         if(n==2){
             return Math.max(nums[0], nums[1]);
         }
-        int first = helper(nums, 0);
-        int second = helper(nums, 1);
+        int first = helper(nums,0);
+        int second = helper(nums,1);
         return Math.max(first, second);
     }
     private int helper(int[] nums, int s){
         int n = nums.length-1;
         int[] dp = new int[n];
-        dp[0]=nums[s];
+        dp[0] = nums[s];
         dp[1] = Math.max(dp[0], nums[s+1]);
         for(int i=2;i<n;i++){
             dp[i] = Math.max(dp[i-1], nums[s+i]+dp[i-2]);
-        }
+        } 
         return dp[n-1];
     }
 }
