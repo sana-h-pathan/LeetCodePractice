80f158c0b11adf5d76328da50dc29a3251cec906
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-24 18:47:49 -0800

    Time: 11 ms (69.21%) | Memory: 47.4 MB (10.24%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.java
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -1,49 +1,44 @@
 class Solution {
-    class TrieNode {
-        boolean isEnd;
+    class TrieNode{
         TrieNode[] children;
-        public TrieNode() {
-            children = new TrieNode[26];
-            //isEnd = false;
+        boolean isEnd;
+        public TrieNode(){
+            this.children = new TrieNode[26];
         }
     }
     TrieNode root;
-
-    public void insert(String word) {
-        TrieNode curr = root;
-        for(int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (curr.children[c - 'a'] == null) {
-                curr.children[c - 'a'] = new TrieNode();
-            }
-            curr = curr.children[c - 'a'];
-        }
-        curr.isEnd = true;
-    }
     public String longestWord(String[] words) {
         this.root = new TrieNode();
-        if (words == null || words.length == 0) 
-            return "";
-        for (String word : words) {
+        for(String word: words){
             insert(word);
         }
-        Queue<TrieNode> q=new LinkedList<>();
-        Queue<String> sq=new LinkedList<>();
-        q.add(root);
-        sq.add("");
+        Queue<TrieNode> bfsQue = new LinkedList<>();
+        Queue<String> wQue = new LinkedList<>();
+        wQue.add("");
         String maxStr="";
-        while(!q.isEmpty()){
-            TrieNode curr=q.poll();
-            maxStr=sq.poll();
-            for(int i=25; i>=0;i--){
-                TrieNode child=curr.children[i];
+        bfsQue.add(root);
+        while(!bfsQue.isEmpty()){
+            TrieNode curr = bfsQue.poll();
+            maxStr = wQue.poll();
+            for(int i=25;i>=0;i--){
+                TrieNode child = curr.children[i];
                 if(child!=null && child.isEnd){
-                    q.add(child);
-                    sq.add(maxStr + (char)('a' + i));
+                    bfsQue.add(child);
+                    wQue.add(maxStr + (char)('a' + i));
                 }
-            }   
+            }
         }
         return maxStr;
     }
-    
+    private void insert(String word){
+        TrieNode curr = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(curr.children[ch-'a']==null){
+                curr.children[ch-'a'] = new TrieNode();
+            }
+            curr = curr.children[ch-'a'];
+        }
+        curr.isEnd=true;
+    }
 }
