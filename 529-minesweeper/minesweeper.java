057f99ef08b391f74fa44670face37786d50da9b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-26 07:45:36 -0800

    Time: 3 ms (30.80%) | Memory: 46.8 MB (81.00%) - LeetSync

--- 529-minesweeper/minesweeper.java
+++ 529-minesweeper/minesweeper.java
@@ -5,7 +5,7 @@ class Solution {
     public char[][] updateBoard(char[][] board, int[] click) {
         this.m = board.length;
         this.n = board[0].length;
-        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1},{-1,-1},{1,1},{-1,1},{1,-1}};
+        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
         if(board[click[0]][click[1]]=='M'){
             board[click[0]][click[1]]='X';
             return board;
@@ -15,7 +15,7 @@ class Solution {
         board[click[0]][click[1]]='B';
         while(!bfsQue.isEmpty()){
             int[] curr = bfsQue.poll();
-            int mines = countMines(board, curr[0],curr[1]);
+            int mines = countMines(board, curr[0], curr[1]);
             if(mines==0){
                 for(int[] dir: dirs){
                     int nr = curr[0]+dir[0];
@@ -23,19 +23,21 @@ class Solution {
                     if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='E'){
                         board[nr][nc]='B';
                         bfsQue.add(new int[]{nr, nc});
+
                     }
                 }
             } else {
-                board[curr[0]][curr[1]]=(char)(mines+'0');
+                board[curr[0]][curr[1]] = (char)(mines+'0');
             }
         }
         return board;
     }
-    private int countMines(char[][] board, int r, int c){
+
+    private int countMines(char[][] board, int i, int j){
         int count=0;
-        for(int[] dir: dirs){
-            int nr = dir[0]+r;
-            int nc = dir[1]+c;
+        for(int[] dir : dirs){
+            int nr = dir[0]+i;
+            int nc = dir[1]+j;
             if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='M'){
                 count++;
             }
