fe48958eb80df15a263fcb8649954afa6d592924
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-25 16:41:05 -0800

    Time: 176 ms (80.85%) | Memory: 276.2 MB (46.85%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -6,54 +6,67 @@ class WordDictionary {
             this.children = new TrieNode[26];
         }
     }
+
     TrieNode root;
+
     public WordDictionary() {
         this.root = new TrieNode();
     }
-    
+
     public void addWord(String word) {
         TrieNode curr = root;
         for(int i=0;i<word.length();i++){
             char ch = word.charAt(i);
             if(curr.children[ch-'a']==null){
-                curr.children[ch-'a']= new TrieNode();
+                curr.children[ch-'a'] = new TrieNode();
             }
             curr = curr.children[ch-'a'];
         }
         curr.isEnd = true;
     }
-    
+
     public boolean search(String word) {
-        TrieNode curr = root;
-        return helper(curr, word, 0);
+        return helper(word, root, 0);
     }
 
-    private boolean helper(TrieNode curr, String word, int idx) {
-        if(idx==word.length()){
+    private boolean helper(String word, TrieNode curr, int idx){
+        if(idx == word.length()){
             return curr.isEnd;
         }
+
         char ch = word.charAt(idx);
-        if(ch=='.'){
+
+        if(ch == '.'){
             for(int i=0;i<26;i++){
                 TrieNode child = curr.children[i];
-                if(child!=null && helper(child, word, idx+1)){
+                if(child != null && helper(word, child, idx + 1)){
                     return true;
                 }
             }
             return false;
-        } else {
-            TrieNode child = curr.children[ch -'a'];
-            if(child==null){
-                return false;
-            }
-            return helper(child, word, idx+1);
         }
+
+        if(ch == '*'){
+            // Option 1: '*' matches zero characters
+            if (helper(word, curr, idx + 1)) {
+                return true;
+            }
+
+            // Option 2: '*' matches one or more characters
+            // consume one char AND move forward in pattern
+            for(int i=0;i<26;i++){
+                TrieNode child = curr.children[i];
+                if(child != null && helper(word, child, idx + 1)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        TrieNode child = curr.children[ch - 'a'];
+        if(child == null){
+            return false;
+        }
+        return helper(word, child, idx + 1);
     }
 }
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary obj = new WordDictionary();
- * obj.addWord(word);
- * boolean param_2 = obj.search(word);
- */
