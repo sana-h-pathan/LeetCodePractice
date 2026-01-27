eee12119b34f67831d72433d3388ffe67ba942ac
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-26 23:53:47 -0800

    Time: 0 ms (100.00%) | Memory: 43 MB (27.82%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,21 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n==1)
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        if(nums.length==1)
             return nums[0];
-        int[] dp = new int[n];
-        
-        dp[0]=nums[0];
-        dp[1] = Math.max(nums[1], nums[0]);
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
         for(int i=2;i<nums.length;i++){
-            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
+            dp[i] = Math.max(dp[i-1], nums[i]+dp[i-2]);
         }
-        return dp[n-1];
+        return dp[nums.length-1];
     }
 
     public int rob1(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
+        int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
         return helper(nums, 0, dp);
     }
@@ -23,17 +21,16 @@ class Solution {
         if(idx==nums.length-1){
             return nums[idx];
         }
-        if(idx>nums.length-1){
+        if(idx==nums.length)
             return 0;
-        }
         if(dp[idx]!=-1){
             return dp[idx];
         }
-        int pick = nums[idx]+helper(nums, idx+2,dp);
-        int notPick = 0+helper(nums, idx+1,dp);
-        return dp[idx]=Math.max(pick, notPick);
+        int notPick = helper(nums, idx+1);
+        int pick = nums[idx]+ helper(nums, idx+2);
+        dp[idx] = Math.max(pick, notPick);
+        return dp[idx];
     }
-
 
     public int rob2(int[] nums) {
         return helper(nums, 0);
@@ -42,11 +39,10 @@ class Solution {
         if(idx==nums.length-1){
             return nums[idx];
         }
-        if(idx>nums.length-1){
+        if(idx==nums.length)
             return 0;
-        }
-        int pick = nums[idx]+helper(nums, idx+2);
-        int notPick = 0+helper(nums, idx+1);
+        int notPick = helper(nums, idx+1);
+        int pick = nums[idx]+ helper(nums, idx+2);
         return Math.max(pick, notPick);
     }
 }
