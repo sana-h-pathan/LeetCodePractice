a53e6c670546a07b7204d1764e809210d0a1bc2d
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-27 08:15:09 -0800

    Time: 1 ms (83.29%) | Memory: 42.5 MB (97.75%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -2,17 +2,17 @@ class Solution {
     public String decodeString(String s) {
         Stack<Integer> numStk = new Stack<>();
         Stack<StringBuilder> strStk = new Stack<>();
+        int currNum=0;
         StringBuilder currStr = new StringBuilder();
-        int currNum = 0;
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             if(Character.isDigit(ch)){
                 currNum = currNum*10+ch-'0';
             } else if(ch=='['){
-                numStk.add(currNum);
-                strStk.add(currStr);
-                currNum = 0;
+                numStk.push(currNum);
+                strStk.push(currStr);
                 currStr = new StringBuilder();
+                currNum = 0;
             } else if(ch==']'){
                 int currItr = numStk.pop();
                 StringBuilder ns = new StringBuilder();
