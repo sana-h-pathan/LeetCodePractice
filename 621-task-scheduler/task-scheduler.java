634e2b1dc91e89040c52cb0006bd89cf17fe6542
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-29 08:33:41 -0800

    Time: 20 ms (61.64%) | Memory: 47.3 MB (98.74%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,22 +1,26 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int l=tasks.length;
-        HashMap<Character, Integer> map=new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        //A=4, B=4, C=3 n=2
+        //AB_AB_AB_AB
         int maxFreq=0;
+        for(char t: tasks){
+            map.put(t, map.getOrDefault(t, 0)+1);
+            maxFreq = Math.max(map.get(t), maxFreq);
+        }
         int maxCount=0;
-        for(char task: tasks){
-            map.put(task, map.getOrDefault(task, 0)+1);
-            maxFreq=Integer.max(maxFreq, map.get(task));
-        }
-        for(char task: map.keySet()){
-            if(map.get(task)==maxFreq)
+        for(char ch: map.keySet()){
+            if(map.get(ch)==maxFreq){
                 maxCount++;
+            }
         }
-        int partition=maxFreq-1;
-        int available=partition*(n-(maxCount-1));
-        int pending=l-maxCount*maxFreq;
-        int idle=Math.max(0, available-pending);
-
-        return l+idle;
+        int totalTask = tasks.length;
+        int partition = maxFreq-1;
+        int availableSlot = partition*(n-(maxCount-1));
+        int pendingTask = totalTask-(maxFreq*maxCount);
+        int idleSlot = Math.max(0, availableSlot-pendingTask);
+        
+        return totalTask+idleSlot;
     }
+    
 }
