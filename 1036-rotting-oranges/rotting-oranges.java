e673c0262545744418a76bb8069146cc531776f3
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-29 11:31:10 -0800

    Time: 2 ms (90.77%) | Memory: 44.1 MB (66.66%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,20 +1,20 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
         int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        Queue<int[]> bfsQue = new LinkedList<>();
         int freshOranges=0;
-        int minutes=0;
+        Queue<int[]> bfsQue = new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    bfsQue.add(new int[]{i,j});
-                } else if(grid[i][j]==1){
+                if(grid[i][j]==1){
                     freshOranges++;
+                } else if(grid[i][j]==2){
+                    bfsQue.add(new int[]{i,j});
                 }
             }
         }
+        int minutes=0;
         if(freshOranges==0){
             return minutes;
         }
@@ -24,15 +24,15 @@ class Solution {
             for(int i=0;i<size;i++){
                 int[] curr = bfsQue.poll();
                 for(int[] dir: dirs){
-                    int nr = dir[0]+curr[0];
-                    int nc = dir[1]+curr[1];
+                    int nr = curr[0]+dir[0];
+                    int nc = curr[1]+dir[1];
                     if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
                         grid[nr][nc]=2;
+                        bfsQue.add(new int[]{nr, nc});
                         freshOranges--;
                         if(freshOranges==0){
                             return minutes;
                         }
-                        bfsQue.add(new int[]{nr, nc});
                     }
                 }
             }
