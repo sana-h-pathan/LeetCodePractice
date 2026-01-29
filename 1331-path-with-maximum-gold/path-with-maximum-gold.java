dec57165c4a152e8393559e266cb3d6fd0883f15
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-28 23:34:22 -0800

    Time: 78 ms (44.47%) | Memory: 42.8 MB (89.99%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,32 +1,32 @@
 class Solution {
-    int[][] dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+    int[][] dirs;
     int m;
     int n;
-    int result=0;
     public int getMaximumGold(int[][] grid) {
-        this.m=grid.length;
-        this.n=grid[0].length;
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        int result = Integer.MIN_VALUE;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if (grid[i][j] != 0) 
-                    result=Math.max(result,dfs(grid,i,j));
+                result = Math.max(result, helper(grid, i, j));
             }
         }
         return result;
     }
 
-    private int dfs(int[][] grid, int r, int c){
-        int gold=grid[r][c];
-        grid[r][c]=0;
-        int best = 0;
+    private int helper(int[][] grid, int i, int j){
+        int currGold = grid[i][j];
+        grid[i][j]=0;
+        int best=0;
         for(int[] dir: dirs){
-            int nr=dir[0]+r;
-            int nc=dir[1]+c;
+            int nr = dir[0]+i;
+            int nc = dir[1]+j;
             if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]!=0){
-                best = Math.max(best, dfs(grid, nr, nc));
-            } 
+                best = Math.max(best, helper(grid, nr, nc));
+            }
         }
-        grid[r][c]=gold;
-        return gold + best;
+        grid[i][j]=currGold;
+        return best+currGold;
     }
 }
