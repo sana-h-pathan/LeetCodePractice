80bdfe4e20e7280281350bb4c99d636e56eec539
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-29 08:13:33 -0800

    Time: 128 ms (91.23%) | Memory: 109.3 MB (14.73%) - LeetSync

---
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -0,0 +1,43 @@
+class TimeMap {
+    HashMap<String, ArrayList<Pair<Integer, String>>> map;
+
+    public TimeMap() {
+        this.map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key)){
+            map.put(key, new ArrayList<>());
+        }
+        map.get(key).add(new Pair(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key)){
+            return "";
+        }
+        List<Pair<Integer, String>> list = map.get(key);
+        int low =0;
+        int high = list.size();
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if(list.get(mid).getKey()<=timestamp){
+                low = mid+1;
+            } else {
+                high = mid;
+            }
+        }
+        if(high==0){
+            return "";
+        }
+        
+        return list.get(high-1).getValue();
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
