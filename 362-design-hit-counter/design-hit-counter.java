408aebfefdd1d6da087b734e0efd93068ccba4a0
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-29 08:04:19 -0800

    Time: 1 ms (93.29%) | Memory: 42.9 MB (52.76%) - LeetSync

---
+++ 362-design-hit-counter/design-hit-counter.java
@@ -0,0 +1,26 @@
+class HitCounter {
+    Queue<Integer> q;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        q = new LinkedList<>();
+    }
+    
+    public void hit(int timestamp) {
+        q.add(timestamp);
+    }
+    
+    public int getHits(int timestamp) {
+        
+        while(!q.isEmpty() && timestamp - q.peek()>= 300)
+            q.poll();
+            
+        return q.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
