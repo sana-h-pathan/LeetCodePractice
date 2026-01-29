0f9f1fb96b4ae02ef048b50f30b8d7eed94f2bfd
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-28 23:29:03 -0800

    Time: 73 ms (50.82%) | Memory: 43 MB (50.24%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -16,17 +16,17 @@ class Solution {
     }
 
     private int dfs(int[][] grid, int r, int c){
-        int val=grid[r][c];
-        int temp=grid[r][c];
+        int gold=grid[r][c];
         grid[r][c]=0;
+        int best = 0;
         for(int[] dir: dirs){
             int nr=dir[0]+r;
             int nc=dir[1]+c;
             if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]!=0){
-                val = Math.max(val, temp + dfs(grid, nr, nc));
+                best = Math.max(best, dfs(grid, nr, nc));
             } 
         }
-        grid[r][c]=temp;
-        return val;
+        grid[r][c]=gold;
+        return gold + best;
     }
 }
