928ef3cae81674c8e9cb773b4277e800ec030048
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-29 00:12:33 -0800

    Time: 35 ms (35.55%) | Memory: 51.2 MB (24.58%) - LeetSync

---
+++ 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestBeautifulSubstring(String word) {
+        if (word.length() < 5) 
+            return 0;
+
+        int maxLength = 0;
+        int vowelCount=1;
+        int l=0;
+        int r=1;
+        
+        while(r<word.length()){
+            if(word.charAt(r) < word.charAt(r-1)){
+                l=r; 
+                vowelCount=1;
+            } else if(word.charAt(r) != word.charAt(r-1)){
+                vowelCount++;
+            }
+            if(vowelCount==5){
+                maxLength = Math.max(maxLength, r-l+1);
+            }
+            r++;
+        }
+        return maxLength;
+    }
+}
