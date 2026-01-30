b5b3a6f4b976ddb12be75d1e471fd02109984adc
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-30 15:39:19 -0800

    Time: 13 ms (88.19%) | Memory: 47.2 MB (47.55%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -3,29 +3,33 @@ class Solution {
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         int m = grid.length;
         int n = grid[0].length;
-        Deque<int[]> deque = new ArrayDeque<>();
+        Deque<int[]> dq = new ArrayDeque<>();
         int[][] minCost = new int[m][n];
-        for(int[] r : minCost){
-            Arrays.fill(r, Integer.MAX_VALUE);
+        for(int[] row: minCost){
+            Arrays.fill(row, Integer.MAX_VALUE);
         }
-        deque.offerFirst(new int[]{0,0});
+        dq.offerFirst(new int[]{0,0});
         minCost[0][0]=0;
-        while(!deque.isEmpty()){
-            int[] curr = deque.pollFirst();
+
+        while(!dq.isEmpty()){
+            int[] curr = dq.pollFirst();
+            int r = curr[0];
+            int c = curr[1];
             for(int i=0;i<dirs.length;i++){
-                int nr = dirs[i][0]+curr[0];
-                int nc = dirs[i][1]+curr[1];
+                int nr = r+dirs[i][0];
+                int nc = c+dirs[i][1];
                 int cost=0;
-                if(grid[curr[0]][curr[1]]!=i+1){
+                if(grid[r][c]!=i+1){
                     cost=1;
                 }
                 if(nr>=0 && nc>=0 && nr<m && nc<n){
-                    if(minCost[curr[0]][curr[1]]+cost<minCost[nr][nc]){
-                        minCost[nr][nc]= minCost[curr[0]][curr[1]]+cost;
+                    if(minCost[r][c]+cost<minCost[nr][nc]){
+                        minCost[nr][nc] = minCost[r][c]+cost;
+
                         if(cost==0){
-                            deque.offerFirst(new int[]{nr,nc});
+                            dq.offerFirst(new int[]{nr,nc});
                         } else {
-                            deque.offerLast(new int[]{nr, nc});
+                            dq.offerLast(new int[]{nr, nc});
                         }
                     }
                 }
