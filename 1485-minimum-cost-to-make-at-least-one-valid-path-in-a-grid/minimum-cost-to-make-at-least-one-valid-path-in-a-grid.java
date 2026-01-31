232fcf10827b250fb14d508b9d81f036c227970e
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-30 16:09:45 -0800

    Time: 13 ms (88.19%) | Memory: 46.9 MB (87.73%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -3,38 +3,37 @@ class Solution {
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         int m = grid.length;
         int n = grid[0].length;
-        int[][] minCost = new int[m][n];
         Deque<int[]> deque = new ArrayDeque<>();
-        for(int[] row: minCost){
+        int[][] mCost = new int[m][n];
+        for(int[] row: mCost){
             Arrays.fill(row, Integer.MAX_VALUE);
         }
         deque.addFirst(new int[]{0,0});
-        minCost[0][0]=0;
+        mCost[0][0]=0;
         while(!deque.isEmpty()){
-            int[] curr = deque.pollFirst();
+            int[] curr = deque.poll();
             int r = curr[0];
             int c = curr[1];
-            
-            for(int i =0;i<4;i++){
+            for(int i =0;i<dirs.length;i++){
                 int nr = dirs[i][0]+r;
                 int nc = dirs[i][1]+c;
-                int cost =0;
+                int cost = 0;
                 if(grid[r][c]!=i+1){
-                    cost=1;
+                    cost = 1;
                 }
                 if(nr>=0 && nc>=0 && nr<m && nc<n){
-                    if(minCost[r][c]+cost<minCost[nr][nc]){
-                        minCost[nr][nc]=minCost[r][c]+cost;
+                    if(mCost[r][c]+cost<mCost[nr][nc]){
+                        mCost[nr][nc] = mCost[r][c]+cost;
 
                         if(cost==0){
-                            deque.addFirst(new int[]{nr,nc});
+                            deque.addFirst(new int[]{nr, nc});
                         } else {
-                            deque.addLast(new int[]{nr,nc});
+                            deque.addLast(new int[]{nr, nc});
                         }
                     }
                 }
             }
         }
-        return minCost[m-1][n-1];
+        return mCost[m-1][n-1];
     }
 }
