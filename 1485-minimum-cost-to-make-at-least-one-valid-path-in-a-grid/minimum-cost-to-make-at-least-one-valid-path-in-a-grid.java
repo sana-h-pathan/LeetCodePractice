bcbbb6a67d1e5d7f3ef2b0f5ae1cd9a5bbffa1bc
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-30 16:02:15 -0800

    Time: 13 ms (88.19%) | Memory: 47.1 MB (63.34%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -4,30 +4,32 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] minCost = new int[m][n];
-        for(int[] row : minCost){
+        Deque<int[]> deque = new ArrayDeque<>();
+        for(int[] row: minCost){
             Arrays.fill(row, Integer.MAX_VALUE);
         }
-        Deque<int[]> deque = new ArrayDeque<>();
         deque.addFirst(new int[]{0,0});
         minCost[0][0]=0;
         while(!deque.isEmpty()){
             int[] curr = deque.pollFirst();
             int r = curr[0];
             int c = curr[1];
-            for(int i=0;i<4;i++){
+            
+            for(int i =0;i<4;i++){
                 int nr = dirs[i][0]+r;
                 int nc = dirs[i][1]+c;
-                int cost=0;
+                int cost =0;
                 if(grid[r][c]!=i+1){
-                    cost = 1;
+                    cost=1;
                 }
                 if(nr>=0 && nc>=0 && nr<m && nc<n){
                     if(minCost[r][c]+cost<minCost[nr][nc]){
                         minCost[nr][nc]=minCost[r][c]+cost;
+
                         if(cost==0){
                             deque.addFirst(new int[]{nr,nc});
                         } else {
-                            deque.addLast(new int[]{nr, nc});
+                            deque.addLast(new int[]{nr,nc});
                         }
                     }
                 }
