8cdaf5a05c9ec6a45e2a314e28f0a032154a39d6
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-31 09:30:48 -0800

    Time: 2 ms (97.54%) | Memory: 43.4 MB (36.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stk = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
+        for(char ch:s.toCharArray()){
             if(ch=='('){
                 stk.push(')');
             } else if(ch=='['){
@@ -10,7 +9,7 @@ class Solution {
             } else if(ch=='{'){
                 stk.push('}');
             } else {
-                if(stk.isEmpty() || stk.pop()!=ch){
+                if(stk.isEmpty() || ch!=stk.pop()){
                     return false;
                 }
             }
