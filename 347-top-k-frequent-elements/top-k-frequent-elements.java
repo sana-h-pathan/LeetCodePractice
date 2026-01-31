ecafef0a6cecc56c4c10b7781c2f4d2d81e655de
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-31 00:26:56 -0800

    Time: 17 ms (20.00%) | Memory: 47.8 MB (29.20%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[k];
         for(int num: nums){
             map.put(num, map.getOrDefault(num, 0)+1);
         }
@@ -12,8 +11,9 @@ class Solution {
                 pq.poll();
             }
         }
+        int[] result = new int[k];
         for(int i=0;i<k;i++){
-            result[i] = pq.poll();
+            result[i]=pq.poll();
         }
         return result;
     }
