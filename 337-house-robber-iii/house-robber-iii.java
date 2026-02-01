c2a7f6e41c520b87ead88a0870a74549fa488967
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-01-31 22:24:35 -0800

    Time: 0 ms (100.00%) | Memory: 46.6 MB (68.07%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -19,13 +19,15 @@ class Solution {
         return Math.max(result[0], result[1]);
     }
     private int[] helper(TreeNode root){
-        if(root==null)
+        if(root==null){
             return new int[]{0,0};
+        }
         int[] left = helper(root.left);
         int[] right = helper(root.right);
 
         int rob = root.val+left[1]+right[1];
         int notRob = Math.max(left[0],left[1])+Math.max(right[0], right[1]);
-        return new int[]{rob, notRob};
+        return new int[]{rob, notRob}; 
     }
 }
+
