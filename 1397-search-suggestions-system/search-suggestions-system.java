61e1680021e3bdb0f2ff1fa96846ab8d159d7a5f
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-01 10:20:41 -0800

    Time: 14 ms (72.99%) | Memory: 48.7 MB (81.43%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -7,12 +7,11 @@ class Solution {
             this.wordList = new ArrayList<>();
         }
     }
-    TrieNode root = new TrieNode();
     private void insert(String word){
         TrieNode curr = root;
         for(int i=0;i<word.length();i++){
             char ch = word.charAt(i);
-            if(curr.children[ch-'a']==null){
+            if(curr.children[ch-'a'] == null){
                 curr.children[ch-'a'] = new TrieNode();
             }
             curr = curr.children[ch-'a'];
@@ -21,21 +20,23 @@ class Solution {
             }
         }
     }
+    TrieNode root;
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        List<List<String>> result = new ArrayList<>();
+        this.root = new TrieNode();
         Arrays.sort(products);
         for(String p: products){
             insert(p);
         }
         TrieNode curr = root;
-        boolean found = false;
+        List<List<String>> result = new ArrayList<>();
+        boolean dead = false;
         for(int i=0;i<searchWord.length();i++){
             char ch = searchWord.charAt(i);
-            if(!found && curr.children[ch-'a']!=null){
+            if(!dead && curr.children[ch-'a']!=null){
                 curr = curr.children[ch-'a'];
-                result.add(curr.wordList); 
+                result.add(curr.wordList);
             } else {
-                found = true;
+                dead = true;
                 result.add(Collections.emptyList());
             }
         }
