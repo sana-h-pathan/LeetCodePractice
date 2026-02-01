c5363bf9f04db48079b6e8d7b5c457d6620ba418
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-01 12:41:34 -0800

    Time: 5 ms (24.48%) | Memory: 46.5 MB (39.35%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,20 +1,32 @@
 class Solution {
     public String intToRoman(int num) {
-        String[][] symbols=new String[][]{
-            {"I","1"},{"IV","4"},{"V","5"},{"IX","9"},{"X","10"},{"XL","40"},
-            {"L","50"},{"XC","90"},{"C","100"},{"CD","400"},{"D","500"},{"CM","900"},{"M","1000"}
+        String[][] symbols = new String[][]{
+            {"I","1"},
+            {"IV","4"},
+            {"V","5"},
+            {"IX","9"},
+            {"X","10"},
+            {"XL","40"},
+            {"L","50"},
+            {"XC","90"},
+            {"C","100"},
+            {"CD","400"},
+            {"D","500"},
+            {"CM","900"},
+            {"M","1000"}
         };
-        StringBuilder sb=new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for(int i=symbols.length-1;i>=0;i--){
-            String sym=symbols[i][0];
-            int currNum=Integer.parseInt(symbols[i][1]);
-            int count=num/currNum;
+            String[] sym = symbols[i];
+            int currNum = Integer.parseInt(sym[1]);
+            String currSym = sym[0];
+            int count = num/currNum;
             while(count>0){
-                sb.append(sym);
+                sb.append(currSym);
                 count--;
             }
-            num=num%currNum;
-        } 
-        return sb.toString();       
+            num = num%currNum;
+        }
+        return sb.toString();
     }
 }
