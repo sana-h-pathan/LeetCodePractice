6fd7db488f5b509fa2380463a1096dfb9aac6289
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-02 11:40:41 -0800

    Time: 12 ms (86.13%) | Memory: 47.4 MB (28.21%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.java
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.java
@@ -21,27 +21,4 @@ class Solution {
         }
         return result;
     }
-
-    public int[] exclusiveTime1(int n, List<String> logs) {
-        Stack<Integer> stk = new Stack<>();
-        int[] result = new int[n];
-        int curr = 0;
-        int prev = 0;
-        for(String log : logs){
-            String[] parts = log.split(":");
-            curr = Integer.parseInt(parts[2]);
-            int idx = Integer.parseInt(parts[0]);
-            if(parts[1].equals("start")){
-                if(!stk.isEmpty()){
-                    result[stk.peek()]+=curr-prev;
-                    prev = curr;
-                }
-                stk.push(idx);
-            } else {
-                result[stk.pop()]+=curr+1-prev;
-                prev= curr+1;
-            }
-        }
-        return result;
-    }
 }
