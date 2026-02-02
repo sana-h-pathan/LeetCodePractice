194d321fba9c776662989c03df120c8fdb3daa2c
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-02 10:45:36 -0800

    Time: 2 ms (98.49%) | Memory: 48.3 MB (27.15%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -3,15 +3,15 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
         int[] result = new int[m*n];
-        int idx =0;
-        boolean dir = true;
+        int idx=0;
+        boolean dir=true;
         int i=0;
         int j=0;
-        while(idx<m*n){
+        while(idx<result.length){
             result[idx++]=mat[i][j];
             if(dir){
                 if(j==n-1){
-                    dir = false;
+                    dir=false;
                     i++;
                 } else if(i==0){
                     dir = false;
@@ -22,14 +22,14 @@ class Solution {
                 }
             } else {
                 if(i==m-1){
-                    dir = true;
                     j++;
-                } else if(j==0){
                     dir=true;
+                } else if(j==0){
                     i++;
+                    dir=true;
                 } else {
-                    j--;
                     i++;
+                    j--;
                 }
             }
         }
