f7805eb2e2a4e377ca3088ac3c7c2f695f0f5826
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-02 13:03:16 -0800

    Time: 20 ms (41.54%) | Memory: 46.7 MB (64.09%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,40 +1,54 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(t.length()>s.length()){
+        if (s == null || t == null || t.length() > s.length()) {
             return "";
         }
+
         HashMap<Character, Integer> map = new HashMap<>();
-        for(char ch: t.toCharArray()){
-            map.put(ch, map.getOrDefault(ch, 0)+1);
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        int l=0;
-        int r=0;
-        int reqdMatches=t.length();
-        int currMatches=0;
-        int sIdx=-1;
+
+        int l = 0;
+        int r = 0;
+        int reqdMatches = t.length();
+        int currMatches = 0;
+
+        int sIdx = -1;
         int minLength = Integer.MAX_VALUE;
-        while(r<s.length()){
+
+        while (r < s.length()) {
             char rChar = s.charAt(r);
-            if(map.getOrDefault(rChar, 0)>0){
-                currMatches++;
-            }
-            map.put(rChar, map.getOrDefault(rChar, 0)-1);
-            while(currMatches==reqdMatches){
-                if(r-l+1<minLength){
-                    minLength = r-l+1;
-                    sIdx=l;
+
+            // CHANGED: only update if rChar is part of t
+            if (map.containsKey(rChar)) {
+                if (map.get(rChar) > 0) {
+                    currMatches++;
                 }
+                map.put(rChar, map.get(rChar) - 1);
+            }
+
+            while (currMatches == reqdMatches) {
+                if (r - l + 1 < minLength) {
+                    minLength = r - l + 1;
+                    sIdx = l;
+                }
+
                 char lChar = s.charAt(l);
-                map.put(lChar, map.getOrDefault(lChar, 0)+1);
-                if(map.get(lChar)>0){
-                    currMatches--;
+
+                // CHANGED: only update if lChar is part of t
+                if (map.containsKey(lChar)) {
+                    map.put(lChar, map.get(lChar) + 1);
+                    if (map.get(lChar) > 0) {
+                        currMatches--;
+                    }
                 }
                 l++;
             }
             r++;
         }
-        if(sIdx==-1)
-            return "";
-        return s.substring(sIdx, sIdx+minLength);
+
+        if (sIdx == -1) return "";
+        return s.substring(sIdx, sIdx + minLength);
     }
 }
