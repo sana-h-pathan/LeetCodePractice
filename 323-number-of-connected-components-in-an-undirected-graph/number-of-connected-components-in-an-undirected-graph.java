b717507a7e657ce197b2f22c33ade6c4763a62a0
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-03 21:49:41 -0800

    Time: 1 ms (100.00%) | Memory: 46.2 MB (94.98%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,37 +1,28 @@
 class Solution {
+    int[] parents;
+   
     public int countComponents(int n, int[][] edges) {
-        List<List<Integer>> adjacencyList=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adjacencyList.add(new ArrayList<>());
+        parents = new int[n];
+        for(int i = 0;i<n;i++){
+            parents[i] = i;
         }
-        for(int[] edge: edges){
-            adjacencyList.get(edge[0]).add(edge[1]);
-            adjacencyList.get(edge[1]).add(edge[0]);
-        }
-        boolean[] visited=new boolean[n];
-        int countComponenent=0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                 visited[i]=true;
-                countComponenent++;
-                bfs(adjacencyList, i,visited);
+        
+        for(int[] e : edges){
+            int a = find(e[0]);
+            int b = find(e[1]);
+            if(a !=b){
+                parents[a] = b;
+                n--;
             }
         }
-        return countComponenent;
+        return n;
     }
-
-    private void bfs(List<List<Integer>> adjacencyList, int node, boolean[] visited){
-        Queue<Integer> bfsQue=new LinkedList<>();
-        bfsQue.add(node);
-        while(!bfsQue.isEmpty()){
-            int curr=bfsQue.poll();
-            List<Integer> adjList=adjacencyList.get(curr);
-            for(Integer adj: adjList){
-                if(!visited[adj]){
-                    visited[adj]=true;
-                    bfsQue.add(adj);
-                }
-            }
+    
+    private int find(int id){
+        if(id==parents[id]){
+            return id;
         }
+        parents[id]=find(parents[id]);
+        return parents[id];
     }
 }
