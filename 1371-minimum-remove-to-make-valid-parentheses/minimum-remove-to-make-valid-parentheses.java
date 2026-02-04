d6647696e5cc3184127fc3a2173643ad2e8c45f3
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-03 22:37:09 -0800

    Time: 32 ms (16.92%) | Memory: 47.8 MB (24.07%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,24 +1,27 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<s.length();i++) {
+        Stack<Integer> stk = new Stack<>();
+        for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(Character.isAlphabetic(ch))
+            if(Character.isAlphabetic(ch)){
                 continue;
-            else if(ch == '(')
-                stack.push(i);
-            else {
-                if(!stack.isEmpty() && s.charAt(stack.peek()) == '(')
-                    stack.pop();
-                else stack.push(i);
+            } else if(ch=='('){
+                stk.push(i);
+            } else {
+                if(!stk.isEmpty() && s.charAt(stk.peek())=='('){
+                    stk.pop();
+                } else {
+                    stk.push(i);
+                }
             }
-        }        
-        StringBuilder result = new StringBuilder();
-        HashSet<Integer> set = new HashSet<>(stack);
-        for(int i=0;i<s.length();i++)
-            if(!set.contains(i))
-                result.append(s.charAt(i));
-        
-        return result.toString();
+        }
+        StringBuilder sb = new StringBuilder();
+        HashSet<Integer> set = new HashSet<>(stk);
+        for(int i=0;i<s.length();i++){
+            if(!set.contains(i)){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
