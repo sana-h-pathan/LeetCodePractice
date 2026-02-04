53611c1a2d22230fadfa8df5183b444961ecb23c
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-03 22:02:34 -0800

    Time: 16 ms (38.28%) | Memory: 47.6 MB (79.06%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,19 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
         for(int num: nums){
             map.put(num, map.getOrDefault(num, 0)+1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
+        int[] result = new int[k];
+        List<Integer> temp = new ArrayList<>();
         for(int num: map.keySet()){
             pq.add(num);
             if(pq.size()>k){
                 pq.poll();
             }
         }
-        int[] result = new int[k];
-        for(int i=0;i<k;i++){
-            result[i]=pq.poll();
+        int idx=0;
+        while(!pq.isEmpty()){
+            result[idx++]=pq.poll();
         }
         return result;
     }
