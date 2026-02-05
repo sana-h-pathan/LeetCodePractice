88b2070df1ed45e56b379ab68a1f50e9946ba9f7
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-04 19:48:13 -0800

    Time: 3 ms (98.03%) | Memory: 52.3 MB (30.75%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -5,8 +5,8 @@ class Solution {
         }
         int l=0;
         int r=0;
+        int zeros = 0;
         int maxLength=0;
-        int zeros=0;
         while(r<nums.length){
             if(nums[r]==0){
                 zeros++;
