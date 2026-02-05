daa9bfbf4f1d30dfad22cdd89407c45af2e18dd4
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-04 21:54:38 -0800

    Time: 63 ms (24.02%) | Memory: 47.7 MB (9.02%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.java
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.java
@@ -1,41 +1,29 @@
 class Solution {
-    public int lengthOfLongestSubstringTwoDistinct(String s) {
+    public int lengthOfLongestSubstringTwoDistinct(String s) { 
+        if(s.length() < 3)
+            return s.length();
         
-        int left = 0, right = 0;
-        int n = s.length();
-        
-        if(n < 3)
-            return n;
-        
-        HashMap<Character, Integer> map = new HashMap<>();
-        int maxLength = 2;
-        int index = 0;
-        while(right < n) {
-            
-            if(map.size() <=2) {
-                char ch = s.charAt(right);
-                map.put(ch, right);
-                right++;
-            }
-            
-            if(map.size() == 3){ 
-                
-                index = Collections.min(map.values());
-               
-                /*for(int num: map.values()) {
-                    if(num < index)
-                        index = num;
-                   // index = Math.min(index,num);
-                }*/
-                
-                map.remove(s.charAt(index));
-                left = index+1;
-            }
-            
-            maxLength = Math.max(maxLength, right - left);
-            
+        if(s==null || s.length()==0 ){
+            return 0;
         }
-        
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l=0;
+        int r=0;
+        int maxLength=0;
+        while(r<s.length()){
+            char rChar = s.charAt(r);
+            map.put(rChar, map.getOrDefault(rChar, 0)+1);
+            if(map.size()>2){
+                char lChar = s.charAt(l);
+                map.put(lChar, map.getOrDefault(lChar, 0)-1);
+                if(map.get(lChar)==0){
+                    map.remove(lChar, 0);
+                }
+                l++;
+            }
+            maxLength=Math.max(maxLength, r-l+1);
+            r++;
+        }
         return maxLength;
     }
 }
