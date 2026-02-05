665787adb9637bf11007f075f9f00e01e2e1383d
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-05 06:38:17 -0800

    Time: 2 ms (48.01%) | Memory: 47.5 MB (43.25%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,34 +1,35 @@
 class Solution {
     class UnionFind{
-        int[] parent;
+        int[] parents;
         int[] rank;
         public UnionFind(int n){
-            this.parent = new int[n];
+            this.parents = new int[n];
             this.rank = new int[n];
             for(int i=0;i<n;i++){
-                parent[i]=i;
+                parents[i]=i;
                 rank[i]=1;
             }
         }
-        public int find(int x){
-            if(x==parent[x]){
-                return x;
+        private int find(int u){
+            if(parents[u]==u){
+                return u;
             }
-            parent[x]=find(parent[x]);
-            return parent[x];
+            parents[u] = find(parents[u]);
+            return parents[u];
         }
-        public boolean union(int u, int v){
-            int uParent = find(u);
-            int vParent = find(v);
-            if(uParent==vParent){
+
+        private boolean union(int u, int v){
+            int uPar = find(u);
+            int vPar = find(v);
+            if(uPar==vPar){
                 return false;
             }
-            if(uParent<vParent){
-                parent[uParent]=vParent;
-                rank[vParent]+= rank[uParent];
+            if(rank[uPar]<rank[vPar]){
+                parents[uPar] = vPar;
+                rank[vPar]+= rank[uPar];
             } else {
-                parent[vParent]=uParent;
-                rank[uParent]+=rank[vParent];
+                parents[vPar] = uPar;
+                rank[uPar]+= rank[vPar];
             }
             return true;
         }
@@ -36,16 +37,16 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         UnionFind uf = new UnionFind(n);
-        int count = n;
+        int provinces = n;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j && isConnected[i][j]==1){
                     if(uf.union(i, j)){
-                        count--;
+                        provinces--;
                     }
                 }
             }
         }
-        return count<1?1:count;
+        return provinces;
     }
 }
