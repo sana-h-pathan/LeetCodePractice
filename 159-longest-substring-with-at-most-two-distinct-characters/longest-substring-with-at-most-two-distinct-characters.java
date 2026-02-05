3fe8c0e16c32a74edd1585552e2a9a042e170e96
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-04 21:58:30 -0800

    Time: 59 ms (25.04%) | Memory: 47.3 MB (46.72%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.java
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int lengthOfLongestSubstringTwoDistinct(String s) { 
-        if(s.length() < 3)
-            return s.length();
-        
-        if(s==null || s.length()==0 ){
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        if(s==null || s.length()==0){
             return 0;
         }
+        int n = s.length();
+        if(n<3){
+            return n;
+        }
+        int k=2;
         HashMap<Character, Integer> map = new HashMap<>();
         int l=0;
         int r=0;
         int maxLength=0;
-        while(r<s.length()){
+        while(r<n){
             char rChar = s.charAt(r);
             map.put(rChar, map.getOrDefault(rChar, 0)+1);
-            if(map.size()>2){
+            if(map.size()>k){
                 char lChar = s.charAt(l);
                 map.put(lChar, map.getOrDefault(lChar, 0)-1);
                 if(map.get(lChar)==0){
@@ -21,7 +23,7 @@ class Solution {
                 }
                 l++;
             }
-            maxLength=Math.max(maxLength, r-l+1);
+            maxLength = Math.max(maxLength, r-l+1);
             r++;
         }
         return maxLength;
