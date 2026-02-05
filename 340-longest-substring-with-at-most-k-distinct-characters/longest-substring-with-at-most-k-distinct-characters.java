49c5f743fe744225dba534ce4db46bfb58ce744f
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-04 21:58:53 -0800

    Time: 26 ms (10.05%) | Memory: 45.7 MB (79.18%) - LeetSync

--- 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
+++ 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
@@ -1,13 +1,14 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-       if(s==null || s.length()==0 || k==0){
+       if(s==null || s.length()==0){
             return 0;
         }
+        int n = s.length();
         HashMap<Character, Integer> map = new HashMap<>();
         int l=0;
         int r=0;
         int maxLength=0;
-        while(r<s.length()){
+        while(r<n){
             char rChar = s.charAt(r);
             map.put(rChar, map.getOrDefault(rChar, 0)+1);
             if(map.size()>k){
@@ -18,7 +19,7 @@ class Solution {
                 }
                 l++;
             }
-            maxLength=Math.max(maxLength, r-l+1);
+            maxLength = Math.max(maxLength, r-l+1);
             r++;
         }
         return maxLength;
