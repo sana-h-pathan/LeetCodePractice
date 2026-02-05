d248e3ef171d1a181cbc3b4508faf8660cb3f3d6
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-04 17:05:26 -0800

    Time: 1 ms (57.22%) | Memory: 43.3 MB (29.61%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,38 +1,36 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int n = nums.length;
-        if (n<k) 
-            return -1;
-        int low = 0;
-        int high = 0;
-        for (int num: nums) {
-            low=Math.max(low, num);
-            high+=num;
-        }
-        int minVal=low;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int count=calculate(nums, mid);
-            if(count>k)
-                low=mid+1;
-            else
-                high=mid-1;
+        if (nums.length < k) return -1;
 
+        int low = 0, high = 0;
+        for (int num : nums) {
+            low = Math.max(low, num);
+            high += num;
         }
 
-        return Math.max(high,minVal);
-    }
-    private int calculate(int[] nums, int capacity){
-        int count=1;
-        int temp=capacity;
-        for(int i=0;i<nums.length;i++){
-            if(temp-nums[i]>0){
-                temp=temp-nums[i];
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int parts = calculate(nums, mid);
+
+            if (parts > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            else{
-                temp=capacity;
-                temp=temp-nums[i];
+        }
+        return low;   // answer
+    }
+
+    private int calculate(int[] nums, int capacity) {
+        int count = 1;
+        int currSum = 0;
+
+        for (int num : nums) {
+            if (currSum + num <= capacity) {
+                currSum += num;
+            } else {
                 count++;
+                currSum = num;
             }
         }
         return count;
