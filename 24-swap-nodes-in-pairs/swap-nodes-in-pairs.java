f2c901bd0c9fd06ec8017c273f6718e23e2cc05e
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-04 19:43:21 -0800

    Time: 0 ms (100.00%) | Memory: 43.1 MB (42.62%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -3,33 +3,32 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int x) { val = x; }
  * }
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if(head==null || head.next==null){
+        if(head == null || head.next == null)
             return head;
+        ListNode newHead = head.next;
+        ListNode curr =  head;
+        ListNode fast =  head.next;
+        ListNode prev = null;
+        
+        while(fast!=null){
+            curr.next = fast.next;
+            fast.next = curr;
+            if(prev!=null)
+                prev.next = fast;
+            
+            if(curr.next == null)
+                break;
+             prev = curr;
+            curr = curr.next;
+            fast = curr.next;
+           
         }
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-
-        ListNode prev = dummy;
-
-        while (prev.next != null && prev.next.next != null) {
-            ListNode first = prev.next;
-            ListNode second = first.next;
-
-            // swap
-            prev.next = second;
-            first.next = second.next;
-            second.next = first;
-
-            // move forward
-            prev = first;
-        }
-        return dummy.next;
+        
+      return newHead;
     }
 }
