b8c3da790e5e02ab9b1f58275c8f9ad29b6e05c8
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-12 11:38:23 -0800

    Time: 3 ms (87.99%) | Memory: 52.1 MB (70.78%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -6,15 +6,15 @@ class Solution {
         if(grid==null || grid.length==0){
             return 0;
         }
-        this.m=grid.length;
+        this.m = grid.length;
         this.n = grid[0].length;
-        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        this.dirs = new int[][]{{1,0}, {0,1},{-1,0},{0,-1}};
         int islandCount = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    grid[i][j]='2';
                     islandCount++;
+                    grid[i][j]='2';
                     helper(grid, i, j);
                 }
             }
@@ -23,8 +23,8 @@ class Solution {
     }
     private void helper(char[][] grid, int i, int j){
         for(int[] dir: dirs){
-            int nr = dir[0]+i;
-            int nc = dir[1]+j;
+            int nr = i+dir[0];
+            int nc = j+dir[1];
             if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1'){
                 grid[nr][nc]='2';
                 helper(grid, nr, nc);
