eb4463012619e5e38808f10d6c03d9ba889f89a7
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-12 13:14:18 -0800

    Time: 7 ms (99.78%) | Memory: 47.4 MB (41.67%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.java
+++ 971-shortest-bridge/shortest-bridge.java
@@ -4,15 +4,14 @@ class Solution {
     public int shortestBridge(int[][] grid) {
         this.n = grid.length;
         this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        boolean found = false;
         Queue<int[]> bfsQue = new LinkedList<>();
-        boolean[][] visited = new boolean[n][n];
+        boolean found=false;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(!found && grid[i][j]==1){
-                    visited[i][j]=true;
+                    grid[i][j]=2;
                     found=true;
-                    helper(grid, i, j, bfsQue, visited);
+                    helper(grid, i, j, bfsQue);
                 }
             }
         }
@@ -24,15 +23,12 @@ class Solution {
                 for(int[] dir: dirs){
                     int nr = dir[0]+curr[0];
                     int nc = dir[1]+curr[1];
-                    if(nr>=0 && nc>=0 && nr<n && nc<n && !visited[nr][nc]){
-                        if(!visited[nr][nc] && grid[nr][nc]==1){
+                    if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]!=2){
+                        if(grid[nr][nc]==1){
                             return gap;
                         }
-                        if(visited[nr][nc]){
-                            continue;
-                        }
                         bfsQue.add(new int[]{nr, nc});
-                        visited[nr][nc]=true;
+                        grid[nr][nc]=2;
                     }
                 }
             }
@@ -40,14 +36,14 @@ class Solution {
         }
         return gap;
     }
-    private void helper(int[][] grid, int i, int j, Queue<int[]> bfsQue, boolean[][] visited){
-        bfsQue.add(new int[]{i,j});
+    private void helper(int[][] grid, int i, int j, Queue<int[]> bfsQue){
+        bfsQue.add(new int[]{i, j});
         for(int[] dir: dirs){
             int nr = dir[0]+i;
             int nc = dir[1]+j;
-            if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==1 && !visited[nr][nc]){
-                visited[nr][nc]=true;
-                helper(grid, nr, nc, bfsQue, visited);
+            if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==1){
+                grid[nr][nc]=2;
+                helper(grid, nr, nc, bfsQue);
             }
         }
     }
