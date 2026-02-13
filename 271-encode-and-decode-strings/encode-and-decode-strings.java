e51e87d8daf2727b5fb415ecb2a393fab1bd2028
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-12 17:09:33 -0800

    Time: 6 ms (52.27%) | Memory: 47.4 MB (62.95%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -2,32 +2,32 @@ public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        StringBuilder wordLen = new StringBuilder();
+        StringBuilder wordLength = new StringBuilder();
         StringBuilder words = new StringBuilder();
         for(String str: strs){
-            int len = str.length();
-            wordLen.append(len).append("#");
+            int wl = str.length();
+            wordLength.append(wl).append("#");
             words.append(str);
         }
-        wordLen.append("/").append(words);
-        return wordLen.toString();
+        wordLength.append("/").append(words);
+        return wordLength.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> result = new ArrayList<>();
-        if(s.length()==0){
+        if(s==null || s.length()==0){
             return result;
         }
         int idx = s.indexOf("/");
-        String wordsLen = s.substring(0, idx-1);
+        String wordLength = s.substring(0, idx-1);
         String words = s.substring(idx+1);
-        String[] wl = wordsLen.split("#");
-        int prev = 0;
-        for(int i=0;i<wl.length;i++){
-            int currLen = Integer.parseInt(wl[i]);
-            result.add(words.substring(prev, prev+currLen));
-            prev = prev+currLen;
+        String[] wordLen = wordLength.split("#");
+        int prev =0;
+        for(String wl: wordLen){
+            int length = Integer.parseInt(wl);
+            result.add(words.substring(prev, prev+length));
+            prev = prev+length;
         }
         return result;
     }
