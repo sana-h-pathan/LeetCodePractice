7452e06477177c252fadb41d67fb224993cd49a5
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-12 23:37:00 -0800

    Time: 9 ms (17.15%) | Memory: 47.2 MB (67.33%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,32 +1,26 @@
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String, PriorityQueue<String>> map = new HashMap<>();
-        for (List<String> ticket : tickets) {
-            String from = ticket.get(0);
-            String to = ticket.get(1);
-
-            if (!map.containsKey(from))
+        HashMap<String, PriorityQueue<String>> map = new HashMap<>();
+        for(List<String> t: tickets){
+            String from = t.get(0);
+            String to = t.get(1);
+            if(!map.containsKey(from)){
                 map.put(from, new PriorityQueue<>());
-
+            }
             map.get(from).add(to);
         }
-
-        List<String> route = new ArrayList<>();   // changed: ArrayList
+        List<String> result = new ArrayList<>();
         Deque<String> dq = new ArrayDeque<>();
-        dq.push("JFK");
-
-        while (!dq.isEmpty()) {
+        dq.add("JFK");
+        while(!dq.isEmpty()){
             String dest = dq.peek();
-            while (map.containsKey(dest) && !map.get(dest).isEmpty()) {
+            if(map.containsKey(dest) && !map.get(dest).isEmpty()){
                 dq.push(map.get(dest).poll());
-                dest = dq.peek();   // changed: update dest
+            } else {
+                result.add(dq.poll());
             }
-
-            route.add(dq.pop());    // changed: normal add
         }
-
-        Collections.reverse(route); // changed: reverse at end
-
-        return route;
+        Collections.reverse(result);
+        return result;
     }
 }
