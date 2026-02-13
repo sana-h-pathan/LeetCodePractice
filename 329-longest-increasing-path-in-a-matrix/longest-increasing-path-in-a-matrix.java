ec8dbbe84de33ef92c1e247f27acfe9dc809866a
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-12 21:57:11 -0800

    Time: 17 ms (13.61%) | Memory: 47.2 MB (44.99%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,10 +1,59 @@
 public class Solution {
-    int[][] dirs;
+    int[][] dirs = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
     int m;
     int n;
-    int[][] dp; // changed: memo table
+    int[][] dp;
 
     public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+        m = matrix.length;
+        n = matrix[0].length;
+        int[][] indegree = new int[m][n];
+        Queue<int[]> bfsQue = new ArrayDeque<>();
+        // build indegree
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int[] dir : dirs) {
+                    int nr = i + dir[0];
+                    int nc = j + dir[1];
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[nr][nc] < matrix[i][j]) {
+                        indegree[i][j]++;
+                    }
+                }
+            }
+        }
+
+        // add all indegree 0 nodes
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (indegree[i][j] == 0)
+                    bfsQue.add(new int[]{i, j});   // changed to add
+            }
+        }
+        int result = 0;
+        while (!bfsQue.isEmpty()) {
+            int size = bfsQue.size();
+            result++;
+            for (int i = 0; i < size; i++) {
+                int[] curr = bfsQue.poll();
+                for (int[] dir : dirs) {
+                    int nr = curr[0] + dir[0];
+                    int nc = curr[1] + dir[1];
+
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[nr][nc] > matrix[curr[0]][curr[1]]) {
+                        indegree[nr][nc]--;
+                        if (indegree[nr][nc] == 0)
+                            bfsQue.add(new int[]{nr, nc});  // changed to add
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int longestIncreasingPath1(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) // changed
             return 0;
 
