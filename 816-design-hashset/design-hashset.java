6858decdd2992d7f6b8b5fd1795e5b2c4e660714
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-13 08:17:26 -0800

    Time: 12 ms (98.69%) | Memory: 53.4 MB (36.60%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -2,22 +2,22 @@ class MyHashSet {
     int firstLevel;
     int secondLevel;
     boolean[][] dataSet;
-
     public MyHashSet() {
         this.firstLevel = 1000;
         this.secondLevel = 1000;
         this.dataSet = new boolean[firstLevel][];
     }
-    private int firstLevelHashing(int key){
+    private int firstHashing(int key){
         return key%firstLevel;
     }
-    private int secondLevelHashing(int key){
+
+    private int secondHashing(int key){
         return key/firstLevel;
     }
-
+    
     public void add(int key) {
-        int fKey = firstLevelHashing(key);
-        int sKey = secondLevelHashing(key);
+        int fKey = firstHashing(key);
+        int sKey = secondHashing(key);
         if(dataSet[fKey]==null){
             if(fKey==0){
                 dataSet[fKey] = new boolean[secondLevel+1];
@@ -25,21 +25,22 @@ class MyHashSet {
                 dataSet[fKey] = new boolean[secondLevel];
             }
         }
-        dataSet[fKey][sKey] = true;
+        dataSet[fKey][sKey]=true;
     }
     
     public void remove(int key) {
-        int fKey = firstLevelHashing(key);
-        int sKey = secondLevelHashing(key);
+        int fKey = firstHashing(key);
+        int sKey = secondHashing(key);
         if(dataSet[fKey]==null){
             return;
         }
-        dataSet[fKey][sKey] = false;
+        dataSet[fKey][sKey]=false;
+        return;
     }
     
     public boolean contains(int key) {
-        int fKey = firstLevelHashing(key);
-        int sKey = secondLevelHashing(key);
+        int fKey = firstHashing(key);
+        int sKey = secondHashing(key);
         if(dataSet[fKey]==null){
             return false;
         }
