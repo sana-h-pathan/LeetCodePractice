3baee9282512f17b00bcaa458940d684ded2e693
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-13 10:22:36 -0800

    Time: 50 ms (92.86%) | Memory: 136.9 MB (54.69%) - LeetSync

--- 460-lfu-cache/lfu-cache.java
+++ 460-lfu-cache/lfu-cache.java
@@ -2,48 +2,48 @@ class LFUCache {
     class Node{
         int key;
         int value;
-        int freq;
-        Node prev;
         Node next;
+        Node prev;
+        int freq;
         public Node(int key, int value){
             this.key = key;
             this.value = value;
             this.freq = 1;
         }
     }
-
     class DLList{
         Node head;
         Node tail;
         int size;
         public DLList(){
             this.head = new Node(-1, -1);
-            this.tail = new Node(-1,-1);
-            this.head.next = tail;
-            this.tail.prev = head;
+            this.tail = new Node(-1, -1);
+            this.head.next = this.tail;
+            this.tail.prev = this.head;
+            this.size = 0;
         }
         private void addToHead(Node curr){
             Node temp = head.next;
             head.next = curr;
             curr.prev = head;
             curr.next = temp;
-            curr.next.prev = curr;
+            temp.prev = curr;
             this.size++;
         }
         private void removeNode(Node curr){
-            curr.prev.next = curr.next;
             curr.next.prev = curr.prev;
+            curr.prev.next = curr.next;
             this.size--;
         }
     }
 
     HashMap<Integer, Node> nodeMap;
     HashMap<Integer, DLList> freqMap;
-    int min;
     int capacity;
+    int min;
 
     public LFUCache(int capacity) {
-        this.capacity=capacity;
+        this.capacity = capacity;
         this.nodeMap = new HashMap<>();
         this.freqMap = new HashMap<>();
     }
@@ -53,43 +53,44 @@ class LFUCache {
             return -1;
         }
         Node curr = nodeMap.get(key);
-        update(curr);
-        return curr.value;
+        updateNode(curr);
+        return curr.value;   
     }
     
     public void put(int key, int value) {
         if(nodeMap.containsKey(key)){
             Node curr = nodeMap.get(key);
-            update(curr);
             curr.value = value;
+            updateNode(curr); //removenode->updatefreq->addToHead
         } else {
-            if(nodeMap.size()==capacity){
+            if(nodeMap.size() == capacity){
                 DLList removeList = freqMap.get(min);
                 Node toRemove = removeList.tail.prev;
                 removeList.removeNode(toRemove);
                 nodeMap.remove(toRemove.key);
             }
             Node newNode = new Node(key, value);
+            nodeMap.put(key, newNode);
             min=1;
             DLList newList = freqMap.getOrDefault(min, new DLList());
             newList.addToHead(newNode);
-            nodeMap.put(key, newNode);
             freqMap.put(min, newList);
+
         }
-        
     }
-    private void update(Node curr){
-        int oldFreq = curr.freq;
-        DLList oldFreqList = freqMap.get(oldFreq);
-        oldFreqList.removeNode(curr);
-        if(oldFreq == min && oldFreqList.size==0){
+
+    private void updateNode(Node curr){
+        int currFreq = curr.freq;
+        DLList currList = freqMap.get(currFreq);
+        currList.removeNode(curr);
+        if(currFreq==min && currList.size==0){
             min++;
         }
         curr.freq++;
         int newFreq = curr.freq;
-        DLList newFreqList = freqMap.getOrDefault(newFreq, new DLList());
-        newFreqList.addToHead(curr);
-        freqMap.put(newFreq, newFreqList);
+        DLList newList = freqMap.getOrDefault(newFreq, new DLList());
+        newList.addToHead(curr);
+        freqMap.put(newFreq, newList);
     }
 }
 
