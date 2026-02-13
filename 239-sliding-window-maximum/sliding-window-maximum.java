db2a22f9ca8372439c8b7fac0a6e83bb42a35457
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-12 22:14:19 -0800

    Time: 27 ms (95.05%) | Memory: 143.4 MB (94.88%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -3,21 +3,21 @@ class Solution {
         if(nums==null || nums.length==0 || k<=0){
             return new int[0];
         }
-        int n=nums.length;
         if(k==1){
             return nums;
         }
-        Deque<Integer> dq = new ArrayDeque<>();
-        int[] result = new int[n-k+1];
+        int n = nums.length;
         int l=0;
         int r=0;
+        int[] result = new int[n-k+1];
+        Deque<Integer> dq = new ArrayDeque<>();
         while(r<n){
             while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[r]){
                 dq.pollLast();
             }
             dq.addLast(r);
             if(r-l+1==k){
-                result[l]=nums[dq.peekFirst()];
+                result[l] = nums[dq.peekFirst()];
                 if(!dq.isEmpty() && dq.peekFirst()==l){
                     dq.pollFirst();
                 }
