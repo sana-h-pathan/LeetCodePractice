48898d96e1e9294e0148095e24e7375a803abb8b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-12 23:21:16 -0800

    Time: 381 ms (11.57%) | Memory: 236.9 MB (21.92%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int lengthOfLIS1(int[] nums) {
+    public int lengthOfLIS(int[] nums) {
         int n=nums.length;
         int[][] dp=new int[n][n+1];
-        for (int row[] : dp) {
+        for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
         return helper(nums, 0, -1,dp);
@@ -22,7 +22,7 @@ class Solution {
         return dp[idx][prevIdx + 1];
     }
     
-    public int lengthOfLIS(int[] nums) {
+    public int lengthOfLIS1(int[] nums) {
         int n=nums.length;
         int[][] dp=new int[n+1][n+1];
         
@@ -38,5 +38,36 @@ class Solution {
         }
         
         return dp[0][-1+1];        
+    }
+    public int lengthOfLISBruteForce(int[] nums) {
+        return helperBruteForce(nums, 0, -1);
+    }
+    private int helperBruteForce(int[] nums, int idx, int prevIdx){
+        if(idx==nums.length)
+            return 0;
+        int notTake=helperBruteForce(nums,idx+1, prevIdx);
+        int take=0;
+        if(prevIdx==-1 || nums[idx]>nums[prevIdx])
+            take=1+helperBruteForce(nums,idx+1, idx);
+        
+        return Math.max(take,notTake);
+    }
+    public int lengthOfLISDP(int[] nums) {
+        if(nums==null || nums.length==0)
+            return 0;
+        int n=nums.length;
+        
+        int[] dp=new int[n];
+        int length=1;
+        Arrays.fill(dp,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                    length=Math.max(length,dp[i]);
+                }
+            }
+        }
+        return length;
     }
 }
