608417122da96092f14549d2ace85817b5389dcd
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-12 23:48:05 -0800

    Time: 0 ms (100.00%) | Memory: 43 MB (15.36%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.java
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.java
@@ -2,10 +2,12 @@ class Solution {
         //Time:O(L + 26 * 26); Space: O(26)
     public boolean equalFrequency(String word) {
         int[] data = new int[26];
-        for (char c: word.toCharArray()) data[c -'a']++;
+        for (char c: word.toCharArray()) 
+            data[c -'a']++;
         for (int i = 0; i < 26; i++){
             data[i]--;
-            if (isEqual(data)) return true;
+            if(isEqual(data)) 
+                return true;
             data[i]++;
         }
         return false;
@@ -13,8 +15,10 @@ class Solution {
     private boolean isEqual(int[] data){
         int lastCount = 0;
         for (int count: data){
-            if (count == 0) continue;
-            if (lastCount != 0 && count != lastCount) return false;
+            if (count == 0) 
+                continue;
+            if (lastCount != 0 && count != lastCount) 
+                return false;
             lastCount = count;
         }
         return true;
