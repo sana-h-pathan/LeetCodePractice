734f6ed989178f87d86f8005261f52d950ed3633
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-19 15:56:54 -0800

    Time: 1 ms (81.68%) | Memory: 46.4 MB (72.21%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,36 +1,34 @@
 class Solution {
     class UnionFind{
-        int[] parents;
+        int[] parent;
         int[] rank;
         public UnionFind(int n){
-            this.parents = new int[n];
+            this.parent = new int[n];
             this.rank = new int[n];
             for(int i=0;i<n;i++){
-                parents[i]=i;
+                parent[i]=i;
                 rank[i]=1;
             }
-        }
-
-        private int find(int u){
-            if(parents[u]==u){
-                return u;
+        } 
+        private int find(int x){
+            if(parent[x]==x){
+                return x;
             }
-            parents[u]=find(parents[u]);
-            return parents[u];
+            parent[x] = find(parent[x]);
+            return parent[x];
         }
-
         private boolean union(int u, int v){
             int uPar = find(u);
             int vPar = find(v);
-            if(uPar==vPar){
+            if(uPar == vPar){
                 return false;
             }
             if(rank[uPar]<rank[vPar]){
-                parents[uPar] = vPar;
+                parent[uPar] = vPar;
                 rank[vPar]+=rank[uPar];
             } else {
-                parents[vPar] = uPar;
-                rank[uPar] = vPar;
+                parent[vPar] = uPar;
+                rank[uPar]+=rank[vPar];
             }
             return true;
         }
@@ -40,7 +38,7 @@ class Solution {
         if(edges.length!=n-1){
             return false;
         }
-        for(int[] edge : edges){
+        for(int[] edge: edges){
             if(!uf.union(edge[0], edge[1])){
                 return false;
             }
