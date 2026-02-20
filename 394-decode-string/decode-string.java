8565417eec656d984770425d76fc2548283f02c4
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-20 14:02:29 -0800

    Time: 1 ms (84.28%) | Memory: 43.1 MB (37.94%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -2,25 +2,25 @@ class Solution {
     public String decodeString(String s) {
         Stack<Integer> numStk = new Stack<>();
         Stack<StringBuilder> strStk = new Stack<>();
-        int currNum=0;
-        StringBuilder currStr=new StringBuilder();
-        for(char ch: s.toCharArray()){
+        int currNum = 0;
+        StringBuilder currStr = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
             if(Character.isDigit(ch)){
-                currNum=currNum*10+ch-'0';
+                currNum = currNum*10+ch-'0';
             } else if(ch=='['){
                 numStk.push(currNum);
                 strStk.push(currStr);
-                currNum=0;
-                currStr=new StringBuilder(); 
+                currNum = 0;
+                currStr = new StringBuilder();
             } else if(ch==']'){
                 int currItr = numStk.pop();
-                StringBuilder ns = new StringBuilder();
-                for(int i=0;i<currItr;i++){
-                    ns.append(currStr);
+                StringBuilder newStr = new StringBuilder();
+                for(int k=0;k<currItr;k++){
+                    newStr.append(currStr);
                 }
                 currStr = strStk.pop();
-                currStr.append(ns);
-
+                currStr.append(newStr);
             } else {
                 currStr.append(ch);
             }
