68ca0e8f93780fa75d5abb522f20eb1c40f1f51b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-21 14:39:27 -0800

    Time: 20 ms (57.96%) | Memory: 47.2 MB (99.67%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -2,24 +2,24 @@ class Solution {
     public int leastInterval(char[] tasks, int n) {
         //A=4, B=4, C=3 n=2
         //AB_AB_AB_AB
-        int maxFreq=0;
         HashMap<Character, Integer> map = new HashMap<>();
+        int maxFreq=0;
         for(char t: tasks){
             map.put(t, map.getOrDefault(t, 0)+1);
             maxFreq = Math.max(maxFreq, map.get(t));
         }
-        int maxCount =0;
+        int maxCount=0;
         for(char ch: map.keySet()){
             if(map.get(ch)==maxFreq){
                 maxCount++;
             }
         }
-        int totalTask = tasks.length;
+        int totalTask=tasks.length;
         int partition = maxFreq-1;
         int availableSlot = partition * (n-(maxCount-1));
-        int pendingSlot = totalTask - (maxFreq*maxCount);
-        int idleSlot = Math.max(0, availableSlot-pendingSlot);
+        int pendingTask = totalTask - (maxFreq*maxCount);
+        int idleSlot = Math.max(0, availableSlot-pendingTask);
 
-        return idleSlot+totalTask;
+        return totalTask+idleSlot;
     }
 }
