695971cb19cfa2754b1c749ca6dd439d9795aa3c
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-21 12:54:12 -0800

    Time: 4 ms (98.33%) | Memory: 47.8 MB (65.05%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,27 +1,20 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int n = arr.length;
+    public List<Integer> findClosestElements(int[] nums, int k, int x) {
         int low = 0;
-        int high = n - k; // last possible window start
-
-        while (low<high) {
-            int mid = low + (high - low) / 2;
-
-            // window is [mid .. mid+k-1], compare edges: arr[mid] vs arr[mid+k]
-            int leftGap = x - arr[mid];
-            int rightGap = arr[mid + k] - x;
-
-            // if left edge is farther than the element just outside right edge, shift right
-            if (leftGap > rightGap) {
-                low = mid + 1;
+        int high = nums.length-k;
+        while(low<high){
+            int mid = low+(high-low)/2;
+            int leftGap = x-nums[mid];
+            int rightGap = nums[mid+k]-x;
+            if(rightGap<leftGap){
+                low = mid+1;
             } else {
                 high = mid;
             }
         }
-
-        List<Integer> result = new ArrayList<>(k);
-        for (int i = low; i < low + k; i++) {
-            result.add(arr[i]);
+        List<Integer> result = new ArrayList<>();
+        for(int i=low;i<low+k;i++){
+            result.add(nums[i]);
         }
         return result;
     }
