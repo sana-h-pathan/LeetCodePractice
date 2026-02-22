8ad52c577abb6e59756215725ff3f8735804a28c
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-22 12:05:44 -0800

    Time: 7 ms (96.45%) | Memory: 47.3 MB (89.24%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.java
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.java
@@ -6,14 +6,10 @@ class Solution {
             }
             return b[0]-a[0];
         });
-        List<int[]> temp = new ArrayList<>();
-        for(int[] p: people){
-            temp.add(p[1], p);
+        List<int[]> result = new ArrayList<>();
+        for(int[] p:people){
+            result.add(p[1], p);
         }
-        int[][] result = new int[temp.size()][2];
-        for(int i=0;i<result.length;i++){
-            result[i] = temp.get(i);
-        }
-        return result;
+        return result.toArray(new int[0][]);
     }
 }
