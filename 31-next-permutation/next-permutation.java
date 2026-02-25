bbd746099e44cc1c66e28c9141cade3e0572dc6c
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-02-24 19:35:01 -0800

    Time: 0 ms (100.00%) | Memory: 44.6 MB (69.59%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,32 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         //14685431
+        //finding breach i.e where previous number is less than the current number
+        //find index of number thats greater than breached value
+        //swap them
+        //reverse entire number
+        if(nums==null || nums.length==0){
+            return;
+        }
         int n = nums.length;
         int i = n-2;
         while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
-        int j=n-1;
+        int j = n-1;
         if(i>=0){
-            while(nums[j]<=nums[i]){
+            while(nums[i]>=nums[j]){
                 j--;
             }
             swap(nums, i, j);
         }
-        reverse(nums, i+1,n-1);
-
+        reverse(nums, i+1, n-1);
     }
-    private void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    private void swap(int[] nums, int l, int r){
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
     }
     private void reverse(int[] nums, int l, int r){
-        while(l<=r){
+        while(l<r){
             swap(nums, l, r);
             l++;
             r--;
         }
-        
     }
 }
