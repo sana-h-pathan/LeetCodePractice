d9b208148d606882c22ac56a77898b796f319daf
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-03-05 09:37:59 -0800

    Time: 16 ms (59.45%) | Memory: 47.2 MB (55.35%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,28 +1,27 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s==null || t==null || t.length()>s.length()){
+        if(s==null || t==null || s.length()<t.length()){
             return "";
         }
-
         HashMap<Character, Integer> map = new HashMap<>();
         for(char ch: t.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
-        int l=0;
-        int r=0;
-        int currMatches = t.length();
-        int reqdMatches = 0;
         int minLength = Integer.MAX_VALUE;
         int sIdx = -1;
+        int l = 0;
+        int r = 0;
+        int currentMatches = t.length();
+        int reqdMatched = 0;
         while(r<s.length()){
             char rChar = s.charAt(r);
             if(map.containsKey(rChar)){
                 if(map.get(rChar)>0){
-                    reqdMatches++;
+                    reqdMatched++;
                 }
                 map.put(rChar, map.getOrDefault(rChar, 0)-1);
             }
-            while(currMatches==reqdMatches){
+            while(currentMatches==reqdMatched){
                 if(minLength>r-l+1){
                     minLength = r-l+1;
                     sIdx = l;
@@ -31,7 +30,7 @@ class Solution {
                 if(map.containsKey(lChar)){
                     map.put(lChar, map.getOrDefault(lChar, 0)+1);
                     if(map.get(lChar)>0){
-                        reqdMatches--;
+                        reqdMatched--;
                     }
                 }
                 l++;
