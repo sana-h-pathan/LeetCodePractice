abba9449118f84d0b52b47779b83e8289971369b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-03-05 09:31:10 -0800

    Time: 17 ms (52.71%) | Memory: 47.5 MB (40.69%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,42 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s==null || t==null|| s.length()<t.length()){
+        if(s==null || t==null || t.length()>s.length()){
             return "";
         }
+
         HashMap<Character, Integer> map = new HashMap<>();
         for(char ch: t.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
-        int currMatches=0;
-        int reqdMatches = t.length();
         int l=0;
         int r=0;
-        int sIdx=-1;
-        int minLength=Integer.MAX_VALUE;
+        int currMatches = t.length();
+        int reqdMatches = 0;
+        int minLength = Integer.MAX_VALUE;
+        int sIdx = -1;
         while(r<s.length()){
             char rChar = s.charAt(r);
             if(map.containsKey(rChar)){
                 if(map.get(rChar)>0){
-                    currMatches++;
+                    reqdMatches++;
                 }
-                map.put(rChar, map.get(rChar)-1);
+                map.put(rChar, map.getOrDefault(rChar, 0)-1);
             }
             while(currMatches==reqdMatches){
                 if(minLength>r-l+1){
-                    sIdx=l;
                     minLength = r-l+1;
+                    sIdx = l;
                 }
                 char lChar = s.charAt(l);
                 if(map.containsKey(lChar)){
-                    map.put(lChar, map.get(lChar)+1);
+                    map.put(lChar, map.getOrDefault(lChar, 0)+1);
                     if(map.get(lChar)>0){
-                        currMatches--;
+                        reqdMatches--;
                     }
                 }
                 l++;
             }
             r++;
         }
-        return sIdx==-1?"":s.substring(sIdx, sIdx+minLength);
+        if(sIdx==-1){
+            return "";
+        }
+        return s.substring(sIdx, sIdx+minLength);
     }
 }
