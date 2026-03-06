49524cde9bc0f359004319b4d33b3f72b7f08345
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-03-06 11:18:47 -0800

    Time: 7 ms (3.61%) | Memory: 46.7 MB (34.33%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,48 +1,38 @@
 class Solution {
-    private int n, m;
+    int m;
+    int n;
     int[][] dirs;
     public int maxAreaOfIsland(int[][] grid) {
-        this.dirs=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        int max = 0;
-        n = grid.length;
-        m = grid[0].length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++){
-                if (grid[i][j] > 0) 
-                    max = Math.max(max, dfs(i, j, grid));
-            }
-        }
-        return max;
-    }
-    private int bfs(int i, int j, int[][] grid){
-        Queue<int[]> bfsQueue=new LinkedList<>();
-        grid[i][j]=0;
-        bfsQueue.add(new int[]{i,j});
-        int sum=1;
-        while(!bfsQueue.isEmpty()){
-            int[] cords=bfsQueue.poll();
-            for(int[]dir : dirs){
-                int nr=cords[0]+dir[0];
-                int nc=cords[1]+dir[1];
-                if (nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == 1){
-                    grid[nr][nc]=0;
-                    sum+=1;
-                    bfsQueue.add(new int[]{nr,nc});
+        this.m=grid.length;
+        this.n = grid[0].length;
+        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        int maxArea = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    maxArea = Math.max(maxArea, helper(grid, i, j));
                 }
             }
         }
-        return sum;
+        return maxArea;
     }
-    private int dfs(int i, int j, int[][] grid) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] < 1) 
-            return 0;
-        grid[i][j] = 0;
-        int sum=1;
-        for(int[] dir: dirs){
-            int nr=dir[0]+i;
-            int nc=dir[1]+j;
-            sum+=dfs(nr, nc, grid);
+    private int helper(int[][] grid, int i, int j){
+        int count=0;
+        Queue<int[]> bfsQue = new LinkedList<>();
+        bfsQue.add(new int[]{i,j});
+        grid[i][j]=0;
+        while(!bfsQue.isEmpty()){
+            int[] curr = bfsQue.poll();
+            count++;
+            for(int[] dir: dirs){
+                int nr = curr[0]+dir[0];
+                int nc = curr[1]+dir[1];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
+                    bfsQue.add(new int[]{nr, nc});
+                    grid[nr][nc]=0;
+                }
+            }
         }
-        return sum;
+        return count;
     }
 }
