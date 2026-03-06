b2962430d05985fc0fc42226184824be4547f898
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-03-06 12:14:22 -0800

    Time: 2 ms (80.56%) | Memory: 47.7 MB (73.27%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,40 +1,44 @@
 class Solution {
-    int[][] dirs;
     int m;
     int n;
+    int[][] dirs;
     public void solve(char[][] board) {
         this.m = board.length;
         this.n = board[0].length;
         this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        if(m<2 || n<2)
-            return;
-        for(int i=0;i<m;i++){ //run for all rows but 1st and last column 
-            helper(board,i,0);
+        for(int i=0;i<m;i++){
+            helper(board, i, 0);
             helper(board, i, n-1);
         }
-        for(int j=0;j<n;j++){ //run for all columns but 1st and last row 
+        for(int j=0;j<n;j++){
             helper(board, 0, j);
             helper(board, m-1, j);
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]=='O'){
-                    board[i][j]='X';
-                } else if(board[i][j]=='0'){
+                if(board[i][j]=='0'){
                     board[i][j]='O';
+                } else if(board[i][j]=='O'){
+                     board[i][j]='X';
                 }
             }
         }
     }
-    private void helper(char[][] board, int r, int c){
-        if(r<0 || c<0 || r>m-1|| c>n-1)
-            return;
-        if(board[r][c]=='O'){
-            board[r][c] = '0';
+    private void helper(char[][] board, int i, int j){
+        Queue<int[]> bfsQue = new LinkedList<>();
+        if(board[i][j]=='O'){
+            bfsQue.add(new int[]{i,j});
+            board[i][j]='0';
+        }
+        while(!bfsQue.isEmpty()){
+            int[] curr = bfsQue.poll();
             for(int[] dir: dirs){
-                int nr = dir[0]+r;
-                int nc = dir[1]+c;
-                helper(board, nr, nc);
+                int nr = dir[0]+curr[0];
+                int nc = dir[1]+curr[1];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='O'){
+                    board[nr][nc] = '0';
+                    bfsQue.add(new int[]{nr, nc});
+                }
             }
         }
     }
