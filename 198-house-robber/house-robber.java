231ae1cde8e863baa382813e794dd2b9827145db
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-03-11 22:57:14 -0700

    Time: 0 ms (100.00%) | Memory: 42.7 MB (71.93%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,48 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        if(nums.length==1)
+        int n = nums.length;
+        if(n<2){
             return nums[0];
+        }
+        int[] dp = new int[n];
         dp[0] = nums[0];
-        dp[1] = Math.max(dp[0], nums[1]);
-        for(int i=2;i<nums.length;i++){
-            dp[i] = Math.max(dp[i-1], nums[i]+dp[i-2]);
+        dp[1] = Math.max(nums[1], dp[0]);
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
         }
-        return dp[nums.length-1];
-    }
-
-    public int rob1(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return helper(nums, 0, dp);
-    }
-    private int helper(int[] nums, int idx, int[] dp){
-        if(idx==nums.length-1){
-            return nums[idx];
-        }
-        if(idx==nums.length)
-            return 0;
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int notPick = helper(nums, idx+1);
-        int pick = nums[idx]+ helper(nums, idx+2);
-        dp[idx] = Math.max(pick, notPick);
-        return dp[idx];
-    }
-
-    public int rob2(int[] nums) {
-        return helper(nums, 0);
-    }
-    private int helper(int[] nums, int idx){
-        if(idx==nums.length-1){
-            return nums[idx];
-        }
-        if(idx==nums.length)
-            return 0;
-        int notPick = helper(nums, idx+1);
-        int pick = nums[idx]+ helper(nums, idx+2);
-        return Math.max(pick, notPick);
+        return dp[n-1];
     }
 }
