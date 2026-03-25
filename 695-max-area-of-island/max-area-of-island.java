e27ca092d30c1d8d3572ae0fa7c63c0fdc66d2b7
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-03-24 21:56:49 -0700

    Time: 3 ms (26.19%) | Memory: 46.9 MB (20.15%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -3,33 +3,35 @@ class Solution {
     int n;
     int[][] dirs;
     public int maxAreaOfIsland(int[][] grid) {
-        this.m=grid.length;
+        this.m = grid.length;
         this.n = grid[0].length;
         this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        int maxArea = 0;
+        int maxArea = Integer.MIN_VALUE;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    maxArea = Math.max(maxArea, helper(grid, i, j));
-                }
+                int currArea = helper(grid, i, j);
+                maxArea = Math.max(currArea, maxArea);
             }
         }
         return maxArea;
     }
     private int helper(int[][] grid, int i, int j){
-        int count=0;
+        int count = 0;
         Queue<int[]> bfsQue = new LinkedList<>();
-        bfsQue.add(new int[]{i,j});
-        grid[i][j]=0;
-        while(!bfsQue.isEmpty()){
-            int[] curr = bfsQue.poll();
+        if(grid[i][j]==1){
             count++;
-            for(int[] dir: dirs){
-                int nr = curr[0]+dir[0];
-                int nc = curr[1]+dir[1];
-                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
-                    bfsQue.add(new int[]{nr, nc});
-                    grid[nr][nc]=0;
+            bfsQue.add(new int[]{i, j});
+            grid[i][j]=2;
+            while(!bfsQue.isEmpty()){
+                int[] curr = bfsQue.poll();
+                for(int[] dir: dirs){
+                    int nr = dir[0]+curr[0];
+                    int nc = dir[1]+curr[1];
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
+                        count++;
+                        grid[nr][nc]=2;
+                        bfsQue.add(new int[]{nr, nc});
+                    }
                 }
             }
         }
