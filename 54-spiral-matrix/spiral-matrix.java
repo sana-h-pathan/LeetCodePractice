e80dde97b9eea56864ce476c88d855284c3396cd
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-05-04 22:37:02 -0700

    Time: 0 ms (100.00%) | Memory: 42.8 MB (78.12%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -2,22 +2,21 @@ class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        List<Integer> result = new ArrayList<>();
-        int left = 0;
-        int right = n-1;
         int top = 0;
+        int left = 0;
         int bottom = m-1;
+        int right = n-1;
+        List<Integer> result = new ArrayList<>();
         while(left<=right && top<=bottom){
             for(int j=left;j<=right;j++){
-                result.add(matrix[top][j]);
+               result.add(matrix[top][j]);
             }
             top++;
             for(int i=top;i<=bottom;i++){
-                result.add(matrix[i][right]);
+               result.add(matrix[i][right]);
             }
             right--;
             if(top<=bottom){
-            
                 for(int j=right;j>=left;j--){
                     result.add(matrix[bottom][j]);
                 }
