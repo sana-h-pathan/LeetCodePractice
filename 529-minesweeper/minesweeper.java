e803690ec8a0c7c219cb4aff7a14fe77dd17dbdd
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-05-11 12:50:09 -0700

    Time: 3 ms (25.88%) | Memory: 46.4 MB (98.25%) - LeetSync

--- 529-minesweeper/minesweeper.java
+++ 529-minesweeper/minesweeper.java
@@ -5,14 +5,16 @@ class Solution {
     public char[][] updateBoard(char[][] board, int[] click) {
         this.m = board.length;
         this.n = board[0].length;
-        this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
-        if(board[click[0]][click[1]]=='M'){
-            board[click[0]][click[1]]='X';
+        this.dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{1,-1},{-1,1}};
+        int r = click[0];
+        int c = click[1];
+        if(board[r][c]=='M'){
+            board[r][c] = 'X';
             return board;
         }
         Queue<int[]> bfsQue = new LinkedList<>();
-        bfsQue.add(new int[]{click[0],click[1]});
-        board[click[0]][click[1]]='B';
+        bfsQue.add(new int[]{r,c});
+        board[r][c] = 'B';
         while(!bfsQue.isEmpty()){
             int[] curr = bfsQue.poll();
             int mines = countMines(board, curr[0], curr[1]);
@@ -21,9 +23,8 @@ class Solution {
                     int nr = curr[0]+dir[0];
                     int nc = curr[1]+dir[1];
                     if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='E'){
-                        board[nr][nc]='B';
                         bfsQue.add(new int[]{nr, nc});
-
+                        board[nr][nc] = 'B';
                     }
                 }
             } else {
@@ -32,16 +33,15 @@ class Solution {
         }
         return board;
     }
-
-    private int countMines(char[][] board, int i, int j){
-        int count=0;
-        for(int[] dir : dirs){
-            int nr = dir[0]+i;
-            int nc = dir[1]+j;
+    private int countMines(char[][] board, int r, int c){
+        int mines = 0;
+        for(int[] dir: dirs){
+            int nr = dir[0]+r;
+            int nc = dir[1]+c;
             if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='M'){
-                count++;
+                mines++;
             }
         }
-        return count;
+        return mines;
     }
 }
