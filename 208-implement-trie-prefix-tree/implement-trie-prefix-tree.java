23976598a793e34c4e4224c842592b8cc04e9987
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-05-11 17:15:34 -0700

    Time: 31 ms (89.93%) | Memory: 61.8 MB (73.13%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -13,21 +13,21 @@ class Trie {
     
     public void insert(String word) {
         TrieNode curr = root;
-        for(int i=0;i<word.length();i++){
-            char ch = word.charAt(i);
-            if(curr.children[ch-'a']==null)
+        for(char ch: word.toCharArray()){
+            if(curr.children[ch-'a']==null){
                 curr.children[ch-'a'] = new TrieNode();
+            }
             curr = curr.children[ch-'a'];
         }
-        curr.isEnd=true;
+        curr.isEnd = true;
     }
     
     public boolean search(String word) {
         TrieNode curr = root;
-        for(int i=0;i<word.length();i++){
-            char ch = word.charAt(i);
-            if(curr.children[ch-'a']==null)
+        for(char ch: word.toCharArray()){
+            if(curr.children[ch-'a']==null){
                 return false;
+            }
             curr = curr.children[ch-'a'];
         }
         return curr.isEnd;
@@ -35,10 +35,10 @@ class Trie {
     
     public boolean startsWith(String prefix) {
         TrieNode curr = root;
-        for(int i=0;i<prefix.length();i++){
-            char ch = prefix.charAt(i);
-            if(curr.children[ch-'a']==null)
+        for(char ch: prefix.toCharArray()){
+            if(curr.children[ch-'a']==null){
                 return false;
+            }
             curr = curr.children[ch-'a'];
         }
         return true;
