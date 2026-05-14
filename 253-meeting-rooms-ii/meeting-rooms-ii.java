972a7fe9dcd69e388dfeb1178a59c7055196a97b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-05-14 15:33:31 -0700

    Time: 8 ms (95.13%) | Memory: 48.5 MB (53.76%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)->Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0], b[0]));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int[] interval : intervals){
+        for(int[] interval: intervals){
             if(!pq.isEmpty() && pq.peek()<=interval[0]){
                 pq.poll();
             }
