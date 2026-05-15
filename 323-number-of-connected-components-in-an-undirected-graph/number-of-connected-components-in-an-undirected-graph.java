9ac6050598359c6d5d7a56e63c066a80377189fa
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-05-15 16:10:45 -0700

    Time: 2 ms (71.93%) | Memory: 46.7 MB (33.06%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,28 +1,50 @@
 class Solution {
-    int[] parents;
-   
-    public int countComponents(int n, int[][] edges) {
-        parents = new int[n];
-        for(int i = 0;i<n;i++){
-            parents[i] = i;
-        }
-        
-        for(int[] e : edges){
-            int a = find(e[0]);
-            int b = find(e[1]);
-            if(a !=b){
-                parents[a] = b;
-                n--;
+    class UnionFind{
+        int[] parents;
+        int[] rank;
+        public UnionFind(int n){
+            this.parents = new int[n];
+            this.rank = new int[n];
+            for(int i=0;i<n;i++){
+                parents[i]=i;
+                rank[i] = 1;
             }
         }
-        return n;
-    }
-    
-    private int find(int id){
-        if(id==parents[id]){
-            return id;
+        public int find(int x){
+            if(parents[x]==x){
+                return parents[x];
+            }
+            parents[x] = find(parents[x]);
+            return parents[x];
         }
-        parents[id]=find(parents[id]);
-        return parents[id];
+
+        public boolean union(int u, int v){
+            int uPar = find(u);
+            int vPar = find(v);
+            if(uPar==vPar){
+                return false;
+            }
+            if(rank[uPar]<rank[vPar]){
+                parents[uPar] = vPar;
+                rank[vPar]+=rank[uPar];
+            } else {
+                parents[vPar] = uPar;
+                rank[uPar]+=rank[vPar];
+            }
+            return true;
+        }
+    }
+    public int countComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        for(int[] edge: edges){
+            uf.union(edge[0], edge[1]);
+        }
+        int component=0;
+        for(int i=0;i<n;i++){
+            if(uf.find(i)==i){
+                component++;
+            }
+        }
+        return component;
     }
 }
