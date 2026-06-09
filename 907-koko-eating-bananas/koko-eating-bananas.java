cd38363b0ecdcc25b189b24c19698caffcd22dd0
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-06-09 16:12:25 -0700

    Time: 12 ms (54.41%) | Memory: 48 MB (29.29%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -3,11 +3,11 @@ class Solution {
         int low = 1;
         int high = 0;
         for(int p: piles){
-            high = Math.max(high, p);
+            high = Math.max(p, high);
         }
         while(low<high){
             int mid = low+(high-low)/2;
-            int hrs = countHrs(piles, mid);
+            int hrs = countHours(piles, mid);
             if(hrs>h){
                 low = mid+1;
             } else {
@@ -16,11 +16,12 @@ class Solution {
         }
         return low;
     }
-    private int countHrs(int[] nums, int h){
+
+    private int countHours(int[] piles, int hrs){
         int count = 0;
-        for(int num: nums){
-            count+=num/h;
-            if(num%h!=0){
+        for(int p: piles){
+            count+=p/hrs;
+            if(p%hrs!=0){
                 count++;
             }
         }
