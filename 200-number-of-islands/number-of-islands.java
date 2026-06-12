06653974bc600a033685fa666ddbe7e7e83b8440
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-06-11 23:58:26 -0700

    Time: 3 ms (87.45%) | Memory: 52.6 MB (14.47%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -6,31 +6,24 @@ class Solution {
         this.m = grid.length;
         this.n = grid[0].length;
         this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        int count =0;
+        int islandCount=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    grid[i][j]='2';
-                    count++;
-                    helper(grid, i, j);
+                    islandCount++;
+                    helper(grid,i, j);
                 }
             }
         }
-        return count;
+        return islandCount;
     }
-
     private void helper(char[][] grid, int i, int j){
-        Queue<int[]> bfsQue = new LinkedList<>();
-        bfsQue.add(new int[]{i,j});
-        while(!bfsQue.isEmpty()){
-            int[] curr = bfsQue.poll();
-            for(int[] dir: dirs){
-                int nr = dir[0]+curr[0];
-                int nc = dir[1]+curr[1];
-                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1'){
-                    grid[nr][nc]='2';
-                    bfsQue.add(new int[]{nr, nc});
-                }
+        grid[i][j]='0';
+        for(int[] dir: dirs){
+            int nr = dir[0]+i;
+            int nc = dir[1]+j;
+            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1'){
+                helper(grid, nr, nc);
             }
         }
     }
