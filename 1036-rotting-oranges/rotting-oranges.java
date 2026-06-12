4e3968626efc4725daa304cd4f784b06c8e498d2
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-06-12 12:56:05 -0700

    Time: 2 ms (90.85%) | Memory: 44.2 MB (40.21%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,44 +1,48 @@
+
+
+import javax.crypto.Cipher;
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        int freshOranges=0;
+        int freshOranges = 0;
         Queue<int[]> bfsQue = new LinkedList<>();
-        int minutes=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    freshOranges++;
-                } else if(grid[i][j]==2){
+                if(grid[i][j]==2){
                     bfsQue.add(new int[]{i,j});
+                } else if(grid[i][j]==1){
+                    freshOranges++;
                 }
             }
         }
+        int mins=0;
         if(freshOranges==0){
-            return minutes;
+            return mins;
         }
         while(!bfsQue.isEmpty()){
             int size = bfsQue.size();
-            minutes++;
+            mins++;
             for(int i=0;i<size;i++){
                 int[] curr = bfsQue.poll();
                 for(int[] dir: dirs){
                     int nr = dir[0]+curr[0];
                     int nc = dir[1]+curr[1];
-                    while(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
-                        grid[nr][nc]=2;
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
                         bfsQue.add(new int[]{nr, nc});
+                        grid[nr][nc]=2;
                         freshOranges--;
                         if(freshOranges==0){
-                            return minutes;
+                            return mins;
                         }
                     }
                 }
             }
         }
         if(freshOranges==0){
-            return minutes;
+            return mins;
         }
         return -1;
     }
