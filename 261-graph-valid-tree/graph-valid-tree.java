03b3735f8ccc7ff0dfa5b9664580445b310b1903
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-06-12 23:08:51 -0700

    Time: 0 ms (100.00%) | Memory: 46.4 MB (69.58%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,43 +1,42 @@
 class Solution {
     class UnionFind{
+        int[] ranks;
         int[] parents;
-        int[] rank;
         public UnionFind(int n){
             this.parents = new int[n];
-            this.rank = new int[n];
+            this.ranks = new int[n];
             for(int i=0;i<n;i++){
                 parents[i] = i;
-                rank[i] = 1;
             }
         }
-        private int find(int x){
+        public int find(int x){
             if(parents[x]==x){
                 return x;
             }
             parents[x] = find(parents[x]);
             return parents[x];
         }
-        private boolean union(int u, int v){
+        public boolean union(int u, int v){
             int uPar = find(u);
             int vPar = find(v);
             if(uPar==vPar){
                 return false;
             }
-            if(rank[vPar]<rank[uPar]){
-                parents[vPar] = uPar;
-                rank[uPar] += vPar;
-            } else {
+            if(ranks[uPar]<ranks[vPar]){
                 parents[uPar] = vPar;
-                rank[vPar] += uPar;
+                ranks[vPar]+=ranks[uPar];
+            } else {
+                parents[vPar] = uPar;
+                ranks[uPar]+=ranks[vPar];
             }
             return true;
         }
     }
     public boolean validTree(int n, int[][] edges) {
-        UnionFind uf = new UnionFind(n);
-        if(edges.length!=n-1){
+        if(edges.length != n - 1){ 
             return false;
         }
+        UnionFind uf = new UnionFind(n);
         for(int[] edge: edges){
             if(!uf.union(edge[0], edge[1])){
                 return false;
