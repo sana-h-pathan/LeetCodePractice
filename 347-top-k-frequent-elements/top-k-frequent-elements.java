8dce295fdacfeb6c520d2ef4f751fd9b957853b2
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-06-28 15:48:57 -0700

    Time: 16 ms (40.06%) | Memory: 47.6 MB (65.45%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -4,7 +4,7 @@ class Solution {
         for(int num: nums){
             map.put(num, map.getOrDefault(num, 0)+1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> Integer.compare(map.get(a), map.get(b)));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->Integer.compare(map.get(a), map.get(b)));
         for(int num: map.keySet()){
             pq.add(num);
             if(pq.size()>k){
@@ -12,8 +12,9 @@ class Solution {
             }
         }
         int[] result = new int[k];
-        for(int i=0;i<k;i++){
-            result[i] = pq.poll();
+        int i = 0;
+        while(!pq.isEmpty()){
+            result[i++] = pq.poll();
         }
         return result;
     }
