47287cfe0a8675cd953047a32eec0f7d23f629d4
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-06-28 17:24:13 -0700

    Time: 10 ms (14.04%) | Memory: 47.2 MB (33.64%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
         int[] indegree = new int[n];
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        HashMap<Integer, List<Integer>> courseMap = new HashMap<>();
         for(int[] pre: prerequisites){
-            int dep = pre[0];
             int ind = pre[1];
-            if(!map.containsKey(ind)){
-                map.put(ind, new ArrayList<>());
+            int dep = pre[0];
+            if(!courseMap.containsKey(ind)){
+                courseMap.put(ind, new ArrayList<>());
             }
-            map.get(ind).add(dep);
+            courseMap.get(ind).add(dep);
             indegree[dep]++;
         }
-        Queue<Integer> bfsQue = new LinkedList<>();
         int completedCourse = 0;
+        Queue<Integer> bfsQue = new LinkedList<>();
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
                 bfsQue.add(i);
@@ -24,22 +24,19 @@ class Solution {
         }
         while(!bfsQue.isEmpty()){
             int curr = bfsQue.poll();
-            if(map.containsKey(curr)){
-                for(int ne: map.get(curr)){
+            if(courseMap.containsKey(curr)){
+                for(int ne: courseMap.get(curr)){
                     indegree[ne]--;
                     if(indegree[ne]==0){
+                        bfsQue.add(ne);
                         completedCourse++;
                         if(completedCourse==n){
                             return true;
                         }
-                        bfsQue.add(ne);
                     }
                 }
             }
         }
-        if(completedCourse==n){
-            return true;
-        }
-        return false;
+        return completedCourse==n;
     }
 }
