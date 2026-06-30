f1ad1755f3411393bb19704b8d0d07340214fdad
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-06-29 23:05:40 -0700

    Time: 16 ms (58.24%) | Memory: 46.9 MB (53.94%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,27 +1,23 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s==null || t==null || s.length()<t.length()){
-            return "";
-        }
+        int l=0;
+        int r=0;
         HashMap<Character, Integer> map = new HashMap<>();
         for(char ch: t.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
         int minLength = Integer.MAX_VALUE;
+        int matches = 0;
         int sIdx = -1;
-        int l = 0;
-        int r = 0;
-        int currentMatches = t.length();
-        int reqdMatched = 0;
         while(r<s.length()){
             char rChar = s.charAt(r);
             if(map.containsKey(rChar)){
                 if(map.get(rChar)>0){
-                    reqdMatched++;
+                    matches++;
                 }
                 map.put(rChar, map.getOrDefault(rChar, 0)-1);
             }
-            while(currentMatches==reqdMatched){
+            while(matches==t.length()){
                 if(minLength>r-l+1){
                     minLength = r-l+1;
                     sIdx = l;
@@ -30,14 +26,14 @@ class Solution {
                 if(map.containsKey(lChar)){
                     map.put(lChar, map.getOrDefault(lChar, 0)+1);
                     if(map.get(lChar)>0){
-                        reqdMatched--;
+                        matches--;
                     }
                 }
                 l++;
             }
             r++;
         }
-        if(sIdx==-1){
+        if(minLength==Integer.MAX_VALUE){
             return "";
         }
         return s.substring(sIdx, sIdx+minLength);
