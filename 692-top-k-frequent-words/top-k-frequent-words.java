1c7f292d476dc143404ae2779cd7fae4e9dc9a3b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-07-01 07:43:35 -0700

    Time: 8 ms (55.48%) | Memory: 46.4 MB (67.81%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,44 +1,32 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-         if(words == null && words.length == 0) {
+        if(words==null || words.length ==0){
             return new ArrayList<>();
         }
-        Map<String,Integer> map = new HashMap<>();
-        
-        for(String w : words){
-            
-            map.put(w, map.getOrDefault(w,0)+1);
-        }
-        
-        Queue<String> pq = new PriorityQueue<>((a, b) -> {
-            int freqCompare = Integer.compare(map.get(a), map.get(b)); // ascending by frequency
-            if (freqCompare == 0) {
-                return b.compareTo(a); // if freq same → higher lexicographically first
+        HashMap<String, Integer> map = new HashMap<>();
+        PriorityQueue<String> pq = new PriorityQueue<>((a,b)->{
+            int freqA = map.get(a);
+            int freqB = map.get(b);
+            if(freqA==freqB){
+                return b.compareTo(a);
             }
-            return freqCompare;
+            return Integer.compare(freqA, freqB);
         });
-
-        
-        
+        for(String word: words){
+            map.put(word, map.getOrDefault(word, 0)+1);
+        }
         for(String key: map.keySet()){
-            pq.offer(key);
-            //create heap in a way that we can remove string whose count is less or we can remove string which is lexicographicallly high
-            //so we sort it in ascending order as we will be removing from start
-            //also we sort with lex desc order so that higher lex will be removed first with poll
-            if(pq.size() > k){
+            pq.add(key);
+
+            if(pq.size()>k){
                 pq.poll();
             }
-            
         }
-        
-        List<String> res= new LinkedList<>();
-        
-        
+        List<String> result = new ArrayList<>();
         while(!pq.isEmpty()){
-            res.add(0,pq.poll());
+            result.add(pq.poll());
         }
-        
-        return res;
-        
+        Collections.reverse(result);
+        return result;
     }
 }
