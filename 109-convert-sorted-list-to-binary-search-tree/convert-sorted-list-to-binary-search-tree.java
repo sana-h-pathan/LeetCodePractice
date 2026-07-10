4f4c9923d634b07a7a0e1d73d3d0a1b213c02af0
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-07-10 08:21:03 -0700

    Time: 1 ms (22.75%) | Memory: 46.5 MB (97.54%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -25,29 +25,35 @@
  */
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
-        if(head==null)
+        if(head==null){
             return null;
-        ListNode mid=findMiddle(head);
-        TreeNode root=new TreeNode(mid.val);
-        if(head==mid)
+        }
+        ListNode mid  = findMiddle(head);
+        TreeNode root = new TreeNode(mid.val);
+        if(head==mid){
             return root;
+        }
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(mid.next);
 
-        root.left=sortedListToBST(head);
-        root.right=sortedListToBST(mid.next);
         return root;
     }
 
     private ListNode findMiddle(ListNode head){
-        ListNode prev=null;
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null && fast.next!=null){
-            prev=slow;
-            slow=slow.next;
-            fast=fast.next.next;
+        if(head==null){
+            return null;
         }
-        if(prev!=null)
-            prev.next=null;
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(prev!=null){
+            prev.next = null;
+        }
         return slow;
     }
 }
