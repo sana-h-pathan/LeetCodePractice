538100a3bf5317c1c79f8c73ba4d70c9fb07b840
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-07-23 09:00:06 -0700

    Time: 19 ms (5.77%) | Memory: 47.6 MB (26.77%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -1,28 +1,21 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
         Stack<Integer> stk = new Stack<>();
-
-        int left = nums.length; // changed
-        int right = 0;          // changed
-
-        // Find the left boundary
-        for (int i = 0; i < nums.length; i++) {
-            while (!stk.isEmpty() && nums[stk.peek()] > nums[i]) {
-                left = Math.min(left, stk.pop()); // changed
+        int l = nums.length;
+        int r = 0;
+        for(int i=0;i<nums.length;i++){
+            while(!stk.isEmpty() && nums[stk.peek()]>nums[i]){
+                l = Math.min(l, stk.pop());
             }
             stk.push(i);
         }
-
         stk.clear();
-
-        // Find the right boundary
-        for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stk.isEmpty() && nums[stk.peek()] < nums[i]) {
-                right = Math.max(right, stk.pop()); // changed
+        for(int i=nums.length-1;i>=0;i--){
+            while(!stk.isEmpty() && nums[stk.peek()]<nums[i]){
+                r = Math.max(r, stk.pop());
             }
             stk.push(i);
         }
-
-        return right > left ? right - left + 1 : 0; // changed
+        return r>l?r-l+1:0;
     }
 }
