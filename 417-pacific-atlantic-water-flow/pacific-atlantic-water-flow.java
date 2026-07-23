f3e0e4d18f3e2ffa1407e00eab11bffb5d49d5d1
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-07-22 18:50:11 -0700

    Time: 5 ms (54.64%) | Memory: 47.2 MB (84.95%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -7,38 +7,45 @@ class Solution {
         this.m = heights.length;
         this.n = heights[0].length;
         this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        boolean[][] pVisted = new boolean[m][n];
         boolean[][] aVisited = new boolean[m][n];
+        boolean[][] pVisited = new boolean[m][n];
         for(int i=0;i<m;i++){
-            dfs(heights, i, 0, pVisted, heights[i][0]);
-            dfs(heights, i, n-1, aVisited, heights[i][n-1]);
+            dfs(heights, i, 0, pVisited);
+            dfs(heights, i, n-1, aVisited);
         }
         for(int j=0;j<n;j++){
-            dfs(heights, 0, j, pVisted, heights[0][j]);
-            dfs(heights, m-1, j, aVisited, heights[m-1][j]);
+            dfs(heights, 0, j, pVisited);
+            dfs(heights, m-1, j, aVisited);
         }
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(aVisited[i][j] && pVisted[i][j]){
-                    List<Integer> subList = new ArrayList<>();
-                    subList.add(i);
-                    subList.add(j);
-                    result.add(subList);
+                if(aVisited[i][j] && pVisited[i][j]){
+                    result.add(Arrays.asList(i, j));
                 }
             }
         }
         return result;
     }
-    private void dfs(int[][] heights, int i, int j, boolean[][] visited, int prevHeight){
+
+    private void dfs(int[][] heights, int i, int j, boolean[][] visited){
         if(visited[i][j]){
             return;
         }
-        visited[i][j]=true;
-        for(int[] dir: dirs){
-            int nr = dir[0]+i;
-            int nc = dir[1]+j;
-            if(nr>=0 && nc>=0 && nr<m && nc<n && heights[nr][nc]>=prevHeight){
-                dfs(heights, nr, nc, visited,  heights[nr][nc]);
+        Queue<int[]> bfsQue = new LinkedList<>();
+        bfsQue.add(new int[]{i,j});
+        visited[i][j] = true;
+        while(!bfsQue.isEmpty()){
+            int[] curr = bfsQue.poll();
+            int r = curr[0];
+            int c = curr[1];
+            for(int[] dir: dirs){
+                int nr = curr[0]+dir[0];
+                int nc = curr[1]+dir[1];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && !visited[nr][nc] && heights[nr][nc]>=heights[r][c]){
+                    visited[nr][nc]= true;
+                    bfsQue.add(new int[]{nr, nc});
+                }
             }
         }
     }
