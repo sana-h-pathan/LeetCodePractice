18ddf2ef2b014c320c71f19e4e6d374f95428156
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-07-22 17:19:36 -0700

    Time: 3 ms (25.18%) | Memory: 46.5 MB (68.14%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -6,35 +6,34 @@ class Solution {
         this.m = grid.length;
         this.n = grid[0].length;
         this.dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-        int maxArea = Integer.MIN_VALUE;
+        int maxArea = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int currArea = helper(grid, i, j);
-                maxArea = Math.max(currArea, maxArea);
+                if(grid[i][j]==1){
+                    int currArea = helper(grid, i, j);
+                    maxArea = Math.max(currArea, maxArea);
+                }
             }
         }
         return maxArea;
     }
     private int helper(int[][] grid, int i, int j){
-        int count = 0;
+        int islandArea = 1;
         Queue<int[]> bfsQue = new LinkedList<>();
-        if(grid[i][j]==1){
-            count++;
-            bfsQue.add(new int[]{i, j});
-            grid[i][j]=2;
-            while(!bfsQue.isEmpty()){
-                int[] curr = bfsQue.poll();
-                for(int[] dir: dirs){
-                    int nr = dir[0]+curr[0];
-                    int nc = dir[1]+curr[1];
-                    if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
-                        count++;
-                        grid[nr][nc]=2;
-                        bfsQue.add(new int[]{nr, nc});
-                    }
+        bfsQue.add(new int[]{i,j});
+        grid[i][j]=0;
+        while(!bfsQue.isEmpty()){
+            int[] curr = bfsQue.poll();
+            for(int[] dir: dirs){
+                int nr = dir[0]+curr[0];
+                int nc = dir[1]+curr[1];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]==1){
+                    islandArea++;
+                    bfsQue.add(new int[]{nr, nc});
+                    grid[nr][nc]=0;
                 }
             }
         }
-        return count;
+        return islandArea;
     }
 }
