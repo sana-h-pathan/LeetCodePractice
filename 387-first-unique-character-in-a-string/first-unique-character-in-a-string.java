5c30a901929fe69ffe76f6fd862cccb78b67735b
Sana Pathan <63260577+sana-h-pathan@users.noreply.github.com>
2026-07-23 09:28:26 -0700

    Time: 6 ms (84.53%) | Memory: 47 MB (39.84%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,14 +3,13 @@ class Solution {
         if(s==null || s.length()==0){
             return 0;
         }
-        int[] charArr = new int[26];
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            charArr[ch-'a']++;
+        int[] alphabets = new int[26];
+        for(char ch:s.toCharArray()){
+            alphabets[ch-'a']++;
         }
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(charArr[ch-'a']==1){
+            if(alphabets[ch-'a']==1){
                 return i;
             }
         }
